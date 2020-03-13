07a16b43c6bb376d95f5a9569940732e06cf9c41
Daniel Lacasse <daniel@nokee.dev>
2020-03-13 17:52:40 -0400

    Pull asciidoctor command test to WellBehavingSampleTest

--- subprojects/docs/src/docsTest/groovy/dev/nokee/docs/samples/JavaCJniLibrarySampleTest.groovy
+++ subprojects/docs/src/docsTest/groovy/dev/nokee/docs/samples/JavaCJniLibrarySampleTest.groovy
@@ -1,33 +1,5 @@
 package dev.nokee.docs.samples
 
-import dev.gradleplugins.test.fixtures.file.TestFile
-import dev.gradleplugins.test.fixtures.gradle.GradleExecuterFactory
-import dev.gradleplugins.test.fixtures.gradle.executer.GradleExecuter
-import dev.gradleplugins.test.fixtures.gradle.executer.LogContent
-import dev.gradleplugins.test.fixtures.gradle.executer.OutputScrapingExecutionResult
-import dev.gradleplugins.test.fixtures.logging.ConsoleOutput
-
-import static dev.gradleplugins.test.fixtures.gradle.GradleScriptDsl.GROOVY_DSL
-import static dev.gradleplugins.test.fixtures.gradle.GradleScriptDsl.KOTLIN_DSL
-
 class JavaCJniLibrarySampleTest extends WellBehavingSampleTest {
 	final String sampleName = 'java-c-jni-library'
-
-	def "can execute commands successfully"(dsl) {
-		def fixture = new SampleContentFixture(sampleName)
-		unzipTo(fixture.getDslSample(dsl), temporaryFolder.testDirectory)
-
-		GradleExecuter executer = configureLocalPluginResolution(new GradleExecuterFactory().wrapper(TestFile.of(temporaryFolder.testDirectory)).withConsole(ConsoleOutput.Rich))
-		expect:
-		fixture.getCommands().size() == 1
-		def command = fixture.getCommands()[0]
-		command.executable == './gradlew'
-		def result = executer.withArguments(command.args).run()
-		def expectedResult = OutputScrapingExecutionResult.from(command.expectedOutput.get(), '')
-
-		OutputScrapingExecutionResult.normalize(LogContent.of(result.getPlainTextOutput())).replace(' in 0s', '').startsWith(expectedResult.getOutput())
-
-		where:
-		dsl << [GROOVY_DSL, KOTLIN_DSL]
-	}
 }
--- subprojects/docs/src/docsTest/groovy/dev/nokee/docs/samples/WellBehavingSampleTest.groovy
+++ subprojects/docs/src/docsTest/groovy/dev/nokee/docs/samples/WellBehavingSampleTest.groovy
@@ -6,6 +6,9 @@ import dev.gradleplugins.test.fixtures.file.TestFile
 import dev.gradleplugins.test.fixtures.gradle.GradleExecuterFactory
 import dev.gradleplugins.test.fixtures.gradle.GradleScriptDsl
 import dev.gradleplugins.test.fixtures.gradle.executer.GradleExecuter
+import dev.gradleplugins.test.fixtures.gradle.executer.LogContent
+import dev.gradleplugins.test.fixtures.gradle.executer.OutputScrapingExecutionResult
+import dev.gradleplugins.test.fixtures.logging.ConsoleOutput
 import org.junit.Rule
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -66,6 +69,24 @@ abstract class WellBehavingSampleTest extends Specification {
 
 		where:
 		taskName << ['help', 'tasks']
+		dsl << [GradleScriptDsl.GROOVY_DSL, GradleScriptDsl.KOTLIN_DSL]
+	}
+
+	def "can execute commands successfully"(dsl) {
+		def fixture = new SampleContentFixture(sampleName)
+		unzipTo(fixture.getDslSample(dsl), temporaryFolder.testDirectory)
+
+		GradleExecuter executer = configureLocalPluginResolution(new GradleExecuterFactory().wrapper(TestFile.of(temporaryFolder.testDirectory)).withConsole(ConsoleOutput.Rich))
+		expect:
+		fixture.getCommands().size() == 1
+		def command = fixture.getCommands()[0]
+		command.executable == './gradlew'
+		def result = executer.withArguments(command.args).run()
+		def expectedResult = OutputScrapingExecutionResult.from(command.expectedOutput.get(), '')
+
+		OutputScrapingExecutionResult.normalize(LogContent.of(result.getPlainTextOutput())).replace(' in 0s', '').startsWith(expectedResult.getOutput())
+
+		where:
 		dsl << [GradleScriptDsl.GROOVY_DSL, GradleScriptDsl.KOTLIN_DSL]
 	}
 }
