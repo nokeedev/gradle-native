6e05eb05206e331d6b5b1a58d6ba57e06f7678d9
Daniel Lacasse <daniel@nokee.dev>
2020-03-22 14:01:10 -0400

    Ignore the /current/ paths

--- subprojects/docs/src/docsTest/groovy/dev/nokee/docs/BrokenLinksTest.groovy
+++ subprojects/docs/src/docsTest/groovy/dev/nokee/docs/BrokenLinksTest.groovy
@@ -9,7 +9,7 @@ class BrokenLinksTest extends Specification {
 		def report = new HtmlLinkTester(HtmlLinkTester.validEmails("hello@nokee.dev"), new HtmlLinkTester.BlackList() {
 			@Override
 			boolean isBlackListed(URI uri) {
-				return uri.toString().contains("/blog")
+				return uri.toString().contains("/blog") || uri.toString().contains('/current/')
 			}
 		}).reportBrokenLinks(new File(System.getProperty('bakedContentDirectory')))
 		report.assertNoFailures()
