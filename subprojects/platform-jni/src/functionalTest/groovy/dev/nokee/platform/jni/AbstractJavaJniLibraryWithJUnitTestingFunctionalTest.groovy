52a832e1722c2127d8b4a06710b10554a6d9ed6f
Daniel Lacasse <daniel@nokee.dev>
2020-03-25 09:39:53 -0400

    Generalize the JUnit test for other native language

---
+++ subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/platform/jni/AbstractJavaJniLibraryWithJUnitTestingFunctionalTest.groovy
@@ -0,0 +1,30 @@
+package dev.nokee.platform.jni
+
+import dev.gradleplugins.integtests.fixtures.nativeplatform.AbstractInstalledToolChainIntegrationSpec
+import dev.nokee.platform.jni.fixtures.elements.JavaGreeterJUnitTest
+import dev.nokee.platform.jni.fixtures.elements.JniLibraryElement
+
+abstract class AbstractJavaJniLibraryWithJUnitTestingFunctionalTest extends AbstractInstalledToolChainIntegrationSpec {
+	def "can test library using JUnit"() {
+		makeSingleProject()
+		buildFile << '''
+			repositories {
+				mavenCentral()
+			}
+
+			dependencies {
+				testImplementation 'junit:junit:4.12'
+			}
+		'''
+		componentUnderTest.writeToProject(testDirectory)
+		new JavaGreeterJUnitTest().writeToProject(testDirectory)
+
+		expect:
+		succeeds('test')
+		// TODO: assert JUnit result
+	}
+
+	protected abstract void makeSingleProject()
+
+	protected abstract JniLibraryElement getComponentUnderTest()
+}
--- subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/platform/jni/JavaCJniLibraryWithJUnitTestingFunctionalTest.groovy
+++ subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/platform/jni/JavaCJniLibraryWithJUnitTestingFunctionalTest.groovy
@@ -1,21 +1,21 @@
 package dev.nokee.platform.jni
 
-import dev.nokee.platform.jni.fixtures.JavaJniCppGreeterLib
+import dev.nokee.platform.jni.fixtures.JavaJniCGreeterLib
 import dev.nokee.platform.jni.fixtures.elements.JniLibraryElement
 
-class JavaCppJniLibraryWithJUnitTestingFunctionalTest extends AbstractJavaJniLibraryWithJUnitTestingFunctionalTest {
+class JavaCJniLibraryWithJUnitTestingFunctionalTest extends AbstractJavaJniLibraryWithJUnitTestingFunctionalTest {
 	protected void makeSingleProject() {
 		settingsFile << "rootProject.name = 'jni-greeter'"
 		buildFile << '''
 			plugins {
 				id 'java'
 				id 'dev.nokee.jni-library'
-				id 'dev.nokee.cpp-language'
+				id 'dev.nokee.c-language'
 			}
 		'''
 	}
 
 	protected JniLibraryElement getComponentUnderTest() {
-		return new JavaJniCppGreeterLib('jni-greeter')
+		return new JavaJniCGreeterLib('jni-greeter')
 	}
 }
