9164224cf5520d20006c97a1411372bdf45590e8
Daniel Lacasse <daniel@nokee.dev>
2020-04-22 21:38:12 -0400

    Add missing sample test for framework dependencies

---
+++ subprojects/docs/src/docsTest/groovy/dev/nokee/docs/samples/JniLibraryWithFrameworkDependenciesSampleTest.groovy
@@ -0,0 +1,8 @@
+package dev.nokee.docs.samples
+
+class JniLibraryWithFrameworkDependenciesSampleTest extends WellBehavingSampleTest {
+	@Override
+	protected String getSampleName() {
+		return 'jni-library-with-framework-dependencies'
+	}
+}
