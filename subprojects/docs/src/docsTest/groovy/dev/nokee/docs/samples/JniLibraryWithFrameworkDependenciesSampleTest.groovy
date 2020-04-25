b643b68ed1498018e155d65bf0c920da624d33eb
Daniel Lacasse <daniel@nokee.dev>
2020-04-24 22:16:17 -0400

    Ignore framework test on Windows and Linux

--- subprojects/docs/src/docsTest/groovy/dev/nokee/docs/samples/JniLibraryWithFrameworkDependenciesSampleTest.groovy
+++ subprojects/docs/src/docsTest/groovy/dev/nokee/docs/samples/JniLibraryWithFrameworkDependenciesSampleTest.groovy
@@ -1,8 +1,18 @@
 package dev.nokee.docs.samples
 
+import dev.gradleplugins.integtests.fixtures.nativeplatform.ToolChainRequirement
+import org.apache.commons.lang3.SystemUtils
+import org.junit.Assume
+
 class JniLibraryWithFrameworkDependenciesSampleTest extends WellBehavingSampleTest {
 	@Override
 	protected String getSampleName() {
 		return 'jni-library-with-framework-dependencies'
+	}
+
+	@Override
+	protected ToolChainRequirement getToolChainRequirement() {
+		Assume.assumeTrue(SystemUtils.IS_OS_MAC)
+		return ToolChainRequirement.GCC_COMPATIBLE // Not technically right but good enough
 	}
 }
