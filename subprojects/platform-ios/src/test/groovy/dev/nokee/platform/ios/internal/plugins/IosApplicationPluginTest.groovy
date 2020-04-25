63497d72b5171b9d711766f94941ea8e9d449cff
Daniel Lacasse <daniel@nokee.dev>
2020-04-25 10:16:16 -0400

    Skip all iOS test on Windows or Linux

--- subprojects/platform-ios/src/test/groovy/dev/nokee/platform/ios/internal/plugins/IosApplicationPluginTest.groovy
+++ subprojects/platform-ios/src/test/groovy/dev/nokee/platform/ios/internal/plugins/IosApplicationPluginTest.groovy
@@ -6,11 +6,14 @@ import dev.nokee.platform.ios.tasks.internal.AssetCatalogCompileTask
 import dev.nokee.platform.ios.tasks.internal.CreateIosApplicationBundleTask
 import dev.nokee.platform.ios.tasks.internal.StoryboardCompileTask
 import dev.nokee.platform.ios.tasks.internal.StoryboardLinkTask
+import org.apache.commons.lang3.SystemUtils
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Rule
+import spock.lang.Requires
 import spock.lang.Specification
 import spock.lang.Subject
 
+@Requires({SystemUtils.IS_OS_MAC})
 @Subject(IosApplicationPlugin)
 @CleanupTestDirectory
 class IosApplicationPluginTest extends Specification {
