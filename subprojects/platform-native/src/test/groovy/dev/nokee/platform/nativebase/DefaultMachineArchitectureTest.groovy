36a9dbcabecb3c0158b9461877daa9c4098bf4b1
Daniel Lacasse <daniel@nokee.dev>
2020-05-21 19:43:36 -0400

    Fix up some test that IntelliJ screw up with

--- subprojects/platform-native/src/test/groovy/dev/nokee/platform/nativebase/DefaultMachineArchitectureTest.groovy
+++ subprojects/platform-native/src/test/groovy/dev/nokee/platform/nativebase/DefaultMachineArchitectureTest.groovy
@@ -1,19 +1,18 @@
 package dev.nokee.platform.nativebase
 
+import dev.nokee.platform.nativebase.internal.DefaultMachineArchitecture
 import spock.lang.Specification
-
-import static dev.nokee.platform.nativebase.internal.DefaultMachineArchitecture.*
 
 class DefaultMachineArchitectureTest extends Specification {
 	def "can compare machine architecture instance"() {
 		expect:
-		dev.nokee.platform.nativebase.internal.DefaultMachineArchitecture.X86 == dev.nokee.platform.nativebase.internal.DefaultMachineArchitecture.X86
-		dev.nokee.platform.nativebase.internal.DefaultMachineArchitecture.X86 != dev.nokee.platform.nativebase.internal.DefaultMachineArchitecture.X86_64
+		DefaultMachineArchitecture.X86 == DefaultMachineArchitecture.X86
+		DefaultMachineArchitecture.X86 != DefaultMachineArchitecture.X86_64
 	}
 
 	def "defaults to the right pre-made instances"() {
 		expect:
 		// TODO Improve when we test on other architectures
-		dev.nokee.platform.nativebase.internal.DefaultMachineArchitecture.HOST == dev.nokee.platform.nativebase.internal.DefaultMachineArchitecture.X86_64
+		DefaultMachineArchitecture.HOST == DefaultMachineArchitecture.X86_64
 	}
 }
