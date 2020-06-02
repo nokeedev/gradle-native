a052ce6841a9debf7d8e2251321e0a9020f01e0a
Daniel Lacasse <daniel@nokee.dev>
2020-06-02 13:14:51 -0400

    Move tests from the wrong location because of IntelliJ

--- subprojects/platform-native/src/test/groovy/dev/nokee/platform/nativebase/internal/DefaultTargetMachineFactoryTest.groovy
+++ subprojects/platform-native/src/test/groovy/dev/nokee/platform/nativebase/internal/DefaultTargetMachineFactoryTest.groovy
@@ -1,9 +1,9 @@
 package dev.nokee.platform.nativebase.internal
 
+import dev.nokee.platform.nativebase.internal.DefaultTargetMachineFactory
 import dev.nokee.runtime.nativebase.internal.DefaultMachineArchitecture
 import dev.nokee.runtime.nativebase.internal.DefaultOperatingSystemFamily
 import dev.nokee.runtime.nativebase.internal.DefaultTargetMachine
-import dev.nokee.platform.nativebase.internal.DefaultTargetMachineFactory
 import spock.lang.Requires
 import spock.lang.Specification
 import spock.util.environment.OperatingSystem
