02c1d1c6515e827db934e144b339a09b38cdb35c
Daniel Lacasse <daniel@nokee.dev>
2020-06-02 12:45:21 -0400

    Remove Objective-C and Swift iOS library plugins

--- subprojects/platform-ios/src/main/java/dev/nokee/platform/ios/ObjectiveCIosLibraryExtension.java
+++
@@ -1,7 +0,0 @@
-package dev.nokee.platform.ios;
-
-import dev.nokee.platform.base.DependencyAwareComponent;
-import dev.nokee.platform.nativebase.NativeLibraryDependencies;
-
-public interface ObjectiveCIosLibraryExtension extends DependencyAwareComponent<NativeLibraryDependencies> {
-}
