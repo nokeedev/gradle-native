05ee1529515afb25ea678e94004df72607b642b6
Daniel Lacasse <daniel@nokee.dev>
2020-06-09 15:11:58 -0400

    Fix the samples with regards to testing

--- subprojects/docs/src/docs/samples/objective-c-library/kotlin-dsl/build.gradle.kts
+++ subprojects/docs/src/docs/samples/objective-c-library/kotlin-dsl/build.gradle.kts
@@ -2,3 +2,13 @@ plugins {
 	id("dev.nokee.objective-c-library")
 	id("dev.nokee.xcode-ide")
 }
+
+import dev.nokee.platform.nativebase.SharedLibraryBinary
+
+library.variants.configureEach {
+	binaries.configureEach(SharedLibraryBinary::class.java) {
+		linkTask.configure {
+			linkerArgs.add("-lobjc")
+		}
+	}
+}
--- subprojects/docs/src/docs/samples/objective-cpp-application/kotlin-dsl/build.gradle.kts
+++ subprojects/docs/src/docs/samples/objective-cpp-application/kotlin-dsl/build.gradle.kts
@@ -2,3 +2,13 @@ plugins {
 	id("dev.nokee.objective-cpp-application")
 	id("dev.nokee.xcode-ide")
 }
+
+import dev.nokee.platform.nativebase.ExecutableBinary
+
+application.variants.configureEach {
+	binaries.configureEach(ExecutableBinary::class.java) {
+		linkTask.configure {
+			linkerArgs.add("-lobjc")
+		}
+	}
+}
