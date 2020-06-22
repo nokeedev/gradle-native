15c5eac4c1b0b2e6ccb2ba273647f5130b295c31
Daniel Lacasse <daniel@nokee.dev>
2020-06-21 20:19:36 -0400

    Extract JavaTaskNames.ProjectTasks to DefaultJavaProjectTasks

---
+++ subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/language/DefaultJavaProjectTasks.groovy
@@ -0,0 +1,111 @@
+package dev.nokee.language
+
+class DefaultJavaProjectTasks {
+	private final String project
+
+	DefaultJavaProjectTasks(String project) {
+		this.project = project
+	}
+
+	String getCompile() {
+		return main.compile
+	}
+
+	String getJar() {
+		return main.jar
+	}
+
+	String getAssemble() {
+		return main.assemble
+	}
+
+	String getProcessResources() {
+		return main.processResources
+	}
+
+	List<String> getAllToJar() {
+		return main.allToJar
+	}
+
+	List<String> getAllToTest() {
+		return test.allToClasses + main.allToClasses + [test.test]
+	}
+
+	List<String> getAllToAssemble() {
+		return allToJar + [assemble]
+	}
+
+	MainTasks getMain() {
+		return new MainTasks()
+	}
+
+	TestTasks getTest() {
+		return new TestTasks()
+	}
+
+	abstract class VariantTasks {
+		protected abstract String getComponentVariantName()
+
+		String getCompile() {
+			return withProject("compile${componentVariantName}Java")
+		}
+
+		String getProcessResources() {
+			return withProject("process${componentVariantName}Resources")
+		}
+
+		protected abstract String getClasses()
+
+		List<String> getAllToJar() {
+			return allToClasses + [jar]
+		}
+
+		List<String> getAllToAssemble() {
+			return allToJar + [assemble]
+		}
+
+		List<String> getAllToClasses() {
+			return [compile, processResources, classes]
+		}
+	}
+
+	class MainTasks extends VariantTasks {
+		@Override
+		protected String getComponentVariantName() {
+			return ""
+		}
+
+		@Override
+		protected String getClasses() {
+			return withProject('classes')
+		}
+
+		String getJar() {
+			return withProject("jar")
+		}
+
+		String getAssemble() {
+			return withProject("assemble")
+		}
+	}
+
+	class TestTasks extends VariantTasks {
+		@Override
+		protected String getComponentVariantName() {
+			return "Test"
+		}
+
+		@Override
+		protected String getClasses() {
+			return withProject('testClasses')
+		}
+
+		String getTest() {
+			return withProject('test')
+		}
+	}
+
+	private withProject(String t) {
+		project + ":" + t
+	}
+}
--- subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/language/java/JavaTaskNames.groovy
+++ subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/language/java/JavaTaskNames.groovy
@@ -1,122 +1,13 @@
 package dev.nokee.language.java
 
+import dev.nokee.language.DefaultJavaProjectTasks
 import dev.nokee.language.LanguageTaskNames
 
 trait JavaTaskNames implements LanguageTaskNames {
 	/**
 	 * Returns the tasks for the root project.
 	 */
-	ProjectTasks getTasks() {
-		return new ProjectTasks('')
-	}
-
-	static class ProjectTasks {
-		private final String project
-
-		ProjectTasks(String project) {
-			this.project = project
-		}
-
-		String getCompile() {
-			return main.compile
-		}
-
-		String getJar() {
-			return main.jar
-		}
-
-		String getAssemble() {
-			return main.assemble
-		}
-
-		String getProcessResources() {
-			return main.processResources
-		}
-
-		List<String> getAllToJar() {
-			return main.allToJar
-		}
-
-		List<String> getAllToTest() {
-			return test.allToClasses + main.allToClasses + [test.test]
-		}
-
-		List<String> getAllToAssemble() {
-			return allToJar + [assemble]
-		}
-
-		MainTasks getMain() {
-			return new MainTasks()
-		}
-
-		TestTasks getTest() {
-			return new TestTasks()
-		}
-
-		abstract class VariantTasks {
-			protected abstract String getComponentVariantName()
-
-			String getCompile() {
-				return withProject("compile${componentVariantName}Java")
-			}
-
-			String getProcessResources() {
-				return withProject("process${componentVariantName}Resources")
-			}
-
-			protected abstract String getClasses()
-
-			List<String> getAllToJar() {
-				return allToClasses + [jar]
-			}
-
-			List<String> getAllToAssemble() {
-				return allToJar + [assemble]
-			}
-
-			List<String> getAllToClasses() {
-				return [compile, processResources, classes]
-			}
-		}
-
-		class MainTasks extends VariantTasks {
-			@Override
-			protected String getComponentVariantName() {
-				return ""
-			}
-
-			@Override
-			protected String getClasses() {
-				return withProject('classes')
-			}
-
-			String getJar() {
-				return withProject("jar")
-			}
-
-			String getAssemble() {
-				return withProject("assemble")
-			}
-		}
-
-		class TestTasks extends VariantTasks {
-			@Override
-			protected String getComponentVariantName() {
-				return "Test"
-			}
-
-			@Override
-			protected String getClasses() {
-				return withProject('testClasses')
-			}
-
-			String getTest() {
-				return withProject('test')
-			}
-		}
-
-		private withProject(String t) {
-			project + ":" + t
-		}
+	DefaultJavaProjectTasks getTasks() {
+		return new DefaultJavaProjectTasks('')
 	}
 }
