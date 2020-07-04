3f11569969306d21dd0862069bea778b87dbfd95
Daniel Lacasse <daniel@nokee.dev>
2020-07-03 23:43:01 -0400

    Convert spaces to tabs

--- subprojects/ide-visual-studio/src/main/java/dev/nokee/ide/visualstudio/internal/VisualStudioIdePropertyAdapter.java
+++ subprojects/ide-visual-studio/src/main/java/dev/nokee/ide/visualstudio/internal/VisualStudioIdePropertyAdapter.java
@@ -8,47 +8,47 @@ import java.util.Arrays;
 import java.util.List;
 
 public abstract class VisualStudioIdePropertyAdapter {
-    @Inject
+	@Inject
 	protected abstract ProviderFactory getProviders();
 
-    public Provider<String> getAction() {
-    	return getXcodeProperty("Action");
+	public Provider<String> getAction() {
+		return getXcodeProperty("Action");
 	}
 
-    public Provider<String> getConfiguration() {
-        return getXcodeProperty("Configuration");
-    }
-
-    public Provider<String> getPlatformName() {
-    	return getXcodeProperty("PlatformName");
+	public Provider<String> getConfiguration() {
+		return getXcodeProperty("Configuration");
 	}
 
-    public Provider<String> getProjectName() {
-    	return getXcodeProperty("ProjectName");
+	public Provider<String> getPlatformName() {
+		return getXcodeProperty("PlatformName");
+	}
+
+	public Provider<String> getProjectName() {
+		return getXcodeProperty("ProjectName");
 	}
 
 	public Provider<String> getOutputDirectory() {
-    	return getXcodeProperty("OutDir");
+		return getXcodeProperty("OutDir");
 	}
 
-    private Provider<String> getXcodeProperty(String name) {
-        return getProviders().gradleProperty(prefixName(name));
-    }
+	private Provider<String> getXcodeProperty(String name) {
+		return getProviders().gradleProperty(prefixName(name));
+	}
 
-    public static List<String> getAdapterCommandLine() {
-        return Arrays.asList(
-            toGradleProperty("OutDir"),
-            toGradleProperty("PlatformName"),
-            toGradleProperty("Configuration"),
+	public static List<String> getAdapterCommandLine() {
+		return Arrays.asList(
+			toGradleProperty("OutDir"),
+			toGradleProperty("PlatformName"),
+			toGradleProperty("Configuration"),
 			toGradleProperty("ProjectName")
-        );
-    }
+		);
+	}
 
-    private static String toGradleProperty(String source) {
-        return "-P" + prefixName(source) + "=$(" + source + ")";
-    }
+	private static String toGradleProperty(String source) {
+		return "-P" + prefixName(source) + "=$(" + source + ")";
+	}
 
-    private static String prefixName(String source) {
-        return "dev.nokee.internal.visualStudio.bridge." + source;
-    }
+	private static String prefixName(String source) {
+		return "dev.nokee.internal.visualStudio.bridge." + source;
+	}
 }
