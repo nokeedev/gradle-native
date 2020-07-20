59d4b14cd80dd95c471f4a686e39d940dba8b4dd
Daniel Lacasse <daniel@nokee.dev>
2020-07-20 17:32:41 +0200

    Split C++ Visual Studio IDE language configuration tests

--- subprojects/ide-visual-studio/src/functionalTest/groovy/dev/nokee/ide/visualstudio/VisualStudioIdeCppComponentFunctionalTest.groovy
+++ subprojects/ide-visual-studio/src/functionalTest/groovy/dev/nokee/ide/visualstudio/VisualStudioIdeCppComponentFunctionalTest.groovy
@@ -1,14 +1,9 @@
 package dev.nokee.ide.visualstudio
 
-import dev.gradleplugins.test.fixtures.sources.NativeSourceElement
 import dev.gradleplugins.test.fixtures.sources.SourceElement
-import dev.nokee.ide.visualstudio.fixtures.VisualStudioIdeProjectFixture
 import dev.nokee.language.cpp.CppTaskNames
-import dev.nokee.language.cpp.tasks.CppCompile
 import dev.nokee.platform.jni.fixtures.elements.CppGreeter
-import dev.nokee.platform.nativebase.NativeBinary
 import dev.nokee.platform.nativebase.fixtures.CppGreeterApp
-import spock.lang.Unroll
 
 class VisualStudioIdeCppApplicationFunctionalTest extends AbstractVisualStudioIdeNativeComponentPluginFunctionalTest implements CppTaskNames {
 	@Override
@@ -44,46 +39,6 @@ class VisualStudioIdeCppApplicationFunctionalTest extends AbstractVisualStudioId
 	@Override
 	protected List<String> getAllTasksForBuildAction() {
 		return tasks.allToLink
-	}
-
-	@Override
-	protected VisualStudioIdeProjectFixture getVisualStudioProjectUnderTest() {
-		return visualStudioProject('main')
-	}
-
-	@Unroll
-	def "configures C++ language support in generated projects"(compilerFlag, expectedLanguageStandard, uniqueIndex) {
-		given:
-		makeSingleProject()
-		componentUnderTest.writeToProject(testDirectory)
-		buildFile << """
-			import ${NativeBinary.canonicalName}
-			import ${CppCompile.canonicalName}
-			application {
-				binaries.configureEach(${NativeBinary.simpleName}) {
-					compileTasks.configureEach(${CppCompile.simpleName}) {
-						compilerArgs.add('${compilerFlag}')
-					}
-				}
-			}
-		"""
-
-		when:
-		succeeds('visualStudio')
-
-		then:
-		visualStudioProject('main').projectConfigurations*.languageStandard.every { it == expectedLanguageStandard }
-
-		where:
-		// UniqueIndex: // uniqueIndex: https://github.com/gradle/gradle/issues/8787
-		compilerFlag     | expectedLanguageStandard | uniqueIndex
-		''				 | 'Default'				| 0
-		'/std:c++14'     | 'stdcpp14'               | 1
-		'-std:c++14'     | 'stdcpp14'               | 2
-		'/std:c++17'     | 'stdcpp17'               | 3
-		'-std:c++17'     | 'stdcpp17'               | 4
-		'/std:c++latest' | 'stdcpplatest'           | 5
-		'-std:c++latest' | 'stdcpplatest'           | 6
 	}
 }
 
@@ -121,46 +76,6 @@ class VisualStudioIdeCppLibraryFunctionalTest extends AbstractVisualStudioIdeNat
 	@Override
 	protected List<String> getAllTasksForBuildAction() {
 		return tasks.allToLink
-	}
-
-	@Override
-	protected VisualStudioIdeProjectFixture getVisualStudioProjectUnderTest() {
-		return visualStudioProject('main')
-	}
-
-	@Unroll
-	def "configures C++ language support in generated projects"(compilerFlag, expectedLanguageStandard, uniqueIndex) {
-		given:
-		makeSingleProject()
-		componentUnderTest.writeToProject(testDirectory)
-		buildFile << """
-			import ${NativeBinary.canonicalName}
-			import ${CppCompile.canonicalName}
-			library {
-				binaries.configureEach(${NativeBinary.simpleName}) {
-					compileTasks.configureEach(${CppCompile.simpleName}) {
-						compilerArgs.add('${compilerFlag}')
-					}
-				}
-			}
-		"""
-
-		when:
-		succeeds('visualStudio')
-
-		then:
-		visualStudioProject('main').projectConfigurations*.languageStandard.every { it == expectedLanguageStandard }
-
-		where:
-		// UniqueIndex: // uniqueIndex: https://github.com/gradle/gradle/issues/8787
-		compilerFlag     | expectedLanguageStandard | uniqueIndex
-		''				 | 'Default'				| 0
-		'/std:c++14'     | 'stdcpp14'               | 1
-		'-std:c++14'     | 'stdcpp14'               | 2
-		'/std:c++17'     | 'stdcpp17'               | 3
-		'-std:c++17'     | 'stdcpp17'               | 4
-		'/std:c++latest' | 'stdcpplatest'           | 5
-		'-std:c++latest' | 'stdcpplatest'           | 6
 	}
 }
 
