ab34e334b7a477365a2adeea59d31a9cbbcd86d2
Daniel Lacasse <daniel@nokee.dev>
2020-07-25 20:41:09 +0200

    Add more core utilities functionality around composite build detection

--- subprojects/core-utils/src/main/java/dev/nokee/utils/GradleUtils.java
+++ subprojects/core-utils/src/main/java/dev/nokee/utils/GradleUtils.java
@@ -8,4 +8,16 @@ public final class GradleUtils {
 	public static boolean isHostBuild(Gradle gradle) {
 		return gradle.getParent() == null;
 	}
+
+	public static boolean hasIncludedBuilds(Gradle gradle) {
+		return !gradle.getIncludedBuilds().isEmpty();
+	}
+
+	public static boolean isIncludedBuild(Gradle gradle) {
+		return gradle.getParent() != null;
+	}
+
+	public static boolean isCompositeBuild(Gradle gradle) {
+		return isIncludedBuild(gradle) || hasIncludedBuilds(gradle);
+	}
 }
