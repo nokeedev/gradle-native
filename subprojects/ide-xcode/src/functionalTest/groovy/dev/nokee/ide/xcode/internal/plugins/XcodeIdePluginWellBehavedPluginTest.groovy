a8690efd7e2238c223bdbf608214bc4d297a4e70
Daniel Lacasse <daniel@nokee.dev>
2020-07-25 19:59:03 +0200

    Add @Subject annotation

--- subprojects/ide-xcode/src/functionalTest/groovy/dev/nokee/ide/xcode/internal/plugins/XcodeIdePluginWellBehavedPluginTest.groovy
+++ subprojects/ide-xcode/src/functionalTest/groovy/dev/nokee/ide/xcode/internal/plugins/XcodeIdePluginWellBehavedPluginTest.groovy
@@ -1,7 +1,9 @@
 package dev.nokee.ide.xcode.internal.plugins
 
 import dev.gradleplugins.integtests.fixtures.WellBehavedPluginTest
+import spock.lang.Subject
 
+@Subject(XcodeIdePlugin)
 class XcodeIdePluginWellBehavedPluginTest extends WellBehavedPluginTest {
 	@Override
 	String getQualifiedPluginId() {
