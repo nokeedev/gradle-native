8be2073cc65f988c8421f847b2593cbb4c09a7a8
Daniel Lacasse <daniel@nokee.dev>
2020-08-24 21:47:44 +0200

    Fix condition regarding deprecation coming in 6.7

--- subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/platform/jni/JavaApplicationFixture.groovy
+++ subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/platform/jni/JavaApplicationFixture.groovy
@@ -6,7 +6,7 @@ import org.gradle.util.GradleVersion
 trait JavaApplicationFixture {
 	String configureJavaApplicationMainClassName(String mainClassName) {
 		def gradleVersionValue = System.getProperty(AbstractGradleSpecification.DEFAULT_GRADLE_VERSION_SYSPROP_NAME)
-		if (gradleVersionValue != null && 0 >= GradleVersion.version(gradleVersionValue).compareTo(GradleVersion.version("6.7"))) {
+		if (gradleVersionValue != null && GradleVersion.version(gradleVersionValue) >= GradleVersion.version("6.7")) {
 			return """
 				application {
 					mainClass = '${mainClassName}'
