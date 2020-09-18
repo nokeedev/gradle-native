f554ac50339721cdba7813ee8eb6ecb06decbb9a
Daniel Lacasse <daniel@nokee.dev>
2020-09-18 10:57:38 +0200

    Introduce whenElementKnown on VariantView
    Currently it's an internal API but this will most likely become public
    API soon.

--- subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/VariantViewImpl.java
+++ subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/VariantViewImpl.java
@@ -2,9 +2,12 @@ package dev.nokee.platform.base.internal;
 
 import com.google.common.collect.ImmutableSet;
 import dev.nokee.model.internal.NokeeCollection;
+import dev.nokee.model.internal.NokeeMap;
+import dev.nokee.model.internal.Value;
 import dev.nokee.platform.base.Variant;
 import dev.nokee.platform.base.VariantView;
 import dev.nokee.utils.ProviderUtils;
+import lombok.EqualsAndHashCode;
 import org.gradle.api.Action;
 import org.gradle.api.Transformer;
 import org.gradle.api.provider.Provider;
@@ -14,34 +17,34 @@ import java.util.List;
 import java.util.Set;
 
 import static dev.nokee.utils.ActionUtils.onlyIf;
-import static dev.nokee.utils.SpecUtils.byType;
+import static dev.nokee.utils.TransformerUtils.configureInPlace;
 import static java.util.Objects.requireNonNull;
 
-public final class VariantViewImpl<T extends Variant> implements VariantView<T> {
-	private final NokeeCollection<T> store;
+public final class VariantViewImpl<T extends Variant> implements VariantViewInternal<T> {
+	private final NokeeCollection<NokeeMap.Entry<VariantIdentifier<T>, T>> store;
 
-	public VariantViewImpl(NokeeCollection<T> store) {
+	public VariantViewImpl(NokeeCollection<NokeeMap.Entry<VariantIdentifier<T>, T>> store) {
 		this.store = requireNonNull(store);
 	}
 
 	@Override
 	public void configureEach(Action<? super T> action) {
-		store.forEach(action);
+		store.forEach(configureValue(action));
 	}
 
 	@Override
 	public <S extends T> void configureEach(Class<S> type, Action<? super S> action) {
-		store.forEach(onlyIf(type, action));
+		store.forEach(configureValue(onlyIf(type, action)));
 	}
 
 	@Override
 	public void configureEach(Spec<? super T> spec, Action<? super T> action) {
-		store.forEach(onlyIf(spec, action));
+		store.forEach(configureValue(onlyIf(spec, action)));
 	}
 
 	@Override
 	public <S extends T> VariantView<S> withType(Class<S> type) {
-		return new VariantViewImpl<>(store.filter(byType(type)));
+		return new VariantViewImpl(store.filter(byType(type)));
 	}
 
 	@Override
@@ -51,7 +54,7 @@ public final class VariantViewImpl<T extends Variant> implements VariantView<T> 
 
 	@Override
 	public Set<? extends T> get() {
-		return ImmutableSet.copyOf(store.get());
+		return store.get().stream().map(NokeeMap.Entry::getValue).map(Value::get).collect(ImmutableSet.toImmutableSet());
 	}
 
 	@Override
@@ -67,5 +70,79 @@ public final class VariantViewImpl<T extends Variant> implements VariantView<T> 
 	@Override
 	public Provider<List<? extends T>> filter(Spec<? super T> spec) {
 		return getElements().map(ProviderUtils.filter(spec));
+	}
+
+	@Override
+	public void whenElementKnown(Action<? super KnownVariant<T>> action) {
+		store.whenElementAdded(asKnownVariant(action));
+	}
+
+	public static <T extends Variant> Action<Value<NokeeMap.Entry<VariantIdentifier<T>, T>>> asKnownVariant(Action<? super KnownVariant<T>> action) {
+		return new AsKnownVariantAction<>(action);
+	}
+
+	@EqualsAndHashCode
+	private static final class AsKnownVariantAction<T extends Variant> implements Action<Value<NokeeMap.Entry<VariantIdentifier<T>, T>>> {
+		private final Action<? super KnownVariant<T>> action;
+
+		public AsKnownVariantAction(Action<? super KnownVariant<T>> action) {
+			this.action = action;
+		}
+
+		@Override
+		public void execute(Value<NokeeMap.Entry<VariantIdentifier<T>, T>> t) {
+			action.execute(new KnownVariant<>(t.get().getKey(), t.get().getValue()));
+		}
+
+		@Override
+		public String toString() {
+			return "VariantViewImpl.asKnownVariant(" + action + ")";
+		}
+	}
+
+	public static <T extends Variant> Action<NokeeMap.Entry<VariantIdentifier<T>, T>> configureValue(Action<? super T> action) {
+		return new ConfigureValueAction<>(action);
+	}
+
+	@EqualsAndHashCode
+	private static final class ConfigureValueAction<T extends Variant> implements Action<NokeeMap.Entry<VariantIdentifier<T>, T>> {
+		private final Action<? super T> action;
+
+		public ConfigureValueAction(Action<? super T> action) {
+			this.action = action;
+		}
+
+		@Override
+		public void execute(NokeeMap.Entry<VariantIdentifier<T>, T> t) {
+			t.getValue().mapInPlace(configureInPlace(action));
+		}
+
+		@Override
+		public String toString() {
+			return "VariantViewImpl.configureValue(" + action + ")";
+		}
+	}
+
+	public static <T extends Variant> Spec<NokeeMap.Entry<VariantIdentifier<T>, T>> byType(Class<? extends T> type) {
+		return new ByTypeSpec<>(type);
+	}
+
+	@EqualsAndHashCode
+	private static final class ByTypeSpec<T extends Variant> implements Spec<NokeeMap.Entry<VariantIdentifier<T>, T>> {
+		private final Class<? extends T> type;
+
+		public ByTypeSpec(Class<? extends T> type) {
+			this.type = type;
+		}
+
+		@Override
+		public boolean isSatisfiedBy(NokeeMap.Entry<VariantIdentifier<T>, T> t) {
+			return type.isAssignableFrom(t.getValue().getType());
+		}
+
+		@Override
+		public String toString() {
+			return "VariantViewImpl.byType(" + type.getSimpleName() + ")";
+		}
 	}
 }
--- subprojects/platform-base/src/test/groovy/dev/nokee/platform/base/internal/ViewImplTest.groovy
+++ subprojects/platform-base/src/test/groovy/dev/nokee/platform/base/internal/ViewImplTest.groovy
@@ -1,17 +1,34 @@
 package dev.nokee.platform.base.internal
 
 import dev.nokee.model.internal.NokeeCollection
+import dev.nokee.utils.SpecUtils
+import org.gradle.api.Action
 import spock.lang.Subject
 
 @Subject(ViewImpl)
 class ViewImplTest extends AbstractViewImplTest {
 	@Override
-	protected newSubject(NokeeCollection<?> collection) {
+	protected newSubject(NokeeCollection collection) {
 		return new ViewImpl<>(collection)
 	}
 
 	@Override
 	protected Class<?> getViewImplementationType() {
 		return ViewImpl
+	}
+
+	@Override
+	protected def valueOf(String value) {
+		return value
+	}
+
+	@Override
+	protected values(Action action) {
+		return action
+	}
+
+	@Override
+	protected byType(Class type) {
+		return SpecUtils.byType(type)
 	}
 }
