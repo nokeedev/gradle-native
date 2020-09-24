d987964e214856b5b008cb8807fa391ec2df805b
Daniel Lacasse <daniel@nokee.dev>
2020-09-24 19:43:39 +0200

    Make Nokee aware of the different methods of attaching a display name to property
    We are not using ObjectFactory anymore, it's just one of the down side.
    We could get away by not configuring the display name but it gives a
    better user experience.

--- subprojects/core-utils/src/main/java/dev/nokee/utils/ConfigureUtils.java
+++ subprojects/core-utils/src/main/java/dev/nokee/utils/ConfigureUtils.java
@@ -1,14 +1,16 @@
 package dev.nokee.utils;
 
-import org.gradle.api.internal.provider.AbstractProperty;
+import lombok.val;
 import org.gradle.api.internal.provider.PropertyInternal;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.provider.SetProperty;
 import org.gradle.internal.Describables;
+import org.gradle.internal.DisplayName;
 
-import java.util.List;
-import java.util.Set;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
+import java.util.function.BiConsumer;
 
 public final class ConfigureUtils {
 	private ConfigureUtils() {}
@@ -56,7 +58,7 @@ public final class ConfigureUtils {
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T> Property<T> configureDisplayName(Property<T> self, String propertyName) {
-		((AbstractProperty<T>) self).attachDisplayName(Describables.of("property '" + propertyName + "'"));
+		attachDisplayName(self, Describables.of("property '" + propertyName + "'"));
 		return self;
 	}
 
@@ -70,7 +72,7 @@ public final class ConfigureUtils {
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T> SetProperty<T> configureDisplayName(SetProperty<T> self, String propertyName) {
-		((AbstractProperty<Set<T>>) self).attachDisplayName(Describables.of("property '" + propertyName + "'"));
+		attachDisplayName(self, Describables.of("property '" + propertyName + "'"));
 		return self;
 	}
 
@@ -84,7 +86,51 @@ public final class ConfigureUtils {
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T> ListProperty<T> configureDisplayName(ListProperty<T> self, String propertyName) {
-		((AbstractProperty<List<T>>) self).attachDisplayName(Describables.of("property '" + propertyName + "'"));
+		attachDisplayName(self, Describables.of("property '" + propertyName + "'"));
 		return self;
+	}
+
+	private static BiConsumer<Object, DisplayName> attachDisplayName;
+	private static BiConsumer<Object, DisplayName> ATTACH_DISPLAY_NAME_IDENTITY = (obj, arg) -> {};
+	private static void attachDisplayName(Object obj, DisplayName displayName) {
+		if (attachDisplayName == null) {
+			attachDisplayName = findAttachOwnerMethod().orElseGet(() -> findAttachDisplayNameMethod().orElse(ATTACH_DISPLAY_NAME_IDENTITY));
+		}
+		attachDisplayName.accept(obj, displayName);
+	}
+
+	private static Optional<BiConsumer<Object, DisplayName>> findAttachDisplayNameMethod() {
+		try {
+			val OwnerAware = Class.forName("org.gradle.api.internal.provider.OwnerAware");
+			val method = OwnerAware.getMethod("attachDisplayName", DisplayName.class);
+			return Optional.of((obj, arg) -> {
+				try {
+					method.invoke(obj, arg);
+				} catch (IllegalAccessException | InvocationTargetException e) {
+					// Something is wrong so let's ignore any more display name configuration
+					attachDisplayName = ATTACH_DISPLAY_NAME_IDENTITY;
+				}
+			});
+		} catch (NoSuchMethodException | ClassNotFoundException e) {
+			return Optional.empty();
+		}
+	}
+
+	private static Optional<BiConsumer<Object, DisplayName>> findAttachOwnerMethod() {
+		try {
+			val OwnerAware = Class.forName("org.gradle.internal.state.OwnerAware");
+			val ModelObject = Class.forName("org.gradle.internal.state.ModelObject");
+			val method = OwnerAware.getMethod("attachOwner", ModelObject, DisplayName.class);
+			return Optional.of((obj, arg) -> {
+				try {
+					method.invoke(obj, null, arg);
+				} catch (IllegalAccessException | InvocationTargetException e) {
+					// Something is wrong so let's ignore any more display name configuration
+					attachDisplayName = ATTACH_DISPLAY_NAME_IDENTITY;
+				}
+			});
+		} catch (NoSuchMethodException | ClassNotFoundException e) {
+			return Optional.empty();
+		}
 	}
 }
