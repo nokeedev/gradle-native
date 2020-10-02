f22879d6b4e5492b07bf65344846052d62ff84b2
Daniel Lacasse <daniel@nokee.dev>
2020-10-02 09:27:32 -0400

    Configure development binary for JNI library

--- subprojects/platform-jni/src/main/java/dev/nokee/platform/jni/internal/JniLibraryInternal.java
+++ subprojects/platform-jni/src/main/java/dev/nokee/platform/jni/internal/JniLibraryInternal.java
@@ -63,6 +63,7 @@ public class JniLibraryInternal extends BaseVariant implements JniLibrary, Varia
 
 		parentSources.all(sources::add);
 
+		getDevelopmentBinary().convention(getProviders().provider(() -> getBinaryCollection().iterator().next()));
 		getBinaryCollection().configureEach(parentBinaries::add);
 		getResourcePath().convention(getProviders().provider(() -> names.getResourcePath(groupId)));
 	}
