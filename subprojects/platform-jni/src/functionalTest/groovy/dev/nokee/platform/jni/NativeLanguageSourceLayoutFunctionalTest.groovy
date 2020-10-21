f7d7fc94265316862b544981c19b797e300b80a2
Daniel Lacasse <daniel@nokee.dev>
2020-10-21 16:44:11 -0400

    Add source layout functional test for JNI libraries

---
+++ subprojects/platform-jni/src/functionalTest/groovy/dev/nokee/platform/jni/NativeLanguageSourceLayoutFunctionalTest.groovy
@@ -0,0 +1,229 @@
+package dev.nokee.platform.jni
+
+import dev.gradleplugins.fixtures.sources.SourceElement
+import dev.gradleplugins.integtests.fixtures.nativeplatform.RequiresInstalledToolChain
+import dev.gradleplugins.integtests.fixtures.nativeplatform.ToolChainRequirement
+import dev.nokee.fixtures.AbstractNativeLanguageSourceLayoutFunctionalTest
+import dev.nokee.language.c.CTaskNames
+import dev.nokee.language.cpp.CppTaskNames
+import dev.nokee.language.objectivec.ObjectiveCTaskNames
+import dev.nokee.language.objectivecpp.ObjectiveCppTaskNames
+import dev.nokee.platform.jni.fixtures.*
+import spock.lang.Requires
+import spock.util.environment.OperatingSystem
+
+class JavaCJniLibraryNativeLanguageSourceLayoutFunctionalTest extends AbstractNativeLanguageSourceLayoutFunctionalTest implements CTaskNames, JavaJniTaskNames {
+	@Override
+	protected SourceElement getComponentUnderTest() {
+		return new JavaJniCGreeterLib('jni-library')
+	}
+
+	@Override
+	protected void makeSingleProject() {
+		settingsFile << '''
+			rootProject.name = 'jni-library'
+		'''
+		buildFile << '''
+			plugins {
+				id 'java'
+				id 'dev.nokee.jni-library'
+				id 'dev.nokee.c-language'
+			}
+		'''
+	}
+
+	@Override
+	protected void makeProjectWithLibrary() {
+		settingsFile << '''
+			rootProject.name = 'jni-library'
+			include 'library'
+		'''
+		buildFile << '''
+			plugins {
+				id 'java'
+				id 'dev.nokee.jni-library'
+				id 'dev.nokee.c-language'
+			}
+
+			library {
+				dependencies {
+					nativeImplementation project(':library')
+				}
+			}
+		'''
+		file('library', buildFileName) << '''
+			plugins {
+				id 'dev.nokee.c-library'
+			}
+		'''
+	}
+}
+
+class JavaCppJniLibraryNativeLanguageSourceLayoutFunctionalTest extends AbstractNativeLanguageSourceLayoutFunctionalTest implements CppTaskNames, JavaJniTaskNames {
+	@Override
+	protected SourceElement getComponentUnderTest() {
+		return new JavaJniCppGreeterLib('jni-library')
+	}
+
+	@Override
+	protected void makeSingleProject() {
+		settingsFile << '''
+			rootProject.name = 'jni-library'
+		'''
+		buildFile << '''
+			plugins {
+				id 'java'
+				id 'dev.nokee.jni-library'
+				id 'dev.nokee.cpp-language'
+			}
+		'''
+	}
+
+	@Override
+	protected void makeProjectWithLibrary() {
+		settingsFile << '''
+			rootProject.name = 'jni-library'
+			include 'library'
+		'''
+		buildFile << '''
+			plugins {
+				id 'java'
+				id 'dev.nokee.jni-library'
+				id 'dev.nokee.cpp-language'
+			}
+
+			library {
+				dependencies {
+					nativeImplementation project(':library')
+				}
+			}
+		'''
+		file('library', buildFileName) << '''
+			plugins {
+				id 'dev.nokee.cpp-library'
+			}
+		'''
+	}
+}
+
+@RequiresInstalledToolChain(ToolChainRequirement.GCC_COMPATIBLE)
+@Requires({!OperatingSystem.current.windows})
+class JavaObjectiveCJniLibraryNativeLanguageSourceLayoutFunctionalTest extends AbstractNativeLanguageSourceLayoutFunctionalTest implements ObjectiveCTaskNames, JavaJniTaskNames {
+	@Override
+	protected SourceElement getComponentUnderTest() {
+		return new JavaJniObjectiveCGreeterLib('jni-library')
+	}
+
+	@Override
+	protected void makeSingleProject() {
+		settingsFile << '''
+			rootProject.name = 'jni-library'
+		'''
+		buildFile << '''
+			plugins {
+				id 'java'
+				id 'dev.nokee.jni-library'
+				id 'dev.nokee.objective-c-language'
+			}
+
+			tasks.withType(LinkSharedLibrary).configureEach {
+				linkerArgs.add('-lobjc')
+			}
+		'''
+	}
+
+	@Override
+	protected void makeProjectWithLibrary() {
+		settingsFile << '''
+			rootProject.name = 'jni-library'
+			include 'library'
+		'''
+		buildFile << '''
+			plugins {
+				id 'java'
+				id 'dev.nokee.jni-library'
+				id 'dev.nokee.objective-c-language'
+			}
+
+			library {
+				dependencies {
+					nativeImplementation project(':library')
+				}
+			}
+
+			tasks.withType(LinkSharedLibrary).configureEach {
+				linkerArgs.add('-lobjc')
+			}
+		'''
+		file('library', buildFileName) << '''
+			plugins {
+				id 'dev.nokee.objective-c-library'
+			}
+
+			tasks.withType(LinkSharedLibrary).configureEach {
+				linkerArgs.add('-lobjc')
+			}
+		'''
+	}
+}
+
+@RequiresInstalledToolChain(ToolChainRequirement.GCC_COMPATIBLE)
+@Requires({!OperatingSystem.current.windows})
+class JavaObjectiveCppJniLibraryNativeLanguageSourceLayoutFunctionalTest extends AbstractNativeLanguageSourceLayoutFunctionalTest implements ObjectiveCppTaskNames, JavaJniTaskNames {
+	@Override
+	protected SourceElement getComponentUnderTest() {
+		return new JavaJniObjectiveCppGreeterLib('jni-library')
+	}
+
+	@Override
+	protected void makeSingleProject() {
+		settingsFile << '''
+			rootProject.name = 'jni-library'
+		'''
+		buildFile << '''
+			plugins {
+				id 'java'
+				id 'dev.nokee.jni-library'
+				id 'dev.nokee.objective-cpp-language'
+			}
+
+			tasks.withType(LinkSharedLibrary).configureEach {
+				linkerArgs.add('-lobjc')
+			}
+		'''
+	}
+
+	@Override
+	protected void makeProjectWithLibrary() {
+		settingsFile << '''
+			rootProject.name = 'jni-library'
+			include 'library'
+		'''
+		buildFile << '''
+			plugins {
+				id 'java'
+				id 'dev.nokee.jni-library'
+				id 'dev.nokee.objective-cpp-language'
+			}
+
+			library {
+				dependencies {
+					nativeImplementation project(':library')
+				}
+			}
+
+			tasks.withType(LinkSharedLibrary).configureEach {
+				linkerArgs.add('-lobjc')
+			}
+		'''
+		file('library', buildFileName) << '''
+			plugins {
+				id 'dev.nokee.objective-cpp-library'
+			}
+
+			tasks.withType(LinkSharedLibrary).configureEach {
+				linkerArgs.add('-lobjc')
+			}
+		'''
+	}
+}
