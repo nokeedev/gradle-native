fce91eb955ec8c698480c84d5a1d2b48fe236e79
Daniel Lacasse <daniel@nokee.dev>
2020-10-23 13:40:44 -0400

    Strongly type identifier of known components

--- subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/components/KnownComponent.java
+++ subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/components/KnownComponent.java
@@ -8,7 +8,15 @@ import org.gradle.api.provider.Provider;
 
 @ToString
 public final class KnownComponent<T extends Component> extends AbstractKnownDomainObject<Component, T> {
+	private final ComponentIdentifier<T> identifier;
+
 	KnownComponent(ComponentIdentifier<T> identifier, Provider<T> provider, ComponentConfigurer configurer) {
 		super(identifier, provider, configurer);
+		this.identifier = identifier;
+	}
+
+	@Override
+	public ComponentIdentifier<T> getIdentifier() {
+		return identifier;
 	}
 }
