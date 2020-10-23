0a5962ce6ab1f62714c1e726ff2b171bb23f0e74
Daniel Lacasse <daniel@nokee.dev>
2020-10-23 16:58:55 -0400

    Add binding registration capability to polymorphic instantiator

--- subprojects/core-model/src/main/java/dev/nokee/model/DomainObjectFactoryRegistry.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/DomainObjectFactoryRegistry.java
@@ -3,5 +3,7 @@ package dev.nokee.model;
 public interface DomainObjectFactoryRegistry<T> {
     <U extends T> void registerFactory(Class<U> type, DomainObjectFactory<? extends U> factory);
 
+	<U extends T> void registerBinding(Class<U> type, Class<? extends U> implementationType);
+
 	<U extends T> void registerFactoryIfAbsent(Class<U> type, DomainObjectFactory<? extends U> factory);
 }
--- subprojects/core-model/src/main/java/dev/nokee/model/internal/AbstractPolymorphicDomainObjectInstantiator.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/AbstractPolymorphicDomainObjectInstantiator.java
@@ -3,6 +3,7 @@ package dev.nokee.model.internal;
 import com.google.common.collect.ImmutableSet;
 import dev.nokee.model.DomainObjectFactory;
 import dev.nokee.model.DomainObjectIdentifier;
+import lombok.val;
 import org.gradle.api.InvalidUserDataException;
 
 import java.util.*;
@@ -19,22 +20,26 @@ public abstract class AbstractPolymorphicDomainObjectInstantiator<T> implements 
 
 	@Override
 	public <U extends T> void registerFactory(Class<U> type, DomainObjectFactory<? extends U> factory) {
-		if (!baseType.isAssignableFrom(type)) {
-			String message = String.format("Cannot register a factory for type %s because it is not a subtype of container element type %s.", type.getSimpleName(), baseType.getSimpleName());
-			throw new IllegalArgumentException(message);
-		}
-		if (factories.containsKey(type)) {
-			throw new RuntimeException(String.format("Cannot register a factory for type %s because a factory for this type is already registered.", type.getSimpleName()));
-		}
+		val assertRegisteringType = registering(type);
+		assertRegisteringType.isSubTypeOfBaseType();
+		assertRegisteringType.isNotAlreadyRegistered();
 		factories.put(type, factory);
 	}
 
 	@Override
+	public <U extends T> void registerBinding(Class<U> type, Class<? extends U> implementationType) {
+		val assertBindingType = binding(type);
+		assertBindingType.isSubTypeOfBaseType();
+		assertBindingType.isNotBindingToItself(implementationType);
+		assertBindingType.isSuperTypeOf(implementationType);
+		assertBindingType.isNotAlreadyRegistered();
+		assertBindingType.isBindingToKnownType(implementationType);
+		factories.put(type, factories.get(implementationType));
+	}
+
+	@Override
 	public <U extends T> void registerFactoryIfAbsent(Class<U> type, DomainObjectFactory<? extends U> factory) {
-		if (!baseType.isAssignableFrom(type)) {
-			String message = String.format("Cannot register a factory for type %s because it is not a subtype of container element type %s.", type.getSimpleName(), baseType.getSimpleName());
-			throw new IllegalArgumentException(message);
-		}
+		registering(type).isSubTypeOfBaseType();
 		if (!factories.containsKey(type)) {
 			factories.put(type, factory);
 		}
@@ -77,5 +82,73 @@ public abstract class AbstractPolymorphicDomainObjectInstantiator<T> implements 
 
 	private static final class NoFactoryRegisteredForTypeException extends RuntimeException {
 		public NoFactoryRegisteredForTypeException() {}
+	}
+
+	private BindingChecks binding(Class<?> bindingType) {
+		return new BindingChecks(bindingType);
+	}
+
+	private RegisteringChecks registering(Class<?> registeringType) {
+		return new RegisteringChecks(registeringType);
+	}
+
+	private final class RegisteringChecks {
+		private final Class<?> registeringType;
+
+		RegisteringChecks(Class<?> registeringType) {
+			this.registeringType = registeringType;
+		}
+
+		public void isSubTypeOfBaseType() {
+			if (!baseType.isAssignableFrom(registeringType)) {
+				String message = String.format("Cannot register a factory for type %s because it is not a subtype of type %s.", registeringType.getSimpleName(), baseType.getSimpleName());
+				throw new IllegalArgumentException(message);
+			}
+		}
+
+		public void isNotAlreadyRegistered() {
+			if (factories.containsKey(registeringType)) {
+				throw new RuntimeException(String.format("Cannot register a factory for type %s because a factory for this type is already registered.", registeringType.getSimpleName()));
+			}
+		}
+	}
+
+	private final class BindingChecks {
+		private final Class<?> bindingType;
+
+		BindingChecks(Class<?> bindingType) {
+			this.bindingType = bindingType;
+		}
+
+		void isNotBindingToItself(Class<?> implementationType) {
+			if (bindingType.equals(implementationType)) {
+				throw new IllegalArgumentException(String.format("Cannot bind type %s to itself.", bindingType.getSimpleName()));
+			}
+		}
+
+		void isSuperTypeOf(Class<?> implementationType) {
+			if (!bindingType.isAssignableFrom(implementationType)) {
+				throw new IllegalArgumentException(String.format("Cannot bind type %s because it is not a supertype of type %s.", bindingType.getSimpleName(), implementationType.getSimpleName()));
+			}
+		}
+
+		void isNotAlreadyRegistered() {
+			if (factories.containsKey(bindingType)) {
+				throw new RuntimeException(String.format("Cannot bind type %s because a factory for this type is already registered.", bindingType.getSimpleName()));
+			}
+		}
+
+		void isBindingToKnownType(Class<?> implementationType) {
+			if (!factories.containsKey(implementationType)) {
+				throw new RuntimeException(String.format("Cannot bind type %s because a factory for type %s is not known to %s. Known types are: %s", bindingType.getSimpleName(), implementationType.getSimpleName(), displayName, getSupportedTypeNames()));
+			}
+		}
+
+		public void isSubTypeOfBaseType() {
+			if (!baseType.isAssignableFrom(bindingType)) {
+				String message = String.format("Cannot bind type %s because it is not a subtype of type %s.", bindingType.getSimpleName(), baseType.getSimpleName());
+				throw new IllegalArgumentException(message);
+			}
+		}
 	}
 }
