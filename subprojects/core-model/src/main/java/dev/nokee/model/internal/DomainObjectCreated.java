9b58816f10c8016c2c9cda1e2fdf88844a3e855d
Daniel Lacasse <daniel@nokee.dev>
2020-10-25 19:27:50 -0400

    Assert identifier type matches created object

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/DomainObjectCreated.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/DomainObjectCreated.java
@@ -1,10 +1,25 @@
 package dev.nokee.model.internal;
 
 import dev.nokee.model.DomainObjectIdentifier;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 import lombok.Value;
 
-@Value
+@ToString
+@EqualsAndHashCode
 public class DomainObjectCreated<T> implements DomainObjectEvent {
-	DomainObjectIdentifier identifier;
-	T object;
+	@Getter private final DomainObjectIdentifier identifier;
+	@Getter private final T object;
+
+	public DomainObjectCreated(TypeAwareDomainObjectIdentifier<?> identifier, T object) {
+		assert identifier.getType().isInstance(object);
+		this.identifier = identifier;
+		this.object = object;
+	}
+
+	public DomainObjectCreated(DomainObjectIdentifier identifier, T object) {
+		this.identifier = identifier;
+		this.object = object;
+	}
 }
