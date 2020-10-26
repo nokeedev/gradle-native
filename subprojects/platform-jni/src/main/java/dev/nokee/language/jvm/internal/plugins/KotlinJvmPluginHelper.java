2113efd517bf0918ea40764c3e5d02b164b845f6
Daniel Lacasse <daniel@nokee.dev>
2020-10-26 16:52:31 -0400

    Prevent no class definition exception

--- subprojects/platform-jni/src/main/java/dev/nokee/language/jvm/internal/plugins/KotlinJvmPluginHelper.java
+++ subprojects/platform-jni/src/main/java/dev/nokee/language/jvm/internal/plugins/KotlinJvmPluginHelper.java
@@ -1,12 +1,14 @@
 package dev.nokee.language.jvm.internal.plugins;
 
+import lombok.val;
 import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.internal.plugins.DslObject;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetContainer;
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet;
+
+import java.lang.reflect.InvocationTargetException;
 
 public final class KotlinJvmPluginHelper {
 	private KotlinJvmPluginHelper() {}
@@ -14,12 +16,19 @@ public final class KotlinJvmPluginHelper {
 	public static void whenSourceSetKnown(Project project, String name, Action<? super SourceDirectorySet> action) {
 		project.getExtensions().getByType(SourceSetContainer.class).configureEach(sourceSet -> {
 			if (sourceSet.getName().equals(name)) {
-				action.execute(asKotlinSourceSet(sourceSet).getKotlin());
+				action.execute(asKotlinSourceSet(sourceSet));
 			}
 		});
 	}
 
-	private static KotlinSourceSet asKotlinSourceSet(SourceSet sourceSet) {
-		return (KotlinSourceSet) new DslObject(sourceSet).getConvention().getPlugins().get("kotlin");
+	private static SourceDirectorySet asKotlinSourceSet(SourceSet sourceSet) {
+		try {
+			val kotlinSourceSet = new DslObject(sourceSet).getConvention().getPlugins().get("kotlin");
+			val DefaultKotlinSourceSet = kotlinSourceSet.getClass();
+			val getKotlin = DefaultKotlinSourceSet.getMethod("getKotlin");
+			return (SourceDirectorySet) getKotlin.invoke(kotlinSourceSet);
+		} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
