1681bc310f842c80233faacf3fc7e7cfa26171e6
Daniel Lacasse <daniel@nokee.dev>
2020-10-26 15:58:27 -0400

    Delay configuration of the Gradle JVM source set until after Nokee source set
    This is fragile as it's order dependent and will change when we defer
    Nokee source set creation.

--- subprojects/platform-native/src/testFixtures/groovy/dev/nokee/fixtures/AbstractNativeLanguageSourceLayoutFunctionalTest.groovy
+++ subprojects/platform-native/src/testFixtures/groovy/dev/nokee/fixtures/AbstractNativeLanguageSourceLayoutFunctionalTest.groovy
@@ -175,13 +175,6 @@ abstract class AbstractNativeLanguageSourceLayoutFunctionalTest extends Abstract
 
 	protected String configureSourcesAsConvention(String dsl = componentUnderTestDsl) {
 		return """
-			pluginManager.withPlugin('java') {
-				sourceSets.main.java {
-					setSrcDirs(['srcs'])
-					filter.include('**/*.java')
-				}
-			}
-
 			import ${CHeaderSet.canonicalName}
 			import ${CppHeaderSet.canonicalName}
 
@@ -197,19 +190,18 @@ abstract class AbstractNativeLanguageSourceLayoutFunctionalTest extends Abstract
 					from('srcs')
 				}
 			}
-		"""
-	}
 
-	protected String configureSourcesAsExplicitFiles() {
-		return """
-			// We don't test Gradle Java plugin
 			pluginManager.withPlugin('java') {
 				sourceSets.main.java {
 					setSrcDirs(['srcs'])
 					filter.include('**/*.java')
 				}
 			}
+		"""
+	}
 
+	protected String configureSourcesAsExplicitFiles() {
+		return """
 			import ${CHeaderSet.canonicalName}
 			import ${CppHeaderSet.canonicalName}
 
@@ -223,6 +215,14 @@ abstract class AbstractNativeLanguageSourceLayoutFunctionalTest extends Abstract
 				}
 				sources.configureEach({ !(it instanceof ${CHeaderSet.simpleName} || it instanceof ${CppHeaderSet.simpleName}) }) {
 					${ofSources(componentUnderTest).files.collect { "from('srcs/${it.name}')" }.join('\n')}
+				}
+			}
+
+			// We don't test Gradle Java plugin
+			pluginManager.withPlugin('java') {
+				sourceSets.main.java {
+					setSrcDirs(['srcs'])
+					filter.include('**/*.java')
 				}
 			}
 		"""
