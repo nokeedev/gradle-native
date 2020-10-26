cc399e527e6567c049d80c0345b8b72eef9e43e9
Daniel Lacasse <daniel@nokee.dev>
2020-10-26 14:34:15 -0400

    Ignore JNI library Xcode IDE plugin

--- subprojects/ide-xcode/src/functionalTest/groovy/dev/nokee/ide/xcode/XcodeIdeJavaNativeInterfaceComponentFunctionalTest.groovy
+++ subprojects/ide-xcode/src/functionalTest/groovy/dev/nokee/ide/xcode/XcodeIdeJavaNativeInterfaceComponentFunctionalTest.groovy
@@ -2,7 +2,10 @@ package dev.nokee.ide.xcode
 
 import dev.gradleplugins.fixtures.sources.SourceElement
 import dev.nokee.platform.jni.fixtures.JavaJniCppGreeterLib
+import org.junit.Assume
+import spock.lang.Ignore
 
+@Ignore
 class XcodeIdeJavaNativeInterfaceLibraryFunctionalTest extends AbstractXcodeIdeNativeComponentPluginFunctionalTest {
 	@Override
 	protected void makeSingleProject() {
