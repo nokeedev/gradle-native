ebd4553b02216b2cfa81dc92a76893a0b933a095
Daniel Lacasse <daniel@nokee.dev>
2020-11-11 19:33:07 -0500

    Add defer utility converting a runnable into a provider returning empty list

--- subprojects/core-utils/src/main/java/dev/nokee/utils/DeferUtils.java
+++ subprojects/core-utils/src/main/java/dev/nokee/utils/DeferUtils.java
@@ -1,7 +1,11 @@
 package dev.nokee.utils;
 
 import lombok.EqualsAndHashCode;
+import org.gradle.api.provider.Provider;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.function.Supplier;
 
 import static java.util.Objects.requireNonNull;
@@ -32,6 +36,32 @@ public final class DeferUtils {
 		@Override
 		public String toString() {
 			return supplier.get();
+		}
+	}
+
+	/**
+	 * Returns a {@link Provider} instance that runs the specified {@link Runnable} when resolved.
+	 * The provider always run the {@link Runnable} and return an empty list.
+	 *
+	 * @param delegate a runnable to run upon resolving the provider.
+	 * @param <S> the element type of returned list
+	 * @return a provider that runs the specified runnable and return a empty list, never null.
+	 */
+	public static <S> Provider<List<S>> executes(Runnable delegate) {
+		return ProviderUtils.supplied(new DeferRunnableViaEmptyListReturningCallable<>(delegate));
+	}
+
+	private static final class DeferRunnableViaEmptyListReturningCallable<T> implements Callable<List<T>> {
+		private final Runnable delegate;
+
+		public DeferRunnableViaEmptyListReturningCallable(Runnable delegate) {
+			this.delegate = delegate;
+		}
+
+		@Override
+		public List<T> call() throws Exception {
+			delegate.run();
+			return Collections.emptyList();
 		}
 	}
 }
