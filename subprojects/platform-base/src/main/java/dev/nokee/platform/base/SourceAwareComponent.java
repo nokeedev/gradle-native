3d1e0a4ee8b38af1ac30a703ea76659ebbd4d4e7
Daniel Lacasse <daniel@nokee.dev>
2020-11-17 16:57:19 -0500

    Allow source view configuration via action

--- subprojects/platform-base/src/main/java/dev/nokee/platform/base/SourceAwareComponent.java
+++ subprojects/platform-base/src/main/java/dev/nokee/platform/base/SourceAwareComponent.java
@@ -2,7 +2,19 @@ package dev.nokee.platform.base;
 
 import dev.nokee.language.base.LanguageSourceSet;
 import dev.nokee.language.base.LanguageSourceSetView;
+import groovy.lang.Closure;
+import groovy.lang.DelegatesTo;
+import org.gradle.api.Action;
+import org.gradle.util.ConfigureUtil;
 
 public interface SourceAwareComponent {
 	LanguageSourceSetView<LanguageSourceSet> getSources();
+
+	default void sources(Action<? super LanguageSourceSetView<LanguageSourceSet>> action) {
+		action.execute(getSources());
+	}
+
+	default void sources(@DelegatesTo(value = LanguageSourceSetView.class, strategy = Closure.DELEGATE_FIRST) Closure<Void> closure) {
+		ConfigureUtil.configureUsing(closure).execute(getSources());
+	}
 }
