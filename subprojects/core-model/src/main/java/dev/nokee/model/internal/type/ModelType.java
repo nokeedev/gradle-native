6ef54d291ba9a135d8f8641ba402c6a96154f99c
Daniel Lacasse <daniel@nokee.dev>
2020-12-05 15:34:44 -0500

    Add type compatibility check and toString for ModelType

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/type/ModelType.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/type/ModelType.java
@@ -11,22 +11,31 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode
 @SuppressWarnings("UnstableApiUsage")
 public final class ModelType<T> {
-    private final TypeToken<T> type;
+	private final TypeToken<T> type;
 
-    private ModelType(TypeToken<T> type) {
-        this.type = type;
-    }
+	private ModelType(TypeToken<T> type) {
+		this.type = type;
+	}
 
-    public Class<? super T> getRawType() {
-        return type.getRawType();
-    }
+	public Class<? super T> getRawType() {
+		return type.getRawType();
+	}
 
-    @SuppressWarnings("unchecked")
-    public Class<T> getConcreteType() {
-        return (Class<T>) type.getRawType();
-    }
+	@SuppressWarnings("unchecked")
+	public Class<T> getConcreteType() {
+		return (Class<T>) type.getRawType();
+	}
 
-    public static <T> ModelType<T> of(Class<T> type) {
-        return new ModelType<>(TypeToken.of(type));
-    }
+	public boolean isAssignableFrom(ModelType<?> type) {
+		return this == type || this.getConcreteType().isAssignableFrom(type.getConcreteType());
+	}
+
+	@Override
+	public String toString() {
+		return type.getRawType().toString();
+	}
+
+	public static <T> ModelType<T> of(Class<T> type) {
+		return new ModelType<>(TypeToken.of(type));
+	}
 }
--- subprojects/core-model/src/test/groovy/dev/nokee/model/internal/type/ModelTypeTest.java
+++ subprojects/core-model/src/test/groovy/dev/nokee/model/internal/type/ModelTypeTest.java
@@ -2,45 +2,70 @@ package dev.nokee.model.internal.type;
 
 import com.google.common.testing.EqualsTester;
 import com.google.common.testing.NullPointerTester;
+import lombok.val;
 import org.junit.jupiter.api.Test;
 
 import static dev.nokee.model.internal.type.ModelType.of;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasToString;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ModelTypeTest {
-    @Test
-    @SuppressWarnings("UnstableApiUsage")
-    void canEquals() {
-        new EqualsTester()
-                .addEqualityGroup(of(String.class), of(String.class))
-                .addEqualityGroup(of(Integer.class))
-                .testEquals();
-    }
+	@Test
+	@SuppressWarnings("UnstableApiUsage")
+	void canEquals() {
+		new EqualsTester()
+				.addEqualityGroup(of(String.class), of(String.class))
+				.addEqualityGroup(of(Integer.class))
+				.testEquals();
+	}
 
-    @Test
-    void canAccessRawType() {
-        assertAll(() -> {
-            assertEquals(String.class, of(String.class).getRawType());
-            assertEquals(Integer.class, of(Integer.class).getRawType());
-            assertEquals(MyType.class, of(MyType.class).getRawType());
-        });
-    }
+	@Test
+	void canAccessRawType() {
+		assertAll(() -> {
+			assertEquals(String.class, of(String.class).getRawType());
+			assertEquals(Integer.class, of(Integer.class).getRawType());
+			assertEquals(MyType.class, of(MyType.class).getRawType());
+		});
+	}
 
-    @Test
-    void canAccessConcreteType() {
-        assertAll(() -> {
-            assertEquals(String.class, of(String.class).getConcreteType());
-            assertEquals(Integer.class, of(Integer.class).getConcreteType());
-            assertEquals(MyType.class, of(MyType.class).getConcreteType());
-        });
-    }
+	@Test
+	void canAccessConcreteType() {
+		assertAll(() -> {
+			assertEquals(String.class, of(String.class).getConcreteType());
+			assertEquals(Integer.class, of(Integer.class).getConcreteType());
+			assertEquals(MyType.class, of(MyType.class).getConcreteType());
+		});
+	}
 
-    @Test
-    @SuppressWarnings("UnstableApiUsage")
-    void checkNulls() {
-        new NullPointerTester().testAllPublicStaticMethods(ModelType.class);
-    }
+	@Test
+	@SuppressWarnings("UnstableApiUsage")
+	void checkNulls() {
+		new NullPointerTester().testAllPublicStaticMethods(ModelType.class);
+	}
 
-    interface MyType {}
+	@Test
+	void canCheckAssignableCompatibility() {
+		val myType = of(MyType.class);
+		assertAll(() -> {
+			assertTrue(myType.isAssignableFrom(myType), "same instance should be assignable");
+			assertTrue(of(MyType.class).isAssignableFrom(of(MyType.class)), "same type should be assignable");
+			assertTrue(of(Object.class).isAssignableFrom(of(MyType.class)), "all types are assignable to Object");
+			assertFalse(of(String.class).isAssignableFrom(of(MyType.class)), "unrelated types should not be assignable");
+		});
+	}
+
+	@Test
+	void checkToStringForInterfaceType() {
+		assertThat(of(MyInterfaceType.class), hasToString("interface dev.nokee.model.internal.type.ModelTypeTest$MyInterfaceType"));
+	}
+
+	@Test
+	void checkToStringForClassType() {
+		assertThat(of(MyClassType.class), hasToString("class dev.nokee.model.internal.type.ModelTypeTest$MyClassType"));
+	}
+
+	interface MyType {}
+	interface MyInterfaceType {}
+	static class MyClassType {}
 }
