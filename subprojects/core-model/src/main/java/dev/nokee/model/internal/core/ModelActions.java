4935211ca7bcb2bf9c256b931139175a8f2ece88
Daniel Lacasse <daniel@nokee.dev>
2020-12-07 15:42:21 -0500

    Introduce ModelAction to Gradle action adatper for projection

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelActions.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelActions.java
@@ -1,5 +1,9 @@
 package dev.nokee.model.internal.core;
 
+import dev.nokee.model.internal.type.ModelType;
+import lombok.EqualsAndHashCode;
+import org.gradle.api.Action;
+
 public final class ModelActions {
 	private ModelActions() {}
 
@@ -18,6 +22,31 @@ public final class ModelActions {
 			public String toString() {
 				return "ModelActions.doNothing()";
 			}
+		}
+	}
+
+	public static <T> ModelAction executeUsingProjection(ModelType<T> type, Action<? super T> action) {
+		return new ExecuteUsingProjectionModelAction<>(type, action);
+	}
+
+	@EqualsAndHashCode
+	private static final class ExecuteUsingProjectionModelAction<T> implements ModelAction {
+		private final ModelType<T> type;
+		private final Action<? super T> action;
+
+		private ExecuteUsingProjectionModelAction(ModelType<T> type, Action<? super T> action) {
+			this.type = type;
+			this.action = action;
+		}
+
+		@Override
+		public void execute(ModelNode node) {
+			action.execute(node.get(type));
+		}
+
+		@Override
+		public String toString() {
+			return "ModelActions.executeUsingProjection(" + type + ", " + action + ")";
 		}
 	}
 }
---
+++ subprojects/core-model/src/test/groovy/dev/nokee/model/internal/core/ModelActions_ExecuteUsingProjectionTest.java
@@ -0,0 +1,55 @@
+package dev.nokee.model.internal.core;
+
+import com.google.common.testing.EqualsTester;
+import lombok.val;
+import org.gradle.api.Action;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static dev.nokee.model.internal.core.ModelActions.executeUsingProjection;
+import static dev.nokee.model.internal.core.ModelTestUtils.node;
+import static dev.nokee.model.internal.core.ModelTestUtils.projectionOf;
+import static dev.nokee.model.internal.type.ModelType.of;
+import static dev.nokee.utils.ActionUtils.doNothing;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasToString;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.*;
+
+class ModelActions_ExecuteUsingProjectionTest {
+	@Test
+	void checkToString() {
+		assertThat(executeUsingProjection(of(MyType.class), doNothing()), hasToString("ModelActions.executeUsingProjection(interface dev.nokee.model.internal.core.ModelActions_ExecuteUsingProjectionTest$MyType, ActionUtils.doNothing())"));
+	}
+
+	@Test
+	@SuppressWarnings("UnstableApiUsage")
+	void checkEquals() {
+		new EqualsTester()
+			.addEqualityGroup(executeUsingProjection(of(MyType.class), doNothing()), executeUsingProjection(of(MyType.class), doNothing()))
+			.addEqualityGroup(executeUsingProjection(of(WrongType.class), doNothing()))
+			.addEqualityGroup(executeUsingProjection(of(MyType.class), it -> { /* something */ }))
+			.testEquals();
+	}
+
+	@Test
+	void executeActionWhenProjectionIsAvailable() {
+		@SuppressWarnings("unchecked")
+		val action = (Action<MyType>) Mockito.mock(Action.class);
+		assertDoesNotThrow(() -> executeUsingProjection(of(MyType.class), action).execute(node(projectionOf(MyType.class))));
+		verify(action, times(1)).execute(isA(MyType.class));
+	}
+
+	@Test
+	void throwExceptionWhenProjectIsUnavailable() {
+		@SuppressWarnings("unchecked")
+		val action = (Action<WrongType>) Mockito.mock(Action.class);
+		assertThrows(IllegalStateException.class, () -> executeUsingProjection(of(WrongType.class), action).execute(node(projectionOf(MyType.class))));
+		verify(action, never()).execute(any());
+	}
+
+	interface MyType {}
+	interface WrongType {}
+}
