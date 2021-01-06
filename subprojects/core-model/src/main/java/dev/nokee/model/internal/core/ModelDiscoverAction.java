a71893aabd24b3dc7f20f41d2b179438a9941691
Daniel Lacasse <daniel@nokee.dev>
2021-01-06 12:17:11 -0500

    Allow more actions to be done from a discover action
    Discover action now expose KnownDomainObject for each new projection as
    well as able to get projection as KnownDomainObject to allow mapping and
    mutating for later state.

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelDiscoverAction.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelDiscoverAction.java
@@ -1,21 +1,32 @@
 package dev.nokee.model.internal.core;
 
+import dev.nokee.model.KnownDomainObject;
+import dev.nokee.model.internal.registry.ModelNodeBackedKnownDomainObject;
+import dev.nokee.model.internal.type.ModelType;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
 public abstract class ModelDiscoverAction implements ModelAction {
 	@Override
 	public final void execute(ModelNode node) {
 		// TODO: Should be discovered
 		if (node.getState().equals(ModelNode.State.Registered)) {
-			execute(new Context(node));
+			// NOTE: The contextual node should not be accessed from the action, it's simply for contextualizing the action execution.
+			ModelNodeContext.of(node).execute(() -> execute(new Context(node)));
 		}
 	}
 
 	protected abstract void execute(Context context);
 
-	protected static class Context {
+	public static class Context {
 		private final ModelNode node;
 
 		public Context(ModelNode node) {
 			this.node = node;
+		}
+
+		public ModelPath getPath() {
+			return node.getPath();
 		}
 
 		public Context applyTo(NodeAction action) {
@@ -23,13 +34,13 @@ public abstract class ModelDiscoverAction implements ModelAction {
 			return this;
 		}
 
-		public Context register(NodeRegistration<?> registration) {
-			node.register(registration);
-			return this;
+		public <T> KnownDomainObject<T> register(NodeRegistration<T> registration) {
+			return new ModelNodeBackedKnownDomainObject<>(registration.getDefaultProjectionType(), ModelNodes.of(node.register(registration)));
 		}
 
-		public ModelPath getPath() {
-			return node.getPath();
+		public <T> KnownDomainObject<T> projectionOf(ModelType<T> type) {
+			checkArgument(node.canBeViewedAs(type));
+			return new ModelNodeBackedKnownDomainObject<>(type, node);
 		}
 	}
 }
--- subprojects/core-model/src/test/groovy/dev/nokee/model/internal/core/ModelDiscoverActionTest.java
+++ subprojects/core-model/src/test/groovy/dev/nokee/model/internal/core/ModelDiscoverActionTest.java
@@ -1,18 +1,19 @@
 package dev.nokee.model.internal.core;
 
 import com.google.common.collect.ImmutableList;
-import dev.nokee.model.internal.registry.ModelRegistry;
+import dev.nokee.model.KnownDomainObject;
 import lombok.val;
-import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.commons.lang3.mutable.MutableObject;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import spock.lang.Subject;
 
 import java.util.ArrayList;
 
+import static dev.nokee.internal.testing.ExecuteWith.*;
 import static dev.nokee.model.internal.core.ModelActions.discover;
 import static dev.nokee.model.internal.core.ModelActions.initialize;
 import static dev.nokee.model.internal.core.ModelPath.path;
+import static dev.nokee.model.internal.core.ModelProjections.managed;
 import static dev.nokee.model.internal.core.ModelTestActions.CaptureNodeTransitionAction.realized;
 import static dev.nokee.model.internal.core.ModelTestActions.CaptureNodeTransitionAction.registered;
 import static dev.nokee.model.internal.core.NodePredicate.self;
@@ -20,23 +21,28 @@ import static dev.nokee.model.internal.type.ModelType.of;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Subject(ModelDiscoverAction.class)
 class ModelDiscoverActionTest {
 	@Test
 	void executesOnlyIfNodeStateIsRegistered() {
-		val callCount = new MutableInt();
-		node("foo", discover(context -> callCount.increment()));
-		assertThat(callCount.getValue(), equalTo(1));
+		assertThat(executeWith(consumer(action -> node(discover(action)))),
+			calledOnce());
 	}
 
 	@Test
 	void canRegisterChildNode() {
-		val modelRegistry = Mockito.mock(ModelRegistry.class);
-		node("foo", discover(context -> context.register(NodeRegistration.of("bar", of(MyType.class)))), modelRegistry);
-		verify(modelRegistry, times(1)).register(NodeRegistration.of("bar", of(MyType.class)).scope(path("foo")));
+		val node = node(discover(context -> context.register(NodeRegistration.of("bar", of(MyType.class)))));
+		assertThat(node.hasDescendant("bar"), equalTo(true));
+	}
+
+	@Test
+	void returnKnownDomainObjectWhenRegisteringChildNode() {
+		val capturedValue = new MutableObject<KnownDomainObject<MyType>>();
+		val node = node(discover(context -> capturedValue.setValue(context.register(NodeRegistration.of("bar", of(MyType.class))))));
+		assertThat(capturedValue.getValue().getType(), equalTo(MyType.class));
 	}
 
 	@Test
@@ -55,13 +61,35 @@ class ModelDiscoverActionTest {
 		assertThat(paths, contains(path("foo")));
 	}
 
-	interface MyType {}
-
-	private static ModelNode node(String name, ModelAction action) {
-		return ModelTestUtils.childNode(ModelTestUtils.rootNode(), name, ImmutableList.of(action), builder -> {}).register();
+	@Test
+	void throwsExceptionWhenAccessingUnknownProjection() {
+		assertThrows(IllegalArgumentException.class,
+			() -> node(discover(context -> context.projectionOf(of(MyType.class)))));
 	}
 
-	private static ModelNode node(String name, ModelAction action, ModelRegistry modelRegistry) {
-		return ModelTestUtils.childNode(ModelTestUtils.rootNode(), name, ImmutableList.of(action), builder -> builder.withRegistry(modelRegistry)).register();
+	@Test
+	void canAccessKnownProjection() {
+		val capturedValue = new MutableObject<KnownDomainObject<MyType>>();
+		assertDoesNotThrow(() -> node(
+			initialize(context -> context.withProjection(managed(of(MyType.class)))),
+			discover(context -> capturedValue.setValue(context.projectionOf(of(MyType.class))))));
+		assertThat(capturedValue.getValue().getType(), equalTo(MyType.class));
+	}
+
+	@Test
+	void canAccessTheNodeContextually() {
+		val capturedValue = new MutableObject<ModelNode>();
+		val expected = node(discover(context -> capturedValue.setValue(ModelNodeContext.getCurrentModelNode())));
+		assertThat(capturedValue.getValue(), equalTo(expected));
+	}
+
+	interface MyType {}
+
+	private static ModelNode node(ModelAction... action) {
+		return ModelTestUtils.childNode(ModelTestUtils.rootNode(), "test", ImmutableList.copyOf(action), builder -> {}).register();
+	}
+
+	private static ModelNode node(String name, ModelAction... action) {
+		return ModelTestUtils.childNode(ModelTestUtils.rootNode(), name, ImmutableList.copyOf(action), builder -> {}).register();
 	}
 }
