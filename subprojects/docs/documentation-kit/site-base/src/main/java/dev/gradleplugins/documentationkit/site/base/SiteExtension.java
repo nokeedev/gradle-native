2ebf4e91ff5089f7c8872396fca3521adb65c421
Daniel Lacasse <daniel@nokee.dev>
2021-01-26 11:42:58 -0500

    Allow sites to create symbolic links between folders

--- subprojects/docs/documentation-kit/site-base/src/main/java/dev/gradleplugins/documentationkit/site/base/SiteExtension.java
+++ subprojects/docs/documentation-kit/site-base/src/main/java/dev/gradleplugins/documentationkit/site/base/SiteExtension.java
@@ -2,10 +2,12 @@ package dev.gradleplugins.documentationkit.site.base;
 
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
 
 public interface SiteExtension {
 	Property<String> getHost();
 	ConfigurableFileCollection getSources();
 	DirectoryProperty getDestinationDirectory();
+	MapProperty<String, String> getSymbolicLinks();
 }
