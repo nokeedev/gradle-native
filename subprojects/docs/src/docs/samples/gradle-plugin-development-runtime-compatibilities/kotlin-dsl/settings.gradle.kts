45f7599ce84b5643d5a22e731a96ac8aa9550b4b
Daniel Lacasse <daniel@nokee.dev>
2021-02-15 16:33:37 -0500

    Include plugin management block in Gradle plugin development samples

--- subprojects/docs/src/docs/samples/gradle-plugin-development-runtime-compatibilities/kotlin-dsl/settings.gradle.kts
+++ subprojects/docs/src/docs/samples/gradle-plugin-development-runtime-compatibilities/kotlin-dsl/settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+		maven { url = uri("https://repo.nokeedev.net/release") }
+	}
+}
+
 plugins {
 	id("dev.gradleplugins.gradle-plugin-development") version("1.2")
 }
