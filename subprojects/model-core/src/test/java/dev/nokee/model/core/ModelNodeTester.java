75ec6a8482b0d2ba85ec0beaddd0e39d3d6d6897
Daniel Lacasse <daniel@nokee.dev>
2021-05-19 22:00:56 -0400

    Improve model node and projection all around with test coverage

---
+++ subprojects/model-core/src/test/java/dev/nokee/model/core/ModelNodeTester.java
@@ -0,0 +1,87 @@
+package dev.nokee.model.core;
+
+import lombok.val;
+import org.gradle.api.Named;
+import org.junit.jupiter.api.Test;
+
+import static com.spotify.hamcrest.optional.OptionalMatchers.optionalWithValue;
+import static dev.nokee.internal.testing.GradleNamedMatchers.named;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public interface ModelNodeTester {
+	ModelNode createSubject();
+
+	@Test
+	default void childNodeHasParentNodeReference() {
+		val parent = createSubject();
+		assertThat(parent.newChildNode("test").getParent(), optionalWithValue(equalTo(parent)));
+	}
+
+	@Test
+	default void canCreateChildNode() {
+		val childNode = createSubject().newChildNode("test");
+		assertThat(childNode, notNullValue());
+		assertThat(childNode, named("test"));
+	}
+
+	@Test
+	default void childNodesAreAccessibleFromParent() {
+		val subject = createSubject();
+		val childNode0 = subject.newChildNode("test0");
+		val childNode1 = subject.newChildNode("test1");
+		val childNode2 = subject.newChildNode("test2");
+		assertThat(subject.getChildNodes().collect(toList()), contains(childNode0, childNode1, childNode2));
+	}
+
+	@Test
+	default void noChildNodesOnNewNodes() {
+		assertThat(createSubject().getChildNodes().count(), is(0L));
+	}
+
+	@Test
+	default void noProjectionsOnNewNodes() {
+		assertThat(createSubject().getProjections().count(), is(0L));
+	}
+
+	@Test
+	default void useIdentityToStringWhenNoNamedForChildNodeName() {
+		val subject = createSubject();
+		val childNode = subject.newChildNode(new TestIdentity("bar"));
+		assertThat(childNode, named("bar"));
+	}
+
+	class TestIdentity {
+		private final String name;
+
+		public TestIdentity(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
+
+	@Test
+	default void useIdentityNameForChildNodeName() {
+		val subject = createSubject();
+		val childNode = subject.newChildNode(new NamedTestIdentity("far"));
+		assertThat(childNode, named("far"));
+	}
+
+	class NamedTestIdentity implements Named {
+		private final String name;
+
+		public NamedTestIdentity(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+	}
+}
---
+++ subprojects/model-core/src/test/java/dev/nokee/model/core/ModelProjectionTester.java
@@ -0,0 +1,75 @@
+package dev.nokee.model.core;
+
+import lombok.val;
+import org.gradle.api.Action;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+
+import static dev.nokee.internal.testing.GradleProviderMatchers.absentProvider;
+import static dev.nokee.internal.testing.GradleProviderMatchers.providerOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public interface ModelProjectionTester {
+	ModelProjection createSubject();
+
+	@Test
+	default void canViewAsProjectionType() {
+		assertThat(createSubject().canBeViewedAs(TestProjection.class), is(true));
+	}
+
+	@Test
+	default void canViewAsProjectionBaseType() {
+		assertThat(createSubject().canBeViewedAs(BaseProjection.class), is(true));
+	}
+
+	@Test
+	default void cannotViewAsProjectionImplementationType() {
+		assertThat(createSubject().canBeViewedAs(DefaultTestProjection.class), is(false));
+	}
+
+	@Test
+	default void returnsPresentProviderForViewableType() {
+		assertThat(createSubject().as(TestProjection.class), providerOf(isA(TestProjection.class)));
+	}
+
+	@Test
+	default void returnsAbsentProviderForNonViewableType() {
+		assertThat(createSubject().as(UnknownProjection.class), absentProvider());
+	}
+
+	@Test
+	default void throwsExceptionWhenResolvingProjectionForNonViewableType() {
+		val ex = assertThrows(RuntimeException.class, () -> createSubject().get(UnknownProjection.class));
+		assertThat(ex.getMessage(), is(""));
+	}
+
+	@Test
+	default void canResolveProjectionForViewableType() {
+		assertThat(createSubject().get(TestProjection.class), isA(TestProjection.class));
+	}
+
+	@Test
+	default void canExecuteProjectionWhenRealized() {
+		val action = Mockito.mock(Action.class);
+		val subject = createSubject();
+		subject.whenRealized(TestProjection.class, action);
+		subject.get(TestProjection.class); //realize
+		Mockito.verify(action).execute(ArgumentMatchers.isA(TestProjection.class));
+	}
+
+	@Test
+	default void throwsExceptionWhenConfiguringNonViewableType() {
+		val subject = createSubject();
+		assertThrows(RuntimeException.class, () -> subject.whenRealized(UnknownProjection.class, it -> {})); // TODO Use ActionTestUtils.doSomething()
+	}
+
+	interface BaseProjection {}
+	interface TestProjection extends BaseProjection {}
+	final class DefaultTestProjection implements TestProjection {}
+
+	interface UnknownProjection {}
+}
