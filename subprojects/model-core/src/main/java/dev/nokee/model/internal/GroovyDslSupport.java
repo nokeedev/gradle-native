948bf95bd5bb371778aba44260fda0f4f378b888
Daniel Lacasse <daniel@nokee.dev>
2021-05-30 23:41:24 -0400

    Hide Groovy DSL support implementation

--- subprojects/model-core/src/main/java/dev/nokee/model/internal/GroovyDslSupport.java
+++ subprojects/model-core/src/main/java/dev/nokee/model/internal/GroovyDslSupport.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public final class GroovyDslSupport {
+final class GroovyDslSupport {
 	private static final Object[] EMPTY_ARRAY = {};
 	private final MetaClass metaClass;
 	private final PropertyGetter getter;
