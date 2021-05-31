d5a2348b3ef00d02427017f2fe39b6e3735deb26
Daniel Lacasse <daniel@nokee.dev>
2021-05-30 23:43:18 -0400

    Introduce container bridging convenience method on Nokee extension

--- subprojects/model-core/src/main/java/dev/nokee/model/internal/DefaultNokeeExtension.java
+++ subprojects/model-core/src/main/java/dev/nokee/model/internal/DefaultNokeeExtension.java
@@ -3,22 +3,41 @@ package dev.nokee.model.internal;
 import dev.nokee.model.NokeeExtension;
 import dev.nokee.model.dsl.ModelNode;
 import dev.nokee.model.registry.ModelRegistry;
+import dev.nokee.utils.NamedDomainObjectCollectionUtils;
 import org.gradle.api.Action;
+import org.gradle.api.NamedDomainObjectContainer;
+import org.gradle.api.PolymorphicDomainObjectContainer;
 
 import javax.inject.Inject;
 
+import static dev.nokee.utils.NamedDomainObjectCollectionUtils.whenElementKnown;
+
 /*final*/ abstract class DefaultNokeeExtension implements NokeeExtension {
 	private final ModelRegistry modelRegistry = new DefaultModelRegistry();
+	private final DefaultNamedDomainObjectRegistry registry;
 	private final ModelNode model;
 
 	@Inject
-	public DefaultNokeeExtension(NamedDomainObjectRegistry registry) {
+	public DefaultNokeeExtension(DefaultNamedDomainObjectRegistry registry) {
 		this.model = new DefaultModelNodeDsl(registry, modelRegistry.getRoot());
+		this.registry = registry;
 	}
 
 	@Override
 	public ModelRegistry getModelRegistry() {
 		return modelRegistry;
+	}
+
+	@Override
+	public <T> void bridgeContainer(NamedDomainObjectContainer<T> container) {
+		registry.registerContainer(new NamedDomainObjectContainerRegistry.NamedContainerRegistry<>(container));
+		whenElementKnown(container, new RegisterModelProjection<>(modelRegistry));
+	}
+
+	@Override
+	public <T> void bridgeContainer(PolymorphicDomainObjectContainer<T> container) {
+		registry.registerContainer(new NamedDomainObjectContainerRegistry.PolymorphicContainerRegistry<>(container));
+		whenElementKnown(container, new RegisterModelProjection<>(modelRegistry));
 	}
 
 	@Override
