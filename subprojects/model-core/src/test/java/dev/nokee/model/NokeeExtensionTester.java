a604a1d649bed4dd29733f2d73c2e9522f05389b
Daniel Lacasse <daniel@nokee.dev>
2021-06-03 10:37:28 -0400

    Add basic test coverage for container bridging into Nokee model

--- subprojects/model-core/src/test/java/dev/nokee/model/NokeeExtensionTester.java
+++ subprojects/model-core/src/test/java/dev/nokee/model/NokeeExtensionTester.java
@@ -4,16 +4,15 @@ import dev.nokee.model.dsl.ModelNode;
 import dev.nokee.model.registry.ModelRegistry;
 import lombok.val;
 import org.gradle.api.plugins.ExtensionAware;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
+import static dev.gradleplugins.grava.testing.util.ProjectTestUtils.objectFactory;
 import static dev.nokee.utils.ActionTestUtils.mockAction;
 import static dev.nokee.utils.ClosureTestUtils.mockClosure;
 import static dev.nokee.utils.FunctionalInterfaceMatchers.calledOnceWith;
 import static dev.nokee.utils.FunctionalInterfaceMatchers.singleArgumentOf;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.isA;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public interface NokeeExtensionTester {
@@ -59,6 +58,38 @@ public interface NokeeExtensionTester {
 				is(extension)),
 			() -> assertThat(extension.bridgeContainer(objectFactory().polymorphicDomainObjectContainer(TestProjection.class)),
 				is(extension))
+		);
+	}
+
+	// TODO: Move to integration test
+	@Test
+	default void canBridgeNamedContainer() {
+		val container = objectFactory().domainObjectContainer(TestProjection.class);
+		val extension = createSubject().bridgeContainer(container);
+		val modelRegistry = extension.getModelRegistry();
+		container.register("foo");
+		container.create("bar");
+		extension.getModel().node("far").projection(TestProjection.class);
+		assertAll(
+			() -> assertThat(modelRegistry.getRoot().get("foo").canBeViewedAs(TestProjection.class), is(true)),
+			() -> assertThat(modelRegistry.getRoot().get("bar").canBeViewedAs(TestProjection.class), is(true)),
+			() -> assertThat(container.findByName("far"), notNullValue(TestProjection.class))
+		);
+	}
+
+	@Test
+	default void canBridgePolymorphicContainer() {
+		val container = objectFactory().polymorphicDomainObjectContainer(TestProjection.class);
+		container.registerFactory(TestProjection.class, TestProjection::new);
+		val extension = createSubject().bridgeContainer(container);
+		val modelRegistry = extension.getModelRegistry();
+		container.register("foo", TestProjection.class);
+		container.create("bar", TestProjection.class);
+		extension.getModel().node("far").projection(TestProjection.class);
+		assertAll(
+			() -> assertThat(modelRegistry.getRoot().get("foo").canBeViewedAs(TestProjection.class), is(true)),
+			() -> assertThat(modelRegistry.getRoot().get("bar").canBeViewedAs(TestProjection.class), is(true)),
+			() -> assertThat(container.findByName("far"), notNullValue(TestProjection.class))
 		);
 	}
 }
