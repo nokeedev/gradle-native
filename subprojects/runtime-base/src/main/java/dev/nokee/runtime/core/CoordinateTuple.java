bcb3fca1c55043b3254ced4fcfc4673c6d79aaa2
Daniel Lacasse <daniel@nokee.dev>
2021-06-07 13:22:34 -0400

    Consolidate get/find axis for CoordinateTuple
    To search nested coordinate, use the algorithms. By default coordinate
    typle only check for current coordinate (not nested).

--- subprojects/runtime-base/src/main/java/dev/nokee/runtime/core/CoordinateTuple.java
+++ subprojects/runtime-base/src/main/java/dev/nokee/runtime/core/CoordinateTuple.java
@@ -2,17 +2,21 @@ package dev.nokee.runtime.core;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public interface CoordinateTuple extends Iterable<Coordinate<?>> {
 	/**
-	 * Returns the coordinate value for the specified axis.
+	 * Returns the coordinate value for the specified axis, if available.
+	 *
+	 * If nested subspace coordinate needs to be searched, use {@link Coordinates#find(CoordinateTuple, CoordinateAxis, boolean)}.
+	 * Else, it's possible to flatten the tuple to remove parent coordinate from the search.
 	 *
 	 * @param axis  the axis to retrieve the value, must not be null
 	 * @param <T>  the axis value type
 	 * @return the value for the specified axis, never null
 	 */
-	default <T> T get(CoordinateAxis<T> axis) {
-		return Coordinates.get(this, axis);
+	default <T> Optional<T> find(CoordinateAxis<T> axis) {
+		return Coordinates.find(this, axis, false);
 	}
 
 	static CoordinateTuple of(Coordinate<?>... coordinates) {
