46ac1c2120c43fa332000aece5de1a41c3182a5a
Daniel Lacasse <daniel@nokee.dev>
2021-06-18 19:44:24 -0400

    Introduce matcher method for consumable, resolvable and declarable buckets

---
+++ subprojects/core-utils/src/test/groovy/dev/nokee/utils/ConfigurationUtils_DeclarableTest.java
@@ -0,0 +1,44 @@
+package dev.nokee.utils;
+
+import com.google.common.testing.EqualsTester;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.specs.Spec;
+import org.junit.jupiter.api.Test;
+
+import static dev.nokee.internal.testing.utils.ConfigurationTestUtils.testConfiguration;
+import static dev.nokee.utils.ConfigurationUtils.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ConfigurationUtils_DeclarableTest {
+	@Test
+	void onlyMatchDeclarableBucket() {
+		assertAll(
+			() -> assertThat(declarable().isSatisfiedBy(testConfiguration(asConsumable())), is(false)),
+			() -> assertThat(declarable().isSatisfiedBy(testConfiguration(asResolvable())), is(false)),
+			() -> assertThat(declarable().isSatisfiedBy(testConfiguration(asDeclarable())), is(true))
+		);
+	}
+
+	@Test
+	void returnsEnhancedSpec() {
+		assertThat(declarable(), isA(SpecUtils.Spec.class));
+	}
+
+	@Test
+	@SuppressWarnings("UnstableApiUsage")
+	void checkEquals() {
+		new EqualsTester()
+			.addEqualityGroup(declarable(), declarable())
+			.addEqualityGroup(resolvable())
+			.addEqualityGroup(consumable())
+			.addEqualityGroup((Spec<Configuration>) it -> true)
+			.testEquals();
+	}
+
+	@Test
+	void checkToString() {
+		assertThat(declarable(), hasToString("ConfigurationUtils.declarable()"));
+	}
+}
