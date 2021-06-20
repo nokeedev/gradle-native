0bc025e1a5591e6848d58f204351a46f9fbfbd9d
Daniel Lacasse <daniel@nokee.dev>
2021-06-19 22:33:46 -0400

    Allow target machine to provide attributes to configurations

--- subprojects/runtime-native/src/test/groovy/dev/nokee/runtime/nativebase/TargetMachineFactoryTester.java
+++ subprojects/runtime-native/src/test/groovy/dev/nokee/runtime/nativebase/TargetMachineFactoryTester.java
@@ -9,6 +9,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import static dev.gradleplugins.grava.testing.util.ProjectTestUtils.rootProject;
+import static dev.nokee.internal.testing.ConfigurationMatchers.attributes;
+import static dev.nokee.internal.testing.GradleNamedMatchers.named;
+import static dev.nokee.runtime.base.internal.ProvideAttributes.attributesOf;
+import static dev.nokee.runtime.nativebase.MachineArchitecture.ARCHITECTURE_ATTRIBUTE;
+import static dev.nokee.runtime.nativebase.OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE;
+import static dev.nokee.utils.ConfigurationUtils.asConsumable;
+import static dev.nokee.utils.ConfigurationUtils.asResolvable;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public interface TargetMachineFactoryTester {
@@ -21,6 +32,38 @@ public interface TargetMachineFactoryTester {
 		assertAll(
 			() -> os.assertOperatingSystem(machine),
 			() -> arch.assertMachineArchitecture(machine)
+		);
+	}
+
+	@ParameterizedTest(name = "provides attributes for consumable configuration {arguments}")
+	@MethodSource("dev.nokee.runtime.nativebase.TargetMachineFactoryTestUtils#configurers")
+	default void providesAttributesForConsumableConfiguration(OperatingSystemFamilyConfigurer os, MachineArchitectureConfigurer arch) {
+		val machine = arch.apply(os.apply(createSubject()));
+		val project = rootProject();
+		val test = project.getConfigurations().create("test",
+			asConsumable().andThen(attributesOf(machine)));
+
+		assertAll(
+			() -> assertThat(test, attributes(hasEntry(equalTo(OPERATING_SYSTEM_ATTRIBUTE), named(os.getName())))),
+			() -> assertThat(test, attributes(hasEntry(equalTo(ARCHITECTURE_ATTRIBUTE), named(arch.getName())))),
+			() -> assertThat(test, attributes(hasEntry(equalTo(org.gradle.nativeplatform.OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE), named(os.getName())))),
+			() -> assertThat(test, attributes(hasEntry(equalTo(org.gradle.nativeplatform.MachineArchitecture.ARCHITECTURE_ATTRIBUTE), named(arch.getName()))))
+		);
+	}
+
+	@ParameterizedTest(name = "provides attributes for resolvable configuration {arguments}")
+	@MethodSource("dev.nokee.runtime.nativebase.TargetMachineFactoryTestUtils#configurers")
+	default void providesAttributesForResolvableConfiguration(OperatingSystemFamilyConfigurer os, MachineArchitectureConfigurer arch) {
+		val machine = arch.apply(os.apply(createSubject()));
+		val project = rootProject();
+		val test = project.getConfigurations().create("test",
+			asResolvable().andThen(attributesOf(machine)));
+
+		assertAll(
+			() -> assertThat(test, attributes(hasEntry(equalTo(OPERATING_SYSTEM_ATTRIBUTE), named(os.getName())))),
+			() -> assertThat(test, attributes(hasEntry(equalTo(ARCHITECTURE_ATTRIBUTE), named(arch.getName())))),
+			() -> assertThat(test, attributes(hasEntry(equalTo(org.gradle.nativeplatform.OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE), named(os.getName())))),
+			() -> assertThat(test, attributes(hasEntry(equalTo(org.gradle.nativeplatform.MachineArchitecture.ARCHITECTURE_ATTRIBUTE), named(arch.getName()))))
 		);
 	}
 
