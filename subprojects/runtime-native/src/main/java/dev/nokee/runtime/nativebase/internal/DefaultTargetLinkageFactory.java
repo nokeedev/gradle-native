313bd26c3042e83de384253c366cc9509e82dcc3
Daniel Lacasse <daniel@nokee.dev>
2021-06-19 22:16:50 -0400

    Set correct visibility on target linkage factory

--- subprojects/runtime-native/src/main/java/dev/nokee/runtime/nativebase/internal/DefaultTargetLinkageFactory.java
+++ subprojects/runtime-native/src/main/java/dev/nokee/runtime/nativebase/internal/DefaultTargetLinkageFactory.java
@@ -3,7 +3,7 @@ package dev.nokee.runtime.nativebase.internal;
 import dev.nokee.runtime.nativebase.TargetLinkage;
 import dev.nokee.runtime.nativebase.TargetLinkageFactory;
 
-class DefaultTargetLinkageFactory implements TargetLinkageFactory {
+final class DefaultTargetLinkageFactory implements TargetLinkageFactory {
 	@Override
 	public TargetLinkage getShared() {
 		return TargetLinkages.SHARED;
