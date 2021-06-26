f57bbb4804b05074a52491516a5136b6c3b1edf8
Daniel Lacasse <daniel@nokee.dev>
2021-06-25 20:23:53 -0400

    Refactor binary linkage into schema action and test coverage

--- subprojects/runtime-native/src/main/java/dev/nokee/runtime/nativebase/internal/NativeRuntimePlugin.java
+++ subprojects/runtime-native/src/main/java/dev/nokee/runtime/nativebase/internal/NativeRuntimePlugin.java
@@ -4,19 +4,24 @@ import dev.nokee.runtime.nativebase.*;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.attributes.AttributeMatchingStrategy;
+import org.gradle.api.attributes.AttributesSchema;
+
+import static dev.nokee.runtime.nativebase.BinaryLinkage.BINARY_LINKAGE_ATTRIBUTE;
 
 public /*final*/ abstract class NativeRuntimePlugin implements Plugin<Project> {
 	@Override
 	public void apply(Project project) {
 		project.getDependencies().getAttributesSchema()
-			.attribute(BinaryLinkage.BINARY_LINKAGE_ATTRIBUTE).getDisambiguationRules().add(BinaryLinkageSelectionRule.class);
-
-		project.getDependencies().getAttributesSchema()
 			.attribute(BuildType.BUILD_TYPE_ATTRIBUTE, this::configureBuildTypeRules);
+		project.getDependencies().attributesSchema(this::configureAttributesSchema);
 	}
 
 	private void configureBuildTypeRules(AttributeMatchingStrategy<BuildType> strategy) {
 		strategy.getDisambiguationRules().add(BuildTypeSelectionRule.class);
 		strategy.getCompatibilityRules().add(BuildTypeCompatibilityRule.class);
+	}
+
+	private void configureAttributesSchema(AttributesSchema schema) {
+		schema.attribute(BINARY_LINKAGE_ATTRIBUTE, new BinaryLinkageAttributeSchema());
 	}
 }
