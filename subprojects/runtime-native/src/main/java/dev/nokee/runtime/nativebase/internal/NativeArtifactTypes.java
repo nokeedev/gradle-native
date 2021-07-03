72a7f94b95accc5b6f8dd9411bddcb3634f53bb7
Daniel Lacasse <daniel@nokee.dev>
2021-07-03 15:46:15 -0400

    Extract native artifact type constants

--- subprojects/runtime-native/src/main/java/dev/nokee/runtime/nativebase/internal/NativeArtifactTypes.java
+++ subprojects/runtime-native/src/main/java/dev/nokee/runtime/nativebase/internal/NativeArtifactTypes.java
@@ -12,4 +12,29 @@ public final class NativeArtifactTypes {
 	 * Represent a directory tree containing native headers, often referred to as header search path or include root.
 	 */
 	public static final String NATIVE_HEADERS_DIRECTORY = "native-headers-directory";
+
+	/**
+	 * Represents shared objects library file, mostly used on Unix-like system for shared library at link-time and runtime.
+	 */
+	public static final String SHARED_OBJECTS_LIBRARY = "so";
+
+	/**
+	 * Represents Mach-O dynamic library file, mostly used on macOS for shared library at link-time and runtime.
+	 */
+	public static final String MACH_OBJECT_DYNAMIC_LIBRARY = "dylib";
+
+	/**
+	 * Represents dynamic-link library file, mostly used on Windows for shared library at runtime.
+	 */
+	public static final String DYNAMIC_LINK_LIBRARY = "dll";
+
+	/**
+	 * Represents static library archive file, mostly used on Unix-like system for static library at link-time.
+	 */
+	public static final String STATIC_LIBRARY_ARCHIVE = "a";
+
+	/**
+	 * Represents static or import library file, mostly used on Windows for static library or shared library (linkable part) at link-time.
+	 */
+	public static final String STATIC_OR_IMPORT_LIBRARY = "lib";
 }
