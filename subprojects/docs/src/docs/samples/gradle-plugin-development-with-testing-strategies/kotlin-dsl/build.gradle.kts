e26922faa3d7ef5ed0f0226ff2f69f3f7ae16dc3
Daniel Lacasse <daniel@nokee.dev>
2021-07-04 09:43:36 -0400

    Upgrade toolbox to 1.4.2 to remove a locally published feature

--- subprojects/docs/src/docs/samples/gradle-plugin-development-with-testing-strategies/kotlin-dsl/build.gradle.kts
+++ subprojects/docs/src/docs/samples/gradle-plugin-development-with-testing-strategies/kotlin-dsl/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	id("dev.gradleplugins.java-gradle-plugin") version("1.4.1")
-	id("dev.gradleplugins.gradle-plugin-functional-test") version("1.4.1")
+	id("dev.gradleplugins.java-gradle-plugin") version("1.4.2")
+	id("dev.gradleplugins.gradle-plugin-functional-test") version("1.4.2")
 	id("groovy-base") // for Spock testing
 }
 
