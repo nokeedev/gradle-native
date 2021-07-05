ec45bdd3cdf985b4ea9e9cf3851f91c0bbeaf6ef
Daniel Lacasse <daniel@nokee.dev>
2021-07-04 21:36:57 -0400

    Allow known domain object to be callable resolvable
    Lots of Gradle APIs, will try to resolve Callable objects, such as
    FileCollection. Allowing known object to be Callable as an
    implementation detail, makes integration between Nokee and Gradle
    easier.

--- subprojects/model-core/src/test/java/dev/nokee/model/internal/DefaultKnownDomainObjectTest.java
+++ subprojects/model-core/src/test/java/dev/nokee/model/internal/DefaultKnownDomainObjectTest.java
@@ -5,6 +5,15 @@ import dev.nokee.model.KnownDomainObjectTester;
 import dev.nokee.model.TestProjection;
 import dev.nokee.model.graphdb.Graph;
 import lombok.val;
+import org.gradle.api.provider.Provider;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+
+import static dev.nokee.internal.testing.GradleProviderMatchers.providerOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.isA;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class DefaultKnownDomainObjectTest implements KnownDomainObjectTester<TestProjection> {
 	@Override
@@ -15,5 +24,15 @@ public class DefaultKnownDomainObjectTest implements KnownDomainObjectTester<Tes
 			.type(TestProjection.class)
 			.graph(graph).build();
 		return new DefaultKnownDomainObject<>(TestProjection.class, projection);
+	}
+
+	@Test
+	void canResolveAsCallable() {
+		assertAll(
+			() -> assertThat("is callable", createSubject(), isA(Callable.class)),
+			() -> assertThat("returns a provider object", ((Callable<?>) createSubject()).call(), isA(Provider.class)),
+			() -> assertThat("returns a provider for the object",
+				((Callable<Provider<?>>) createSubject()).call(), providerOf(isA(TestProjection.class)))
+		);
 	}
 }
