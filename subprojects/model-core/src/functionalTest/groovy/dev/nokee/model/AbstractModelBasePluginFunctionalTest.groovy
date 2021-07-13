3505fad7e14d15bffb0f1867e5d3abee9833aac3
Daniel Lacasse <daniel@nokee.dev>
2021-07-13 13:40:11 -0400

    Reuse functional tests between Project and Settings scenarios

--- subprojects/model-core/src/functionalTest/groovy/dev/nokee/model/AbstractModelBasePluginFunctionalTest.groovy
+++ subprojects/model-core/src/functionalTest/groovy/dev/nokee/model/AbstractModelBasePluginFunctionalTest.groovy
@@ -3,4 +3,15 @@ package dev.nokee.model
 import dev.gradleplugins.integtests.fixtures.AbstractGradleSpecification
 
 abstract class AbstractModelBasePluginFunctionalTest extends AbstractGradleSpecification implements ModelBasePluginSpec {
+	def "registers nokee extension"() {
+		buildscriptFile << applyModelBasePlugin() << '''
+			assert nokee instanceof NokeeExtension
+			assert nokee.modelRegistry instanceof ModelRegistry
+		'''
+
+		expect:
+		succeeds()
+	}
+
+	protected abstract File getBuildscriptFile();
 }
