d61188fee563715780d5234e51513c4e25dba159
Daniel Lacasse <daniel@nokee.dev>
2021-07-15 17:37:10 -0400

    Allow iterating over identity from the identifier

--- subprojects/model-core/src/test/java/dev/nokee/model/DomainObjectIdentifierTester.java
+++ subprojects/model-core/src/test/java/dev/nokee/model/DomainObjectIdentifierTester.java
@@ -3,10 +3,14 @@ package dev.nokee.model;
 import dev.nokee.model.core.ModelProjection;
 import dev.nokee.model.graphdb.Graph;
 import dev.nokee.model.internal.DefaultModelFactory;
+import dev.nokee.model.internal.DomainObjectIdentities;
 import lombok.val;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
+import static dev.nokee.model.internal.DomainObjectIdentities.root;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasToString;
 
 public interface DomainObjectIdentifierTester {
@@ -26,6 +30,14 @@ public interface DomainObjectIdentifierTester {
 		val node = new DefaultModelFactory(graph).createNode(graph.createNode()).newChildNode("bar");
 		val projection = node.newProjection(builder -> builder.forInstance(new DefaultTestProjection()));
 		assertThat(createSubject(projection), hasToString("default test projection 'bar'"));
+	}
+
+	@Test
+	default void canIterateNodeIdentity() {
+		val graph = Graph.builder().build();
+		val node = new DefaultModelFactory(graph).createNode(graph.createNode()).newChildNode("foo").newChildNode("bar");
+		val projection = node.newProjection(builder -> builder.forInstance(new DefaultTestProjection()));
+		assertThat(createSubject(projection), contains(root(), "foo", "bar"));
 	}
 
 	@DisplayName("test projection")
