7f09980bd9d2a8856c027a8079dac9a13720723b
Daniel Lacasse <daniel@nokee.dev>
2021-07-17 20:21:16 -0400

    Extract platform Swift code into own project

--- subprojects/platform-swift/src/functionalTest/groovy/dev/nokee/platform/swift/SwiftApplicationComponentFunctionalTest.groovy
+++ subprojects/platform-swift/src/functionalTest/groovy/dev/nokee/platform/swift/SwiftApplicationComponentFunctionalTest.groovy
@@ -1,8 +1,8 @@
 package dev.nokee.platform.swift
 
+import dev.gradleplugins.fixtures.sources.SourceElement
 import dev.gradleplugins.integtests.fixtures.nativeplatform.RequiresInstalledToolChain
 import dev.gradleplugins.integtests.fixtures.nativeplatform.ToolChainRequirement
-import dev.gradleplugins.fixtures.sources.SourceElement
 import dev.nokee.fixtures.AbstractNativeApplicationComponentFunctionalTest
 import dev.nokee.platform.nativebase.fixtures.SwiftGreeterApp
 
