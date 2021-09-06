d62a114c08689d568143d79218f53d3b9a90f0f6
Daniel Lacasse <daniel@nokee.dev>
2021-09-06 12:58:08 -0400

    Allow deferred type as description format arguments
    There is no reason to ever use the toString() of a callable or provider.
    For this reason, we unpack each format arguments. It allows for quick
    and easy deferred description.

--- subprojects/core-utils/src/main/java/dev/nokee/utils/StringFormatSupplier.java
+++ subprojects/core-utils/src/main/java/dev/nokee/utils/StringFormatSupplier.java
@@ -3,6 +3,7 @@ package dev.nokee.utils;
 import lombok.EqualsAndHashCode;
 import lombok.val;
 
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 import static java.util.Objects.requireNonNull;
@@ -17,15 +18,20 @@ final class StringFormatSupplier implements Supplier<String> {
 		this.args = requireNonNull(args);
 	}
 
+	public Object[] getUnpackedArgs() {
+		return Arrays.stream(args).map(DeferredUtils::unpack).toArray();
+	}
+
 	@Override
 	public String get() {
-		return String.format(format, args);
+		return String.format(format, getUnpackedArgs());
 	}
 
 	@Override
 	public String toString() {
 		val builder = new StringBuilder();
 		builder.append("String.format(").append(format);
+		val args = getUnpackedArgs();
 		if (args.length > 0) {
 			for (Object arg : args) {
 				builder.append(", ").append(arg);
