82dfd18fb11fe2a1cd9a41fd341c2022a90f4a94
Daniel Lacasse <daniel@nokee.dev>
2021-09-06 17:24:23 -0400

    Introduce JUnit extension annotation for quick plugin requirement

---
+++ subprojects/internal-testing/src/main/java/dev/nokee/internal/testing/PluginRequirement.java
@@ -0,0 +1,61 @@
+package dev.nokee.internal.testing;
+
+import lombok.val;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.platform.commons.util.AnnotationUtils;
+import org.junit.platform.commons.util.ReflectionUtils;
+
+import java.lang.annotation.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+public final class PluginRequirement {
+	@Target({ElementType.TYPE, ElementType.METHOD})
+	@Retention(RetentionPolicy.RUNTIME)
+	@Inherited
+	@ExtendWith(Extension.class)
+	public @interface Require {
+		Class<? extends Plugin<? extends Project>> type() default NONE.class;
+		String id() default "";
+
+		interface NONE extends Plugin<Project> {}
+	}
+
+	static final class Extension implements BeforeEachCallback {
+		@Override
+		public void beforeEach(ExtensionContext context) throws Exception {
+			AnnotationUtils.findAnnotation(context.getElement(), PluginRequirement.Require.class).ifPresent(it -> applyPlugin(context, it));
+			val allInstances = new ArrayList<>(context.getRequiredTestInstances().getAllInstances());
+			Collections.reverse(allInstances);
+			for (Object testInstance : allInstances) {
+				for (Require require : testInstance.getClass().getAnnotationsByType(Require.class)) {
+					applyPlugin(context, require);
+				}
+			}
+		}
+
+		private void applyPlugin(ExtensionContext context, Require require) {
+			val project = (Project) context.getRequiredTestInstances().getAllInstances().stream().flatMap(it -> {
+				val method = ReflectionUtils.findMethod(it.getClass(), "project");
+				if (method.isPresent()) {
+					return Stream.of(ReflectionUtils.invokeMethod(method.get(), it));
+				} else {
+					return Stream.empty();
+				}
+			}).findFirst().orElseThrow(RuntimeException::new);
+
+			if (!require.id().isEmpty()) {
+				System.out.println("Applying plugin " + require.id());
+				project.getPluginManager().apply(require.id());
+			} else {
+				System.out.println("Applying plugin " + require.type());
+				project.getPluginManager().apply(require.type());
+			}
+		}
+	}
+}
