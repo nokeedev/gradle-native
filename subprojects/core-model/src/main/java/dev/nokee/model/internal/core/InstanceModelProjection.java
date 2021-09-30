d20f173f126a4d8e5b2abd76dfbdefe40d8d15f1
Daniel Lacasse <daniel@nokee.dev>
2021-09-29 22:10:52 -0400

    Remove newline after license header

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/core/InstanceModelProjection.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/core/InstanceModelProjection.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package dev.nokee.model.internal.core;
 
 import dev.nokee.model.internal.type.ModelType;
