ea30723f255c2a6740bc093eba72c27a8b4e94fc
Daniel Lacasse <daniel@nokee.dev>
2021-10-01 10:43:39 -0400

    Move apply notation to seperate class

--- subprojects/internal-testing/src/main/java/dev/nokee/internal/testing/runnerkit/ApplySection.java
+++ subprojects/internal-testing/src/main/java/dev/nokee/internal/testing/runnerkit/ApplySection.java
@@ -15,124 +15,24 @@
  */
 package dev.nokee.internal.testing.runnerkit;
 
-import org.gradle.api.Plugin;
+public final class ApplySection extends AbstractSection {
+	private final ApplyNotation notation;
 
-public class ApplySection extends AbstractSection {
-	private final ApplySectionNotation notation;
-
-	private ApplySection(ApplySectionNotation notation) {
+	private ApplySection(ApplyNotation notation) {
 		this.notation = notation;
 	}
 
-	public static ApplySection apply(ApplySectionNotation notation) {
+	public static ApplySection apply(ApplyNotation notation) {
 		return new ApplySection(notation);
-	}
-
-	public interface ApplySectionNotation extends CodeSegment {
-		static ApplySectionNotation plugin(String pluginId) {
-			return new PluginIdApplyNotation(pluginId);
-		}
-
-		static ApplySectionNotation plugin(Class<? extends Plugin<?>> pluginType) {
-			return new PluginTypeApplyNotation(pluginType);
-		}
-
-		static ApplySectionNotation from(String path) {
-			return new FromApplyNotation(path);
-		}
-	}
-
-	private static final class PluginIdApplyNotation extends AbstractSection implements ApplySectionNotation {
-		private final String pluginId;
-
-		private PluginIdApplyNotation(String pluginId) {
-			this.pluginId = pluginId;
-		}
-
-		@Override
-		protected String getGroovy() {
-			return "plugin: '" + pluginId + "'";
-		}
-
-		@Override
-		protected String getKotlin() {
-			return "plugin = \"" + pluginId + "\"";
-		}
-
-		@Override
-		public String toString(GradleDsl dsl) {
-			return generateSection(dsl);
-		}
-
-		@Override
-		public String toString() {
-			return toString(GradleDsl.GROOVY);
-		}
-	}
-
-	private static final class PluginTypeApplyNotation extends AbstractSection implements ApplySectionNotation {
-		private final Class<? extends Plugin<?>> pluginType;
-
-		private PluginTypeApplyNotation(Class<? extends Plugin<?>> pluginType) {
-			this.pluginType = pluginType;
-		}
-
-		@Override
-		protected String getGroovy() {
-			return "plugin: Class.forName('" + pluginType.getTypeName() + "')";
-		}
-
-		@Override
-		protected String getKotlin() {
-			return "plugin = Class.forName(\"" + pluginType.getTypeName() + "\")";
-		}
-
-		@Override
-		public String toString(GradleDsl dsl) {
-			return generateSection(dsl);
-		}
-
-		@Override
-		public String toString() {
-			return toString(GradleDsl.GROOVY);
-		}
-	}
-
-	private static final class FromApplyNotation extends AbstractSection implements ApplySectionNotation {
-		private final String path;
-
-		private FromApplyNotation(String path) {
-			this.path = path;
-		}
-
-		@Override
-		protected String getGroovy() {
-			return "from: '" + GradleDsl.GROOVY.fileName(path) + "'";
-		}
-
-		@Override
-		protected String getKotlin() {
-			return "from = \"" + GradleDsl.KOTLIN.fileName(path) + "\"";
-		}
-
-		@Override
-		public String toString(GradleDsl dsl) {
-			return generateSection(dsl);
-		}
-
-		@Override
-		public String toString() {
-			return toString(GradleDsl.GROOVY);
-		}
 	}
 
 	@Override
 	protected String getGroovy() {
-		return "apply " + ((Section) notation).generateSection(GradleDsl.GROOVY);
+		return "apply " + ((CodeSegment) notation).toString(GradleDsl.GROOVY);
 	}
 
 	@Override
 	protected String getKotlin() {
-		return "apply(" + ((Section) notation).generateSection(GradleDsl.KOTLIN) + ")";
+		return "apply(" + ((CodeSegment) notation).toString(GradleDsl.KOTLIN) + ")";
 	}
 }
