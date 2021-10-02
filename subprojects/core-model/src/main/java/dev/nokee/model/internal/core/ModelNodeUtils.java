12764f02392e0b2c212aa5961e53b10622c3ffc2
Daniel Lacasse <daniel@nokee.dev>
2021-10-02 12:04:52 -0400

    Extract type description for projections from model node

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelNodeUtils.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelNodeUtils.java
@@ -163,4 +163,17 @@ public final class ModelNodeUtils {
 	public static Stream<ModelProjection> getProjections(ModelNode self) {
 		return self.getComponents().filter(ModelProjection.class::isInstance).map(ModelProjection.class::cast);
 	}
+
+	/**
+	 * Returns the main projection type description of this node.
+	 * In practice, this describes the type of the Object projection of this node.
+	 *
+	 * @param self  the node to query type description, must not be null
+	 * @return the type description of this node, if present.
+	 */
+	// TODO: This is used to generating descriptive message. We should find a better way to do this.
+	//  In the end, this fits in the reporting APIs.
+	public static Optional<String> getTypeDescription(ModelNode self) {
+		return getProjections(self).findFirst().map(ModelProjection::getTypeDescriptions).map(it -> String.join(", ", it));
+	}
 }
