34b643692753620418c102ad074ada9ae0359fd3
Daniel Lacasse <daniel@nokee.dev>
2021-10-02 20:23:46 -0400

    Rework model state tests

---
+++ subprojects/core-model/src/test/groovy/dev/nokee/model/internal/state/ModelStateTester.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.nokee.model.internal.state;
+
+import dev.nokee.model.internal.core.ModelNode;
+import org.junit.jupiter.api.Test;
+
+import static dev.nokee.model.internal.core.ModelNodeUtils.getState;
+import static dev.nokee.model.internal.core.ModelNodeUtils.isAtLeast;
+import static org.junit.jupiter.api.Assertions.*;
+
+public interface ModelStateTester {
+	ModelNode subject();
+
+	interface None extends ModelStateTester {
+		@Test
+		default void doesNotHaveCreatedTag() {
+			assertFalse(subject().hasComponent(ModelState.Created.class));
+		}
+
+		@Test
+		default void doesNotHaveInitializedTag() {
+			assertFalse(subject().hasComponent(ModelState.Initialized.class));
+		}
+
+		@Test
+		default void doesNotHaveRegisteredTag() {
+			assertFalse(subject().hasComponent(ModelState.Registered.class));
+		}
+
+		@Test
+		default void doesNotHaveRealizedTag() {
+			assertFalse(subject().hasComponent(ModelState.Realized.class));
+		}
+
+		@Test
+		default void defaultsToCreatedState() {
+			assertEquals(getState(subject()), ModelNode.State.Created);
+		}
+
+		@Test
+		default void doesNotHaveStateComponent() {
+			assertFalse(subject().hasComponent(ModelNode.State.class));
+		}
+
+		@Test
+		default void isAtLeastCreated() {
+			assertTrue(isAtLeast(subject(), ModelNode.State.Created));
+
+			assertFalse(isAtLeast(subject(), ModelNode.State.Initialized));
+			assertFalse(isAtLeast(subject(), ModelNode.State.Registered));
+			assertFalse(isAtLeast(subject(), ModelNode.State.Realized));
+		}
+	}
+
+	interface Created extends ModelStateTester {
+		@Test
+		default void hasCreatedTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Created.class));
+		}
+
+		@Test
+		default void doesNotHaveInitializedTag() {
+			assertFalse(subject().hasComponent(ModelState.Initialized.class));
+		}
+
+		@Test
+		default void doesNotHaveRegisteredTag() {
+			assertFalse(subject().hasComponent(ModelState.Registered.class));
+		}
+
+		@Test
+		default void doesNotHaveRealizedTag() {
+			assertFalse(subject().hasComponent(ModelState.Realized.class));
+		}
+
+		@Test
+		default void hasCreatedState() {
+			assertEquals(getState(subject()), ModelNode.State.Created);
+		}
+
+		@Test
+		default void hasStateComponent() {
+			assertTrue(subject().hasComponent(ModelNode.State.class));
+		}
+
+		@Test
+		default void isAtLeastCreated() {
+			assertTrue(isAtLeast(subject(), ModelNode.State.Created));
+
+			assertFalse(isAtLeast(subject(), ModelNode.State.Initialized));
+			assertFalse(isAtLeast(subject(), ModelNode.State.Registered));
+			assertFalse(isAtLeast(subject(), ModelNode.State.Realized));
+		}
+	}
+
+	interface Initialized extends ModelStateTester {
+		@Test
+		default void hasCreatedTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Created.class));
+		}
+
+		@Test
+		default void hasInitializedTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Initialized.class));
+		}
+
+		@Test
+		default void doesNotHaveRegisteredTag() {
+			assertFalse(subject().hasComponent(ModelState.Registered.class));
+		}
+
+		@Test
+		default void doesNotHaveRealizedTag() {
+			assertFalse(subject().hasComponent(ModelState.Realized.class));
+		}
+
+		@Test
+		default void hasInitializedState() {
+			assertEquals(getState(subject()), ModelNode.State.Initialized);
+		}
+
+		@Test
+		default void hasStateComponent() {
+			assertTrue(subject().hasComponent(ModelNode.State.class));
+		}
+
+		@Test
+		default void isAtLeastInitialized() {
+			assertTrue(isAtLeast(subject(), ModelNode.State.Created));
+			assertTrue(isAtLeast(subject(), ModelNode.State.Initialized));
+
+			assertFalse(isAtLeast(subject(), ModelNode.State.Registered));
+			assertFalse(isAtLeast(subject(), ModelNode.State.Realized));
+		}
+	}
+
+	interface Registered extends ModelStateTester {
+		@Test
+		default void hasCreatedTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Created.class));
+		}
+
+		@Test
+		default void hasInitializedTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Initialized.class));
+		}
+
+		@Test
+		default void hasRegisteredTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Registered.class));
+		}
+
+		@Test
+		default void doesNotHaveRealizedTag() {
+			assertFalse(subject().hasComponent(ModelState.Realized.class));
+		}
+
+		@Test
+		default void hasRegisteredState() {
+			assertEquals(getState(subject()), ModelNode.State.Registered);
+		}
+
+		@Test
+		default void hasStateComponent() {
+			assertTrue(subject().hasComponent(ModelNode.State.class));
+		}
+
+		@Test
+		default void isAtLeastRegistered() {
+			assertTrue(isAtLeast(subject(), ModelNode.State.Created));
+			assertTrue(isAtLeast(subject(), ModelNode.State.Initialized));
+			assertTrue(isAtLeast(subject(), ModelNode.State.Registered));
+
+			assertFalse(isAtLeast(subject(), ModelNode.State.Realized));
+		}
+	}
+
+	interface Realized extends ModelStateTester {
+		@Test
+		default void hasCreatedTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Created.class));
+		}
+
+		@Test
+		default void hasInitializedTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Initialized.class));
+		}
+
+		@Test
+		default void hasRegisteredTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Registered.class));
+		}
+
+		@Test
+		default void hasRealizedTagComponent() {
+			assertTrue(subject().hasComponent(ModelState.Realized.class));
+		}
+
+		@Test
+		default void hasRealizedState() {
+			assertEquals(getState(subject()), ModelNode.State.Realized);
+		}
+
+		@Test
+		default void hasStateComponent() {
+			assertTrue(subject().hasComponent(ModelNode.State.class));
+		}
+
+		@Test
+		default void isAtLeastRealized() {
+			assertTrue(isAtLeast(subject(), ModelNode.State.Created));
+			assertTrue(isAtLeast(subject(), ModelNode.State.Initialized));
+			assertTrue(isAtLeast(subject(), ModelNode.State.Registered));
+			assertTrue(isAtLeast(subject(), ModelNode.State.Realized));
+		}
+	}
+}
