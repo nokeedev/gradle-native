ad9b5cf998a159bee3f4e44274a5102ce091eb7e
Daniel Lacasse <daniel@nokee.dev>
2021-10-09 15:06:50 -0400

    Extract compile only dependency bucket API to interface

--- subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/NativeComponentDependencies.java
+++ subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/NativeComponentDependencies.java
@@ -15,10 +15,7 @@
  */
 package dev.nokee.platform.nativebase;
 
-import dev.nokee.platform.base.ComponentDependencies;
-import dev.nokee.platform.base.DependencyBucket;
-import dev.nokee.platform.base.HasImplementationDependencyBucket;
-import dev.nokee.platform.base.HasRuntimeOnlyDependencyBucket;
+import dev.nokee.platform.base.*;
 import groovy.lang.Closure;
 import groovy.lang.DelegatesTo;
 import org.gradle.api.Action;
@@ -32,35 +29,7 @@ import org.gradle.util.ConfigureUtil;
  *
  * @since 0.4
  */
-public interface NativeComponentDependencies extends ComponentDependencies, HasImplementationDependencyBucket, HasRuntimeOnlyDependencyBucket {
-	/**
-	 * Adds an native compile only dependency to this component.
-	 * An compile only dependency is not visible to consumers that are compiled or linked against this component.
-	 *
-	 * @param notation The dependency notation, as per {@link org.gradle.api.artifacts.dsl.DependencyHandler#create(Object)}.
-	 */
-	void compileOnly(Object notation);
-
-	/**
-	 * Adds an native compile only dependency to this component.
-	 * An compile only dependency is not visible to consumers that are compiled or linked against this component.
-	 *
-	 * @param notation The dependency notation, as per {@link org.gradle.api.artifacts.dsl.DependencyHandler#create(Object)}.
-	 * @param action The action to run to configure the dependency (project dependencies are {@link ProjectDependency} and external dependencies are {@link ExternalModuleDependency}).
-	 */
-	void compileOnly(Object notation, Action<? super ModuleDependency> action);
-
-	/**
-	 * Adds an native compile only dependency to this component.
-	 * An compile only dependency is not visible to consumers that are compiled or linked against this component.
-	 *
-	 * @param notation The dependency notation, as per {@link org.gradle.api.artifacts.dsl.DependencyHandler#create(Object)}.
-	 * @param closure The closure to run to configure the dependency (project dependencies are {@link ProjectDependency} and external dependencies are {@link ExternalModuleDependency}).
-	 */
-	default void compileOnly(Object notation, @DelegatesTo(value = ModuleDependency.class, strategy = Closure.DELEGATE_FIRST) @SuppressWarnings("rawtypes") Closure closure) {
-		compileOnly(notation, ConfigureUtil.configureUsing(closure));
-	}
-
+public interface NativeComponentDependencies extends ComponentDependencies, HasImplementationDependencyBucket, HasCompileOnlyDependencyBucket, HasRuntimeOnlyDependencyBucket {
 	/**
 	 * Adds an native link only dependency to this component.
 	 * An link only dependency is not visible to consumers that are compiled or linked against this component.
@@ -88,13 +57,6 @@ public interface NativeComponentDependencies extends ComponentDependencies, HasI
 	default void linkOnly(Object notation, @DelegatesTo(value = ModuleDependency.class, strategy = Closure.DELEGATE_FIRST) @SuppressWarnings("rawtypes") Closure closure) {
 		linkOnly(notation, ConfigureUtil.configureUsing(closure));
 	}
-
-	/**
-	 * Returns the compile only bucket of dependencies for this component.
-	 *
-	 * @return a {@link DependencyBucket} representing the compile only bucket of dependencies, never null.
-	 */
-	DependencyBucket getCompileOnly();
 
 	/**
 	 * Returns the link only bucket of dependencies for this component.
