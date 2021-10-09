ee40c856b0bd3bd63f167e99888187c2ee3eff5a
Daniel Lacasse <daniel@nokee.dev>
2021-10-09 15:22:03 -0400

    Remove unecessary ComponentDependencies implementation

--- subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/NativeApplicationComponentDependencies.java
+++ subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/NativeApplicationComponentDependencies.java
@@ -15,12 +15,10 @@
  */
 package dev.nokee.platform.nativebase;
 
-import dev.nokee.platform.base.ComponentDependencies;
-
 /**
  * Allows the implementation dependencies of a native application to be specified.
  *
  * @since 0.5
  */
-public interface NativeApplicationComponentDependencies extends NativeComponentDependencies, ComponentDependencies {
+public interface NativeApplicationComponentDependencies extends NativeComponentDependencies {
 }
