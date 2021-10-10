4a6b8f86132b80983d97a5b2d2dc0e0d618dc661
Daniel Lacasse <daniel@nokee.dev>
2021-10-10 08:49:31 -0400

    Remove property and method mix-in internal API usage

--- subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/dependencies/DefaultComponentDependencies.java
+++ subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/dependencies/DefaultComponentDependencies.java
@@ -17,7 +17,6 @@ package dev.nokee.platform.base.internal.dependencies;
 
 import dev.nokee.model.internal.DomainObjectIdentifierInternal;
 import dev.nokee.platform.base.DependencyBucket;
-import groovy.lang.Closure;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.val;
@@ -28,20 +27,12 @@ import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ModuleDependency;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.plugins.ExtensionAware;
-import org.gradle.api.plugins.ExtensionContainer;
-import org.gradle.internal.metaobject.*;
-import org.gradle.util.ConfigureUtil;
 
-import javax.annotation.Nullable;
 import javax.inject.Inject;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 // DO NOT EXTEND THIS CLASS, extends BaseComponentDependencies
-public abstract class DefaultComponentDependencies implements ComponentDependenciesInternal, MethodMixIn, PropertyMixIn, ExtensionAware {
-	private final ContainerElementsDynamicObject elementsDynamicObject;
+public abstract class DefaultComponentDependencies implements ComponentDependenciesInternal, ExtensionAware {
 	@Getter private final DomainObjectIdentifierInternal ownerIdentifier;
 	private final DependencyBucketFactory factory;
 	@Getter(AccessLevel.PROTECTED) private final DomainObjectSet<DependencyBucket> buckets;
@@ -52,7 +43,6 @@ public abstract class DefaultComponentDependencies implements ComponentDependenc
 		this.ownerIdentifier = ownerIdentifier;
 		this.factory = factory;
 		this.buckets = objects.domainObjectSet(DependencyBucket.class);
-		this.elementsDynamicObject = new ContainerElementsDynamicObject();
 		buckets.all(bucket -> getExtensions().add(DependencyBucket.class, bucket.getName(), bucket));
 	}
 
@@ -108,74 +98,6 @@ public abstract class DefaultComponentDependencies implements ComponentDependenc
 	private void assertBucketExists(String bucketName) {
 		if (getExtensions().findByName(bucketName) == null) {
 			throw new IllegalArgumentException(String.format("Dependency bucket named '%s' couldn't be found.", bucketName));
-		}
-	}
-
-	@Override
-	public MethodAccess getAdditionalMethods() {
-		return elementsDynamicObject;
-	}
-
-	@Override
-	public PropertyAccess getAdditionalProperties() {
-		return elementsDynamicObject;
-	}
-
-	private class ContainerElementsDynamicObject extends AbstractDynamicObject {
-		private final Map<String, DependencyBucket> bucketIndex = new HashMap<>();
-
-		ContainerElementsDynamicObject() {
-			buckets.all(bucket -> bucketIndex.put(bucket.getName(), bucket));
-		}
-
-		@Override
-		public String getDisplayName() {
-			return "";
-		}
-
-		@Override
-		public DynamicInvokeResult tryGetProperty(String name) {
-			@Nullable val bucket = (DependencyBucket) getExtensions().findByName(name);
-			if (bucket == null) {
-				return DynamicInvokeResult.notFound();
-			}
-			return DynamicInvokeResult.found(bucket);
-		}
-
-		@Override
-		public Map<String, DependencyBucket> getProperties() {
-			return Collections.unmodifiableMap(bucketIndex);
-		}
-
-		@Override
-		public boolean hasProperty(String name) {
-			return bucketIndex.containsKey(name);
-		}
-
-		@Override
-		public boolean hasMethod(String name, Object... arguments) {
-			return isConfigureMethod(name, arguments);
-		}
-
-		@Override
-		public DynamicInvokeResult tryInvokeMethod(String name, Object... arguments) {
-			if (isConfigureMethod(name, arguments)) {
-				if (arguments.length == 1) {
-					add(name, arguments[0]);
-					return DynamicInvokeResult.found();
-				} else if (arguments.length == 2) {
-					add(name, arguments[0], ConfigureUtil.configureUsing((Closure) arguments[1]));
-					return DynamicInvokeResult.found();
-				}
-			}
-			return DynamicInvokeResult.notFound();
-		}
-
-		private boolean isConfigureMethod(String name, Object... arguments) {
-			if (name.equals("project")) { // Let project(...) methods be handled by parent objects
-				return false;
-			}
-			return arguments.length == 1 || (arguments.length == 2 && arguments[1] instanceof Closure);
 		}
 	}
 }
