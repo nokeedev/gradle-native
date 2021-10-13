10452eebb603df688efbedce62c4f623b75f0d26
Daniel Lacasse <daniel@nokee.dev>
2021-10-13 10:57:40 -0400

    Mount variant dependencies property into the model

--- subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/internal/DefaultNativeApplicationVariant.java
+++ subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/internal/DefaultNativeApplicationVariant.java
@@ -25,6 +25,7 @@ import dev.nokee.platform.base.internal.VariantInternal;
 import dev.nokee.platform.base.internal.binaries.BinaryViewFactory;
 import dev.nokee.platform.base.internal.dependencies.ResolvableComponentDependencies;
 import dev.nokee.platform.nativebase.NativeApplication;
+import dev.nokee.platform.nativebase.NativeApplicationComponentDependencies;
 import dev.nokee.platform.nativebase.internal.dependencies.DefaultNativeApplicationComponentDependencies;
 import dev.nokee.platform.nativebase.internal.dependencies.VariantComponentDependencies;
 import dev.nokee.platform.nativebase.internal.rules.NativeDevelopmentBinaryConvention;
@@ -38,17 +39,19 @@ import org.gradle.api.tasks.TaskProvider;
 import javax.inject.Inject;
 
 public class DefaultNativeApplicationVariant extends BaseNativeVariant implements NativeApplication, VariantInternal {
-	@Getter private final DefaultNativeApplicationComponentDependencies dependencies;
 	@Getter private final ResolvableComponentDependencies resolvableDependencies;
 	private final ModelNode node = ModelNodeContext.getCurrentModelNode();
 
 	@Inject
 	public DefaultNativeApplicationVariant(VariantIdentifier<?> identifier, VariantComponentDependencies<DefaultNativeApplicationComponentDependencies> dependencies, ObjectFactory objects, ProviderFactory providers, TaskProvider<Task> assembleTask, BinaryViewFactory binaryViewFactory) {
 		super(identifier, objects, providers, assembleTask, binaryViewFactory);
-		this.dependencies = dependencies.getDependencies();
 		this.resolvableDependencies = dependencies.getIncoming();
 
 		getDevelopmentBinary().convention(getBinaries().getElements().flatMap(NativeDevelopmentBinaryConvention.of(getBuildVariant().getAxisValue(BinaryLinkage.BINARY_LINKAGE_COORDINATE_AXIS))));
+	}
+
+	public NativeApplicationComponentDependencies getDependencies() {
+		return ModelProperties.getProperty(node, "dependencies").as(NativeApplicationComponentDependencies.class).get();
 	}
 
 	@Override
