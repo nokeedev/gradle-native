45eb025ffb4d3245350082a988f86a86422b7c6b
Daniel Lacasse <daniel@nokee.dev>
2021-10-16 10:11:30 -0400

    Move Variants to platform-base

--- subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/Variants.java
+++ subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/Variants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 the original author or authors.
+ * Copyright 2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.nokee.testing.xctest.internal;
+package dev.nokee.platform.base.internal;
 
 import dev.nokee.model.internal.core.ModelNode;
 import dev.nokee.platform.base.BuildVariant;
-import dev.nokee.platform.base.internal.ComponentVariants;
 
 import java.util.Map;
 
