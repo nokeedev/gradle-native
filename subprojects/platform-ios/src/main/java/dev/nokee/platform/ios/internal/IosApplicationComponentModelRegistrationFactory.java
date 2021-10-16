21f5ee877be1146d4989d16b247ed78fd9766a23
Daniel Lacasse <daniel@nokee.dev>
2021-10-16 11:58:55 -0400

    Move variant calculation into model registration

--- subprojects/platform-ios/src/main/java/dev/nokee/platform/ios/internal/IosApplicationComponentModelRegistrationFactory.java
+++ subprojects/platform-ios/src/main/java/dev/nokee/platform/ios/internal/IosApplicationComponentModelRegistrationFactory.java
@@ -16,6 +16,8 @@
 package dev.nokee.platform.ios.internal;
 
 import dev.nokee.language.base.LanguageSourceSet;
+import dev.nokee.language.swift.SwiftSourceSet;
+import dev.nokee.model.DomainObjectProvider;
 import dev.nokee.model.internal.DomainObjectEventPublisher;
 import dev.nokee.model.internal.ProjectIdentifier;
 import dev.nokee.model.internal.core.*;
@@ -29,19 +31,26 @@ import dev.nokee.platform.base.internal.binaries.BinaryViewFactory;
 import dev.nokee.platform.base.internal.dependencies.ConfigurationBucketRegistryImpl;
 import dev.nokee.platform.base.internal.dependencies.DefaultComponentDependencies;
 import dev.nokee.platform.base.internal.dependencies.DependencyBucketFactoryImpl;
+import dev.nokee.platform.base.internal.tasks.TaskIdentifier;
+import dev.nokee.platform.base.internal.tasks.TaskName;
 import dev.nokee.platform.base.internal.tasks.TaskRegistry;
 import dev.nokee.platform.base.internal.tasks.TaskViewFactory;
 import dev.nokee.platform.base.internal.variants.VariantRepository;
 import dev.nokee.platform.base.internal.variants.VariantViewFactory;
-import dev.nokee.platform.nativebase.internal.dependencies.DefaultNativeComponentDependencies;
-import dev.nokee.platform.nativebase.internal.dependencies.FrameworkAwareDependencyBucketFactory;
+import dev.nokee.platform.nativebase.internal.dependencies.*;
+import dev.nokee.platform.nativebase.internal.rules.DevelopmentVariantConvention;
 import dev.nokee.utils.TransformerUtils;
 import lombok.val;
+import lombok.var;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.artifacts.dsl.DependencyHandler;
+import org.gradle.api.model.ObjectFactory;
 
 import java.util.function.BiConsumer;
 
 import static dev.nokee.model.internal.core.ModelActions.executeUsingProjection;
+import static dev.nokee.model.internal.core.ModelActions.register;
 import static dev.nokee.model.internal.core.ModelNodes.*;
 import static dev.nokee.model.internal.core.ModelProjections.createdUsing;
 import static dev.nokee.model.internal.core.ModelProjections.ofInstance;
@@ -51,6 +60,7 @@ import static dev.nokee.model.internal.type.ModelType.of;
 import static dev.nokee.platform.base.internal.LanguageSourceSetConventionSupplier.maven;
 import static dev.nokee.platform.base.internal.LanguageSourceSetConventionSupplier.withConventionOf;
 import static dev.nokee.utils.TransformerUtils.noOpTransformer;
+import static org.gradle.language.base.plugins.LifecycleBasePlugin.ASSEMBLE_TASK_NAME;
 
 public final class IosApplicationComponentModelRegistrationFactory {
 	private final Class<? extends Component> componentType;
@@ -94,18 +104,63 @@ public final class IosApplicationComponentModelRegistrationFactory {
 			.action(self(stateOf(ModelState.Finalized)).apply(ModelActionWithInputs.of(ModelComponentReference.of(ModelPath.class), (entity, path) -> {
 				val registry = project.getExtensions().getByType(ModelRegistry.class);
 				val component = ModelNodeUtils.get(entity, DefaultIosApplicationComponent.class);
+				component.getDevelopmentVariant().convention(project.getProviders().provider(new DevelopmentVariantConvention<>(() -> component.getVariants().get())));
 				component.finalizeValue();
-				component.getVariantCollection().whenElementKnown(knownVariant -> {
-					val variant = registry.register(ModelRegistration.builder()
-						.withComponent(path.child(knownVariant.getIdentifier().getUnambiguousName()))
-						.withComponent(knownVariant.getIdentifier())
-						.withComponent(IsVariant.tag())
-						.withComponent(createdUsing(of(DefaultIosApplicationVariant.class), () -> knownVariant.map(noOpTransformer()).get()))
-						.build());
-					knownVariant.configure(it -> ModelStates.realize(ModelNodes.of(variant)));
+				component.getBuildVariants().get().forEach(buildVariant -> {
+					val variantIdentifier = VariantIdentifier.builder().withBuildVariant(buildVariant).withComponentIdentifier(component.getIdentifier()).withType(DefaultIosApplicationVariant.class).build();
+
+					val variant = ModelNodeUtils.register(entity, iosApplicationVariant(variantIdentifier, component, project));
+
+					onEachVariantDependencies(variant.as(DefaultIosApplicationVariant.class), ModelNodes.of(variant).getComponent(ModelComponentType.componentOf(VariantComponentDependencies.class)));
 				});
 			})))
 			;
+	}
+
+	private static void onEachVariantDependencies(DomainObjectProvider<DefaultIosApplicationVariant> variant, VariantComponentDependencies<?> dependencies) {
+		dependencies.getOutgoing().getExportedBinary().convention(variant.flatMap(it -> it.getDevelopmentBinary()));
+	}
+
+	private static NodeRegistration iosApplicationVariant(VariantIdentifier<DefaultIosApplicationVariant> identifier, DefaultIosApplicationComponent component, Project project) {
+		val taskRegistry = project.getExtensions().getByType(TaskRegistry.class);
+		val assembleTask = taskRegistry.registerIfAbsent(TaskIdentifier.of(TaskName.of(ASSEMBLE_TASK_NAME), identifier));
+		val variantDependencies = newDependencies((BuildVariantInternal) identifier.getBuildVariant(), identifier, component, project.getObjects(), project.getConfigurations(), project.getDependencies(), project.getExtensions().getByType(ModelLookup.class));
+		return NodeRegistration.unmanaged(identifier.getUnambiguousName(), of(DefaultIosApplicationVariant.class), () -> {
+			val variant = project.getObjects().newInstance(DefaultIosApplicationVariant.class, identifier, variantDependencies, project.getObjects(), project.getProviders(), assembleTask, project.getExtensions().getByType(BinaryViewFactory.class));
+			variant.getProductBundleIdentifier().convention(component.getGroupId().map(it -> it + "." + component.getModuleName().get()));
+			return variant;
+		})
+			.withComponent(identifier)
+			.withComponent(IsVariant.tag())
+			.withComponent(variantDependencies)
+			;
+	}
+
+	private static VariantComponentDependencies<DefaultNativeComponentDependencies> newDependencies(BuildVariantInternal buildVariant, VariantIdentifier<DefaultIosApplicationVariant> variantIdentifier, DefaultIosApplicationComponent component, ObjectFactory objectFactory, ConfigurationContainer configurationContainer, DependencyHandler dependencyHandler, ModelLookup modelLookup) {
+		var variantDependencies = component.getDependencies();
+		if (component.getBuildVariants().get().size() > 1) {
+			val dependencyContainer = objectFactory.newInstance(DefaultComponentDependencies.class, variantIdentifier, new DependencyBucketFactoryImpl(new ConfigurationBucketRegistryImpl(configurationContainer), dependencyHandler));
+			variantDependencies = objectFactory.newInstance(DefaultNativeComponentDependencies.class, dependencyContainer);
+			variantDependencies.configureEach(variantBucket -> {
+				component.getDependencies().findByName(variantBucket.getName()).ifPresent(componentBucket -> {
+					variantBucket.getAsConfiguration().extendsFrom(componentBucket.getAsConfiguration());
+				});
+			});
+		}
+
+		val incomingDependenciesBuilder = DefaultNativeIncomingDependencies.builder(variantDependencies).withVariant(buildVariant).withOwnerIdentifier(variantIdentifier).withBucketFactory(new DependencyBucketFactoryImpl(new ConfigurationBucketRegistryImpl(configurationContainer), dependencyHandler));
+
+		boolean hasSwift = modelLookup.anyMatch(ModelSpecs.of(ModelNodes.withType(of(SwiftSourceSet.class))));
+		if (hasSwift) {
+			incomingDependenciesBuilder.withIncomingSwiftModules();
+		} else {
+			incomingDependenciesBuilder.withIncomingHeaders();
+		}
+
+		NativeIncomingDependencies incoming = incomingDependenciesBuilder.buildUsing(objectFactory);
+		NativeOutgoingDependencies outgoing = objectFactory.newInstance(IosApplicationOutgoingDependencies.class, variantIdentifier, buildVariant, variantDependencies);
+
+		return new VariantComponentDependencies<>(variantDependencies, incoming, outgoing);
 	}
 
 	private static DefaultIosApplicationComponent create(String name, Project project) {
