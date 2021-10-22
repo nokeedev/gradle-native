036608710d430410a5f2815140429a22f34f730f
Daniel Lacasse <daniel@nokee.dev>
2021-10-22 14:29:52 -0400

    Add unit tests for native application/library component

---
+++ subprojects/platform-native/src/test/groovy/dev/nokee/platform/nativebase/NativeApplicationComponentTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.platform.nativebase;
+
+import dev.nokee.internal.testing.TaskMatchers;
+import dev.nokee.internal.testing.util.ProjectTestUtils;
+import dev.nokee.language.base.FunctionalSourceSet;
+import dev.nokee.model.internal.registry.ModelRegistry;
+import dev.nokee.platform.base.Binary;
+import dev.nokee.platform.base.BinaryView;
+import dev.nokee.platform.base.TaskView;
+import dev.nokee.platform.base.VariantView;
+import dev.nokee.platform.base.testers.*;
+import dev.nokee.platform.nativebase.internal.plugins.NativeComponentBasePlugin;
+import lombok.Getter;
+import lombok.val;
+import org.gradle.api.Task;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+
+import static dev.nokee.internal.testing.GradleNamedMatchers.named;
+import static dev.nokee.internal.testing.GradleProviderMatchers.providerOf;
+import static dev.nokee.platform.nativebase.internal.plugins.NativeApplicationPlugin.nativeApplication;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+
+class NativeApplicationComponentTest  implements ComponentTester<NativeApplicationExtension>
+	, HasBaseNameTester
+	, DependencyAwareComponentTester<NativeApplicationComponentDependencies>
+	, VariantAwareComponentTester<VariantView<NativeApplication>>
+	, BinaryAwareComponentTester<BinaryView<Binary>>
+	, TaskAwareComponentTester<TaskView<Task>>
+{
+	private final NativeApplicationExtension subject = createSubject("qico");
+	@Getter @TempDir File testDirectory;
+
+	public NativeApplicationExtension createSubject(String componentName) {
+		val project = ProjectTestUtils.createRootProject(testDirectory);
+		project.getPluginManager().apply(NativeComponentBasePlugin.class);
+		val component = project.getExtensions().getByType(ModelRegistry.class).register(nativeApplication(componentName, project)).as(NativeApplicationExtension.class).get();
+		((FunctionalSourceSet) component.getSources()).get(); // force realize
+		return component;
+	}
+
+	@Override
+	public NativeApplicationExtension subject() {
+		return subject;
+	}
+
+	@Test
+	void hasBaseNameConventionAsComponentName() {
+		subject().getBaseName().set((String) null);
+		assertThat(subject().getBaseName(), providerOf("qico"));
+	}
+
+	@Nested
+	class ComponentTasksTest {
+		public TaskView<Task> subject() {
+			return subject.getTasks();
+		}
+
+		@Test
+		void hasAssembleTask() {
+			assertThat(subject().get(), hasItem(named("assembleQico")));
+		}
+	}
+
+	@Nested
+	class AssembleTaskTest {
+		public Task subject() {
+			return subject.getTasks().filter(it -> it.getName().equals("assembleQico")).get().get(0);
+		}
+
+		@Test
+		public void hasBuildGroup() {
+			assertThat(subject(), TaskMatchers.group("build"));
+		}
+
+		@Test
+		public void hasDescription() {
+			assertThat(subject(), TaskMatchers.description("Assembles the outputs of the native application ':qico'."));
+		}
+	}
+}
---
+++ subprojects/platform-native/src/test/groovy/dev/nokee/platform/nativebase/NativeLibraryComponentTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.platform.nativebase;
+
+import dev.nokee.internal.testing.TaskMatchers;
+import dev.nokee.internal.testing.util.ProjectTestUtils;
+import dev.nokee.language.base.FunctionalSourceSet;
+import dev.nokee.model.internal.registry.ModelRegistry;
+import dev.nokee.platform.base.Binary;
+import dev.nokee.platform.base.BinaryView;
+import dev.nokee.platform.base.TaskView;
+import dev.nokee.platform.base.VariantView;
+import dev.nokee.platform.base.testers.*;
+import dev.nokee.platform.nativebase.internal.plugins.NativeComponentBasePlugin;
+import lombok.Getter;
+import lombok.val;
+import org.gradle.api.Task;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+
+import static dev.nokee.internal.testing.GradleNamedMatchers.named;
+import static dev.nokee.internal.testing.GradleProviderMatchers.providerOf;
+import static dev.nokee.platform.nativebase.internal.plugins.NativeLibraryPlugin.nativeLibrary;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+
+class NativeLibraryComponentTest implements ComponentTester<NativeLibraryExtension>
+	, HasBaseNameTester
+	, DependencyAwareComponentTester<NativeLibraryComponentDependencies>
+	, VariantAwareComponentTester<VariantView<NativeLibrary>>
+	, BinaryAwareComponentTester<BinaryView<Binary>>
+	, TaskAwareComponentTester<TaskView<Task>>
+{
+	private final NativeLibraryExtension subject = createSubject("zasi");
+	@Getter @TempDir File testDirectory;
+
+	public NativeLibraryExtension createSubject(String componentName) {
+		val project = ProjectTestUtils.createRootProject(testDirectory);
+		project.getPluginManager().apply(NativeComponentBasePlugin.class);
+		val component = project.getExtensions().getByType(ModelRegistry.class).register(nativeLibrary(componentName, project)).as(NativeLibraryExtension.class).get();
+		((FunctionalSourceSet) component.getSources()).get(); // force realize
+		return component;
+	}
+
+	@Override
+	public NativeLibraryExtension subject() {
+		return subject;
+	}
+
+	@Test
+	void hasBaseNameConventionAsComponentName() {
+		subject().getBaseName().set((String) null);
+		assertThat(subject().getBaseName(), providerOf("zasi"));
+	}
+
+	@Nested
+	class ComponentTasksTest {
+		public TaskView<Task> subject() {
+			return subject.getTasks();
+		}
+
+		@Test
+		void hasAssembleTask() {
+			assertThat(subject().get(), hasItem(named("assembleZasi")));
+		}
+	}
+
+	@Nested
+	class AssembleTaskTest {
+		public Task subject() {
+			return subject.getTasks().filter(it -> it.getName().equals("assembleZasi")).get().get(0);
+		}
+
+		@Test
+		public void hasBuildGroup() {
+			assertThat(subject(), TaskMatchers.group("build"));
+		}
+
+		@Test
+		public void hasDescription() {
+			assertThat(subject(), TaskMatchers.description("Assembles the outputs of the native library ':zasi'."));
+		}
+	}
+}
