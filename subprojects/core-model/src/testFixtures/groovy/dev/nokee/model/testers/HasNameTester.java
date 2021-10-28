1277a81096609579a48fcabc71d2f615bbc75d9f
Daniel Lacasse <daniel@nokee.dev>
2021-10-28 17:18:51 -0400

    Move HasNameTester to core-model

--- subprojects/core-model/src/testFixtures/groovy/dev/nokee/model/testers/HasNameTester.java
+++ subprojects/core-model/src/testFixtures/groovy/dev/nokee/model/testers/HasNameTester.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.nokee.platform.base.testers;
+package dev.nokee.model.testers;
 
 import dev.nokee.model.HasName;
 import org.junit.jupiter.api.Test;
