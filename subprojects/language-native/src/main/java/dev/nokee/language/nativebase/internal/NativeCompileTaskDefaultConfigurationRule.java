4c8d3f11c50c8d57eb81aee705f3060c6db3158c
Daniel Lacasse <daniel@nokee.dev>
2021-10-31 17:09:47 -0400

    Configure system includes from platform tools

--- subprojects/language-native/src/main/java/dev/nokee/language/nativebase/internal/NativeCompileTaskDefaultConfigurationRule.java
+++ subprojects/language-native/src/main/java/dev/nokee/language/nativebase/internal/NativeCompileTaskDefaultConfigurationRule.java
@@ -21,12 +21,24 @@ import dev.nokee.model.internal.core.ModelActionWithInputs;
 import dev.nokee.model.internal.core.ModelNode;
 import dev.nokee.platform.base.internal.util.PropertyUtils;
 import org.gradle.api.Action;
+import org.gradle.api.Task;
+import org.gradle.language.c.tasks.CCompile;
+import org.gradle.language.cpp.tasks.CppCompile;
 import org.gradle.language.nativeplatform.tasks.AbstractNativeCompileTask;
+import org.gradle.language.objectivec.tasks.ObjectiveCCompile;
+import org.gradle.language.objectivecpp.tasks.ObjectiveCppCompile;
+import org.gradle.nativeplatform.platform.internal.NativePlatformInternal;
+import org.gradle.nativeplatform.toolchain.internal.NativeToolChainInternal;
+import org.gradle.nativeplatform.toolchain.internal.PlatformToolProvider;
+import org.gradle.nativeplatform.toolchain.internal.ToolType;
 
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 
-import static dev.nokee.platform.base.internal.util.PropertyUtils.set;
-import static dev.nokee.platform.base.internal.util.PropertyUtils.wrap;
+import static dev.nokee.platform.base.internal.util.PropertyUtils.*;
 
 public final class NativeCompileTaskDefaultConfigurationRule extends ModelActionWithInputs.ModelAction2<LanguageSourceSetIdentifier, NativeCompileTask> {
 	private final LanguageSourceSetIdentifier identifier;
@@ -39,12 +51,47 @@ public final class NativeCompileTaskDefaultConfigurationRule extends ModelAction
 	protected void execute(ModelNode entity, LanguageSourceSetIdentifier identifier, NativeCompileTask compileTask) {
 		if (identifier.equals(this.identifier)) {
 			compileTask.configure(NativeSourceCompileTask.class, configurePositionIndependentCode(set(false)));
+			compileTask.configure(NativeSourceCompileTask.class, configureSystemIncludes(from(platformTool())));
 		}
 	}
 
 	//region Position independent code
 	private static Action<NativeSourceCompileTask> configurePositionIndependentCode(BiConsumer<? super NativeSourceCompileTask, ? super PropertyUtils.SetAwareProperty<Boolean>> action) {
 		return task -> action.accept(task, wrap(((AbstractNativeCompileTask) task)::setPositionIndependentCode));
+	}
+	//endregion
+
+	//region System includes
+	private static Action<NativeSourceCompileTask> configureSystemIncludes(BiConsumer<? super AbstractNativeCompileTask, ? super PropertyUtils.FileCollectionProperty> action) {
+		return task -> action.accept((AbstractNativeCompileTask) task, wrap(((AbstractNativeCompileTask) task).getSystemIncludes()));
+	}
+
+	private static Function<AbstractNativeCompileTask, Object> platformTool() {
+		return compileTask -> fromPlatformToolOf(compileTask);
+	}
+
+	private static Callable<List<File>> fromPlatformToolOf(AbstractNativeCompileTask compileTask) {
+		return () -> {
+			NativeToolChainInternal toolChain = (NativeToolChainInternal)compileTask.getToolChain().get();
+			NativePlatformInternal targetPlatform = (NativePlatformInternal)compileTask.getTargetPlatform().get();
+			PlatformToolProvider toolProvider = toolChain.select(targetPlatform);
+
+			return toolProvider.getSystemLibraries(toolType(compileTask.getClass())).getIncludeDirs();
+		};
+	}
+
+	private static ToolType toolType(Class<? extends Task> taskType) {
+		if (CCompile.class.isAssignableFrom(taskType)) {
+			return ToolType.CPP_COMPILER;
+		} else if (CppCompile.class.isAssignableFrom(taskType)) {
+			return ToolType.CPP_COMPILER;
+		} else if (ObjectiveCCompile.class.isAssignableFrom(taskType)) {
+			return ToolType.OBJECTIVEC_COMPILER;
+		} else if (ObjectiveCppCompile.class.isAssignableFrom(taskType)) {
+			return ToolType.OBJECTIVECPP_COMPILER;
+		} else {
+			throw new IllegalArgumentException(String.format("Unknown task type, '%s', cannot choose ToolType.", taskType.getSimpleName()));
+		}
 	}
 	//endregion
 }
