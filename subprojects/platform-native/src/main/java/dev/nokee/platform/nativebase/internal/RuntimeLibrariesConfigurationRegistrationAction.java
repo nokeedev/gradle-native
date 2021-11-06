96f6afafee5e540ba4dbb63db9aa849502b2a34f
Daniel Lacasse <daniel@nokee.dev>
2021-11-06 16:15:50 -0400

    Create configuration and link task for shared library binary

---
+++ subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/internal/RuntimeLibrariesConfigurationRegistrationAction.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.platform.nativebase.internal;
+
+import com.google.auto.factory.AutoFactory;
+import com.google.auto.factory.Provided;
+import dev.nokee.language.base.internal.LanguageSourceSetIdentifier;
+import dev.nokee.model.internal.core.ModelActionWithInputs;
+import dev.nokee.model.internal.core.ModelNode;
+import dev.nokee.model.internal.registry.ModelRegistry;
+import dev.nokee.model.internal.state.ModelState;
+import dev.nokee.platform.base.internal.BinaryIdentifier;
+import dev.nokee.platform.base.internal.dependencies.DependencyBucketIdentifier;
+import dev.nokee.platform.base.internal.dependencies.ResolvableDependencyBucketRegistrationFactory;
+import lombok.val;
+import org.gradle.api.Action;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.attributes.Usage;
+import org.gradle.api.model.ObjectFactory;
+
+import static dev.nokee.platform.base.internal.dependencies.DependencyBucketIdentity.resolvable;
+import static dev.nokee.utils.ConfigurationUtils.configureAttributes;
+
+@AutoFactory
+public final class RuntimeLibrariesConfigurationRegistrationAction extends ModelActionWithInputs.ModelAction2<BinaryIdentifier<?>, ModelState.IsAtLeastRegistered> {
+	private final BinaryIdentifier<?> identifier;
+	private final ModelRegistry registry;
+	private final ResolvableDependencyBucketRegistrationFactory resolvableFactory;
+	private final ObjectFactory objects;
+
+	RuntimeLibrariesConfigurationRegistrationAction(BinaryIdentifier<?> identifier, @Provided ModelRegistry registry, @Provided ResolvableDependencyBucketRegistrationFactory resolvableFactory, @Provided ObjectFactory objects) {
+		this.identifier = identifier;
+		this.registry = registry;
+		this.resolvableFactory = resolvableFactory;
+		this.objects = objects;
+	}
+
+	@Override
+	protected void execute(ModelNode entity, BinaryIdentifier<?> identifier, ModelState.IsAtLeastRegistered isAtLeastRegistered) {
+		if (identifier.equals(this.identifier)) {
+			val runtimeLibraries = registry.register(resolvableFactory.create(DependencyBucketIdentifier.of(resolvable("runtimeLibraries"), identifier)));
+			runtimeLibraries.configure(Configuration.class, forNativeRuntimeUsage());
+		}
+	}
+
+	private Action<Configuration> forNativeRuntimeUsage() {
+		return configureAttributes(builder -> builder.usage(objects.named(Usage.class, Usage.NATIVE_RUNTIME)));
+	}
+}
