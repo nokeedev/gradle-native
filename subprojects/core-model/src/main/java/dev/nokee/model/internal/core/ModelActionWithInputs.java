9beb3f9671534e3229a11bfd774c1c2b048a7823
Daniel Lacasse <daniel@nokee.dev>
2021-11-09 15:00:48 -0500

    Allow input component reference configuration on ModelAction

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelActionWithInputs.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelActionWithInputs.java
@@ -17,7 +17,6 @@ package dev.nokee.model.internal.core;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.reflect.TypeToken;
-import dev.nokee.model.internal.type.ModelType;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -54,7 +53,15 @@ public abstract class ModelActionWithInputs implements ModelAction, HasInputs {
 	}
 
 	public static abstract class ModelAction1<I0> extends ModelActionWithInputs {
-		private final ModelComponentReference<I0> i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
+		private final ModelComponentReference<I0> i0;
+
+		protected ModelAction1() {
+			this.i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
+		}
+
+		protected ModelAction1(ModelComponentReference<I0> i0) {
+			this.i0 = i0;
+		}
 
 		@Override
 		public final void execute(ModelNode node, List<?> inputs) {
@@ -88,8 +95,18 @@ public abstract class ModelActionWithInputs implements ModelAction, HasInputs {
 	}
 
 	public static abstract class ModelAction2<I0, I1> extends ModelActionWithInputs {
-		private final ModelComponentReference<I0> i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I1> i1 = ModelComponentReference.of((Class<I1>)new TypeToken<I1>(getClass()) {}.getRawType());
+		private final ModelComponentReference<I0> i0;
+		private final ModelComponentReference<I1> i1;
+
+		protected ModelAction2() {
+			this.i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
+			this.i1 = ModelComponentReference.of((Class<I1>)new TypeToken<I1>(getClass()) {}.getRawType());
+		}
+
+		protected ModelAction2(ModelComponentReference<I0> i0, ModelComponentReference<I1> i1) {
+			this.i0 = i0;
+			this.i1 = i1;
+		}
 
 		@Override
 		public final void execute(ModelNode node, List<?> inputs) {
@@ -123,9 +140,21 @@ public abstract class ModelActionWithInputs implements ModelAction, HasInputs {
 	}
 
 	public static abstract class ModelAction3<I0, I1, I2> extends ModelActionWithInputs {
-		private final ModelComponentReference<I0> i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I1> i1 = ModelComponentReference.of((Class<I1>)new TypeToken<I1>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I2> i2 = ModelComponentReference.of((Class<I2>)new TypeToken<I2>(getClass()) {}.getRawType());
+		private final ModelComponentReference<I0> i0;
+		private final ModelComponentReference<I1> i1;
+		private final ModelComponentReference<I2> i2;
+
+		protected ModelAction3() {
+			this.i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
+			this.i1 = ModelComponentReference.of((Class<I1>)new TypeToken<I1>(getClass()) {}.getRawType());
+			this.i2 = ModelComponentReference.of((Class<I2>)new TypeToken<I2>(getClass()) {}.getRawType());
+		}
+
+		protected ModelAction3(ModelComponentReference<I0> i0, ModelComponentReference<I1> i1, ModelComponentReference<I2> i2) {
+			this.i0 = i0;
+			this.i1 = i1;
+			this.i2 = i2;
+		}
 
 		@Override
 		public final void execute(ModelNode node, List<?> inputs) {
@@ -159,10 +188,24 @@ public abstract class ModelActionWithInputs implements ModelAction, HasInputs {
 	}
 
 	public static abstract class ModelAction4<I0, I1, I2, I3> extends ModelActionWithInputs {
-		private final ModelComponentReference<I0> i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I1> i1 = ModelComponentReference.of((Class<I1>)new TypeToken<I1>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I2> i2 = ModelComponentReference.of((Class<I2>)new TypeToken<I2>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I3> i3 = ModelComponentReference.of((Class<I3>)new TypeToken<I3>(getClass()) {}.getRawType());
+		private final ModelComponentReference<I0> i0;
+		private final ModelComponentReference<I1> i1;
+		private final ModelComponentReference<I2> i2;
+		private final ModelComponentReference<I3> i3;
+
+		protected ModelAction4() {
+			this.i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
+			this.i1 = ModelComponentReference.of((Class<I1>)new TypeToken<I1>(getClass()) {}.getRawType());
+			this.i2 = ModelComponentReference.of((Class<I2>)new TypeToken<I2>(getClass()) {}.getRawType());
+			this.i3 = ModelComponentReference.of((Class<I3>)new TypeToken<I3>(getClass()) {}.getRawType());
+		}
+
+		protected ModelAction4(ModelComponentReference<I0> i0, ModelComponentReference<I1> i1, ModelComponentReference<I2> i2, ModelComponentReference<I3> i3) {
+			this.i0 = i0;
+			this.i1 = i1;
+			this.i2 = i2;
+			this.i3 = i3;
+		}
 
 		@Override
 		public final void execute(ModelNode node, List<?> inputs) {
@@ -196,12 +239,27 @@ public abstract class ModelActionWithInputs implements ModelAction, HasInputs {
 	}
 
 	public static abstract class ModelAction5<I0, I1, I2, I3, I4> extends ModelActionWithInputs {
-		private final ModelComponentReference<I0> i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I1> i1 = ModelComponentReference.of((Class<I1>)new TypeToken<I1>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I2> i2 = ModelComponentReference.of((Class<I2>)new TypeToken<I2>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I3> i3 = ModelComponentReference.of((Class<I3>)new TypeToken<I3>(getClass()) {}.getRawType());
-		private final ModelComponentReference<I4> i4 = ModelComponentReference.of((Class<I4>)new TypeToken<I4>(getClass()) {}.getRawType());
+		private final ModelComponentReference<I0> i0;
+		private final ModelComponentReference<I1> i1;
+		private final ModelComponentReference<I2> i2;
+		private final ModelComponentReference<I3> i3;
+		private final ModelComponentReference<I4> i4;
 
+		protected ModelAction5() {
+			this.i0 = ModelComponentReference.of((Class<I0>)new TypeToken<I0>(getClass()) {}.getRawType());
+			this.i1 = ModelComponentReference.of((Class<I1>)new TypeToken<I1>(getClass()) {}.getRawType());
+			this.i2 = ModelComponentReference.of((Class<I2>)new TypeToken<I2>(getClass()) {}.getRawType());
+			this.i3 = ModelComponentReference.of((Class<I3>)new TypeToken<I3>(getClass()) {}.getRawType());
+			this.i4 = ModelComponentReference.of((Class<I4>)new TypeToken<I4>(getClass()) {}.getRawType());
+		}
+
+		protected ModelAction5(ModelComponentReference<I0> i0, ModelComponentReference<I1> i1, ModelComponentReference<I2> i2, ModelComponentReference<I3> i3, ModelComponentReference<I4> i4) {
+			this.i0 = i0;
+			this.i1 = i1;
+			this.i2 = i2;
+			this.i3 = i3;
+			this.i4 = i4;
+		}
 		@Override
 		public final void execute(ModelNode node, List<?> inputs) {
 			execute(node, (I0) inputs.get(0), (I1) inputs.get(1), (I2) inputs.get(2), (I3) inputs.get(3), (I4) inputs.get(4));
