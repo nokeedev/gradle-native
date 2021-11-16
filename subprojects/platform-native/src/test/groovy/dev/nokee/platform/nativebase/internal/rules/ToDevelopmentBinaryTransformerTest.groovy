2f13878d75386955d9a2031811111faae343aaf7
Daniel Lacasse <daniel@nokee.dev>
2021-11-15 19:31:41 -0500

    Fix test around HasDevelopmentBinary interface extraction

--- subprojects/platform-native/src/test/groovy/dev/nokee/platform/nativebase/internal/rules/ToDevelopmentBinaryTransformerTest.groovy
+++ subprojects/platform-native/src/test/groovy/dev/nokee/platform/nativebase/internal/rules/ToDevelopmentBinaryTransformerTest.groovy
@@ -15,11 +15,13 @@
  */
 package dev.nokee.platform.nativebase.internal.rules
 
+import dev.nokee.platform.base.Binary
 import dev.nokee.platform.base.Variant
 import org.gradle.api.provider.Provider
 import spock.lang.Specification
 import spock.lang.Subject
 
+import static dev.nokee.internal.testing.util.ProjectTestUtils.objectFactory
 import static dev.nokee.platform.nativebase.internal.rules.ToDevelopmentBinaryTransformer.TO_DEVELOPMENT_BINARY
 
 @Subject(ToDevelopmentBinaryTransformer)
@@ -27,7 +29,7 @@ class ToDevelopmentBinaryTransformerTest extends Specification {
 	def "returns variant development binary"() {
 		given:
 		def variant = Mock(Variant)
-		def developmentBinaryProvider = Stub(Provider)
+		def developmentBinaryProvider = objectFactory().property(Binary.class)
 
 		when:
 		def result = TO_DEVELOPMENT_BINARY.transform(variant)
