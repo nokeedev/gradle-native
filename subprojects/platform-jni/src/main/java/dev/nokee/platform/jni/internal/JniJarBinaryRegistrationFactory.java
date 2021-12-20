dc722b8899e1808be7fb7c12c11858fea0389100
Daniel Lacasse <daniel@nokee.dev>
2021-12-20 18:18:13 -0500

    Avoid overriding task description for single variant component

--- subprojects/platform-jni/src/main/java/dev/nokee/platform/jni/internal/JniJarBinaryRegistrationFactory.java
+++ subprojects/platform-jni/src/main/java/dev/nokee/platform/jni/internal/JniJarBinaryRegistrationFactory.java
@@ -48,7 +48,11 @@ public final class JniJarBinaryRegistrationFactory {
 			.action(ModelActionWithInputs.of(ModelComponentReference.of(BinaryIdentifier.class), ModelComponentReference.of(JarTask.class), (entity, id, jarTask) -> {
 				if (id.equals(identifier)) {
 					jarTask.configure(task -> task.getArchiveBaseName().convention(identifier.getName().get()));
-					jarTask.configure(configureDescription("Assembles a JAR archive containing the shared library for %s.", identifier));
+					jarTask.configure(task -> {
+						if (task.getDescription() == null) {
+							task.setDescription(String.format("Assembles a JAR archive containing the shared library for %s.", identifier));
+						}
+					});
 				}
 			}))
 			.build();
