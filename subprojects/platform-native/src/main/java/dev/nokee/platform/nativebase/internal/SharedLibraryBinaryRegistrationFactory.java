bed9806b111f2f9c96802fcbb80a4b71e6c921f9
Daniel Lacasse <daniel@nokee.dev>
2021-12-20 18:19:03 -0500

    Correctly expose header search paths to IDE plugins

--- subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/internal/SharedLibraryBinaryRegistrationFactory.java
+++ subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/internal/SharedLibraryBinaryRegistrationFactory.java
@@ -16,6 +16,8 @@
 package dev.nokee.platform.nativebase.internal;
 
 import dev.nokee.language.base.tasks.SourceCompile;
+import dev.nokee.language.nativebase.tasks.NativeSourceCompile;
+import dev.nokee.language.nativebase.tasks.internal.NativeSourceCompileTask;
 import dev.nokee.model.internal.core.*;
 import dev.nokee.model.internal.state.ModelState;
 import dev.nokee.model.internal.type.ModelType;
@@ -25,14 +27,23 @@ import dev.nokee.platform.nativebase.SharedLibraryBinary;
 import dev.nokee.platform.nativebase.tasks.LinkSharedLibrary;
 import dev.nokee.platform.nativebase.tasks.internal.LinkSharedLibraryTask;
 import dev.nokee.utils.TaskDependencyUtils;
+import lombok.val;
+import org.gradle.api.file.FileSystemLocation;
+import org.gradle.api.file.FileSystemLocationProperty;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
+import org.gradle.api.provider.Provider;
 import org.gradle.api.reflect.HasPublicType;
 import org.gradle.api.reflect.TypeOf;
 import org.gradle.api.tasks.TaskDependency;
 import org.gradle.api.tasks.TaskProvider;
 
+import java.util.Set;
+import java.util.concurrent.Callable;
+
 import static dev.nokee.model.internal.DomainObjectIdentifierUtils.toPath;
 import static dev.nokee.model.internal.core.ModelProjections.createdUsing;
+import static dev.nokee.utils.TransformerUtils.transformEach;
 
 public final class SharedLibraryBinaryRegistrationFactory {
 	private final LinkLibrariesConfigurationRegistrationActionFactory linkLibrariesRegistrationFactory;
@@ -41,14 +52,16 @@ public final class SharedLibraryBinaryRegistrationFactory {
 	private final BaseNamePropertyRegistrationActionFactory baseNamePropertyRegistrationActionFactory;
 	private final RegisterCompileTasksPropertyActionFactory compileTasksPropertyActionFactory;
 	private final AttachAttributesToConfigurationRuleFactory attachAttributesToConfigurationRuleFactory;
+	private final ObjectFactory objectFactory;
 
-	public SharedLibraryBinaryRegistrationFactory(LinkLibrariesConfigurationRegistrationActionFactory linkLibrariesRegistrationFactory, RuntimeLibrariesConfigurationRegistrationActionFactory runtimeLibrariesRegistrationFactory, NativeLinkTaskRegistrationActionFactory linkTaskRegistrationActionFactory, BaseNamePropertyRegistrationActionFactory baseNamePropertyRegistrationActionFactory, RegisterCompileTasksPropertyActionFactory compileTasksPropertyActionFactory, AttachAttributesToConfigurationRuleFactory attachAttributesToConfigurationRuleFactory) {
+	public SharedLibraryBinaryRegistrationFactory(LinkLibrariesConfigurationRegistrationActionFactory linkLibrariesRegistrationFactory, RuntimeLibrariesConfigurationRegistrationActionFactory runtimeLibrariesRegistrationFactory, NativeLinkTaskRegistrationActionFactory linkTaskRegistrationActionFactory, BaseNamePropertyRegistrationActionFactory baseNamePropertyRegistrationActionFactory, RegisterCompileTasksPropertyActionFactory compileTasksPropertyActionFactory, AttachAttributesToConfigurationRuleFactory attachAttributesToConfigurationRuleFactory, ObjectFactory objectFactory) {
 		this.linkLibrariesRegistrationFactory = linkLibrariesRegistrationFactory;
 		this.runtimeLibrariesRegistrationFactory = runtimeLibrariesRegistrationFactory;
 		this.linkTaskRegistrationActionFactory = linkTaskRegistrationActionFactory;
 		this.baseNamePropertyRegistrationActionFactory = baseNamePropertyRegistrationActionFactory;
 		this.compileTasksPropertyActionFactory = compileTasksPropertyActionFactory;
 		this.attachAttributesToConfigurationRuleFactory = attachAttributesToConfigurationRuleFactory;
+		this.objectFactory = objectFactory;
 	}
 
 	public ModelRegistration create(BinaryIdentifier<?> identifier) {
@@ -71,15 +84,20 @@ public final class SharedLibraryBinaryRegistrationFactory {
 			.action(new ConfigureLinkTaskTargetPlatformFromBuildVariantRule(identifier))
 			.action(ModelActionWithInputs.of(ModelComponentReference.of(BinaryIdentifier.class), ModelComponentReference.of(ModelState.IsAtLeastRegistered.class), (entity, id, ignored) -> {
 				if (id.equals(identifier)) {
-					entity.addComponent(createdUsing(ModelType.of(SharedLibraryBinary.class), ModelBackedSharedLibraryBinary::new));
+					entity.addComponent(createdUsing(ModelType.of(SharedLibraryBinary.class), () -> new ModelBackedSharedLibraryBinary(objectFactory)));
 				}
 			}))
 			.build();
 	}
 
-	private static final class ModelBackedSharedLibraryBinary implements SharedLibraryBinary, HasPublicType, ModelNodeAware, ModelBackedNamedMixIn {
+	private static final class ModelBackedSharedLibraryBinary implements SharedLibraryBinary, HasPublicType, ModelNodeAware, ModelBackedNamedMixIn, HasHeaderSearchPaths {
 		private final ModelNode node = ModelNodeContext.getCurrentModelNode();
 		private final NativeBinaryBuildable isBuildable = new NativeBinaryBuildable(this);
+		private final ObjectFactory objectFactory;
+
+		public ModelBackedSharedLibraryBinary(ObjectFactory objectFactory) {
+			this.objectFactory = objectFactory;
+		}
 
 		@Override
 		public TaskView<SourceCompile> getCompileTasks() {
@@ -114,6 +132,13 @@ public final class SharedLibraryBinaryRegistrationFactory {
 		@Override
 		public Property<String> getBaseName() {
 			return ModelProperties.getProperty(this, "baseName").as(Property.class).get();
+		}
+
+		@Override
+		public Provider<Set<FileSystemLocation>> getHeaderSearchPaths() {
+			val result = objectFactory.fileCollection();
+			result.from((Callable<Object>) getCompileTasks().withType(NativeSourceCompile.class).map(it -> it.getHeaderSearchPaths().map(transformEach(path -> path.getAsFile())))::get);
+			return result.getElements();
 		}
 	}
 }
