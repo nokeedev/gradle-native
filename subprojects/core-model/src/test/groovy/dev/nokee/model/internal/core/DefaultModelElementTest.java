02f59add4fd51d2246a94f3a46437f88935b8f09
Daniel Lacasse <daniel@nokee.dev>
2021-12-29 09:44:44 -0500

    Focus forward tests on delegates instead of full API contract

--- subprojects/core-model/src/test/groovy/dev/nokee/model/internal/core/DefaultModelElementTest.java
+++ subprojects/core-model/src/test/groovy/dev/nokee/model/internal/core/DefaultModelElementTest.java
@@ -21,53 +21,31 @@ import dev.nokee.model.internal.ConfigurableStrategy;
 import dev.nokee.model.internal.InstanceOfOperatorStrategy;
 import dev.nokee.model.internal.TypeCastOperatorStrategy;
 import dev.nokee.model.internal.type.ModelType;
-import dev.nokee.utils.ActionTestUtils;
 import lombok.val;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatcher;
 import org.mockito.Mockito;
 
 import java.util.function.Supplier;
-import java.util.stream.Stream;
 
 import static dev.nokee.model.internal.type.ModelType.of;
+import static dev.nokee.utils.ActionTestUtils.doSomething;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-class DefaultModelElementTest implements ModelElementTester {
-	private static final ModelType<MyType> KNOWN_TYPE = of(MyType.class);
+class DefaultModelElementTest {
 	@SuppressWarnings("unchecked") private final Supplier<String> nameSupplier = Mockito.mock(Supplier.class);
-	@SuppressWarnings("unchecked") private final Supplier<String> displayNameSupplier = Mockito.mock(Supplier.class);
 	private final ConfigurableStrategy configurableStrategy = Mockito.mock(ConfigurableStrategy.class);
 	private final InstanceOfOperatorStrategy instanceOfStrategy = Mockito.mock(InstanceOfOperatorStrategy.class);
 	private final TypeCastOperatorStrategy typeCastOperatorStrategy = Mockito.mock(TypeCastOperatorStrategy.class);
 	private final ModelPropertyLookupStrategy propertyLookupStrategy = Mockito.mock(ModelPropertyLookupStrategy.class);
-	private final DefaultModelElement subject = new DefaultModelElement(nameSupplier, displayNameSupplier, configurableStrategy, instanceOfStrategy, typeCastOperatorStrategy, propertyLookupStrategy);
+	private final DefaultModelElement subject = new DefaultModelElement(nameSupplier, configurableStrategy, instanceOfStrategy, typeCastOperatorStrategy, propertyLookupStrategy);
 
-	@BeforeEach
-	void mockName() {
-		when(nameSupplier.get()).thenReturn("test");
-		when(displayNameSupplier.get()).thenReturn("element 'test'");
-		when(instanceOfStrategy.instanceOf(argThat(notA(KNOWN_TYPE)))).thenReturn(false);
-		when(instanceOfStrategy.instanceOf(KNOWN_TYPE)).thenReturn(true);
-		when(instanceOfStrategy.getCastableTypes()).thenAnswer(it -> Stream.of(KNOWN_TYPE));
-		when(typeCastOperatorStrategy.castTo(argThat(notA(KNOWN_TYPE)))).thenThrow(ClassCastException.class);
-	}
-
-	private static <T> ArgumentMatcher<T> notA(T type) {
-		return argument -> !type.equals(argument);
-	}
-
-	public ModelElement subject() {
-		return subject;
-	}
-
-	@Override
-	public ModelType<?> aKnownType() {
-		return of(MyType.class);
+	@Test
+	@SuppressWarnings("UnstableApiUsage")
+	void checkNullsOnPublicMethods() {
+		new NullPointerTester().setDefault(ModelType.class, ModelType.untyped()).testAllPublicInstanceMethods(subject);
 	}
 
 	@Test
@@ -77,30 +55,28 @@ class DefaultModelElementTest implements ModelElementTester {
 	}
 
 	@Test
-	void forwardsConfigureUsingActionToStrategy() {
-		val action = ActionTestUtils.mockAction(MyType.class);
-		subject.configure(KNOWN_TYPE, action);
-		verify(configurableStrategy).configure(KNOWN_TYPE, action);
+	void forwardsConfigureUsingModelTypeAndActionToStrategy() {
+		subject.configure(of(MyType.class), doSomething());
+		verify(configurableStrategy).configure(of(MyType.class), doSomething());
 	}
 
 	@Test
 	void forwardsNameQueryToSupplier() {
-		assertEquals("test", subject.getName());
+		val expectedName = "zalu";
+		when(nameSupplier.get()).thenReturn(expectedName);
+
+		assertEquals(expectedName, subject.getName());
 		verify(nameSupplier).get();
 	}
 
 	@Test
-	void forwardsDisplayNameQueryToSupplier() {
-		assertEquals("element 'test'", subject.getDisplayName());
-		verify(displayNameSupplier).get();
-	}
-
-	@Test
 	void forwardsInstanceOfToStrategy() {
+		when(instanceOfStrategy.instanceOf(of(MyType.class))).thenReturn(Boolean.TRUE);
+		when(instanceOfStrategy.instanceOf(of(WrongType.class))).thenReturn(Boolean.FALSE);
 		assertAll(
 			() -> {
-				assertTrue(subject.instanceOf(KNOWN_TYPE));
-				verify(instanceOfStrategy).instanceOf(KNOWN_TYPE);
+				assertTrue(subject.instanceOf(of(MyType.class)));
+				verify(instanceOfStrategy).instanceOf(of(MyType.class));
 			},
 			() -> {
 				assertFalse(subject.instanceOf(of(WrongType.class)));
@@ -110,19 +86,21 @@ class DefaultModelElementTest implements ModelElementTester {
 	}
 
 	@Test
-	void forwardsKnownTypeCastToStrategy() {
-		@SuppressWarnings("unchecked")
-		final DomainObjectProvider<MyType> result = Mockito.mock(DomainObjectProvider.class);
-		when(typeCastOperatorStrategy.castTo(KNOWN_TYPE)).thenReturn(result);
-		assertEquals(result, subject.as(KNOWN_TYPE));
-		verify(typeCastOperatorStrategy).castTo(KNOWN_TYPE);
+	@SuppressWarnings("unchecked")
+	void forwardsTypeCastOperatorUsingModelTypeToStrategy() {
+		val result = Mockito.mock(DomainObjectProvider.class);
+		when(typeCastOperatorStrategy.castTo(any())).thenReturn(result);
+
+		assertSame(result, subject.as(of(MyType.class)));
+		verify(typeCastOperatorStrategy).castTo(of(MyType.class));
 	}
 
 	@Test
 	void forwardsPropertyQueryToStrategy() {
 		val result = Mockito.mock(ModelElement.class);
 		when(propertyLookupStrategy.get(any())).thenReturn(result);
-		assertEquals(result, subject.property("ripa"));
+
+		assertSame(result, subject.property("ripa"));
 		verify(propertyLookupStrategy).get("ripa");
 	}
 
@@ -130,13 +108,6 @@ class DefaultModelElementTest implements ModelElementTester {
 	void throwsExceptionWhenCastingUsingGroovyTypeCastOperator() {
 		val ex = assertThrows(UnsupportedOperationException.class, () -> subject.asType(MyType.class));
 		assertEquals("Use ModelElement#as(ModelType) instead.", ex.getMessage());
-	}
-
-	@Test
-	void includesDisplayNameInTypeCastException() {
-		when(displayNameSupplier.get()).thenReturn("some element named 'test'");
-		val ex = assertThrows(ClassCastException.class, () -> subject.as(of(WrongType.class)));
-		assertEquals("Could not cast some element named 'test' to WrongType. Available instances: MyType.", ex.getMessage());
 	}
 
 	public interface MyType {}
