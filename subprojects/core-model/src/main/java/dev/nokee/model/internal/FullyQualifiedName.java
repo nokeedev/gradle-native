df39690644e4b1f8ccd6698e553730733e466075
Daniel Lacasse <daniel@nokee.dev>
2021-12-31 22:06:58 -0500

    Use FullyQualifiedName in component

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/FullyQualifiedName.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/FullyQualifiedName.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.nokee.platform.base.internal;
+package dev.nokee.model.internal;
 
 public final class FullyQualifiedName {
 	private final String name;
