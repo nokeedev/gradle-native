6ad03d7531b6f7a95771695946e31ae20db16f71
Daniel Lacasse <daniel@nokee.dev>
2022-01-25 07:44:13 -0500

    Migrate to Nokee native plugins instead of core native plugins
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/docs/src/docs/samples/jni-library-composing-from-source/kotlin-dsl/cpp-jni-greeter/build.gradle.kts
+++ subprojects/docs/src/docs/samples/jni-library-composing-from-source/kotlin-dsl/cpp-jni-greeter/build.gradle.kts
@@ -1,27 +1,31 @@
 import org.gradle.internal.jvm.Jvm
+import dev.nokee.platform.nativebase.NativeBinary
 
 plugins {
-	id("cpp-library")
+	id("dev.nokee.cpp-library")
 }
 
 description = "The JNI shared library, also known as the native bindings."
 
 library {
 	// The native component of the JNI library needs to be a shared library.
-	linkage.set(listOf(Linkage.SHARED))
+	targetLinkages.set(listOf(linkages.shared))
 	dependencies {
 		implementation(project(":cpp-greeter"))
 	}
 
-	binaries.configureEach {
-		compileTask.get().includes.from(compileTask.get().targetPlatform.map {
-			listOf(File("${Jvm.current().javaHome.canonicalPath}/include")) + when {
-				it.operatingSystem.isMacOsX -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/darwin"))
-				it.operatingSystem.isLinux -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/linux"))
-				it.operatingSystem.isWindows -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/win32"))
-				else -> emptyList()
-			}
-		})
-		compileTask.get().setPositionIndependentCode(true)
+	binaries.configureEach(NativeBinary::class.java) {
+		compileTasks.configureEach {
+			includes.from(compileTask.get().targetPlatform.map {
+				listOf(File("${Jvm.current().javaHome.canonicalPath}/include")) + when {
+					it.operatingSystem.isMacOsX -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/darwin"))
+					it.operatingSystem.isLinux -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/linux"))
+					it.operatingSystem.isWindows -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/win32"))
+					it.operatingSystem.isFreeBSD -> listOf(File("${Jvm.current().javaHome.absolutePath}/include/freebsd"))
+					else -> emptyList()
+				}
+			})
+			((AbstractNativeCompileTask) this).isPositionIndependentCode = true
+		}
 	}
 }
