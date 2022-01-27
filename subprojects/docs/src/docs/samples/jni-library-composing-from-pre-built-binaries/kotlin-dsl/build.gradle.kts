5976c33983e2d319f0fbf3a6310155fdb44b7189
Daniel Lacasse <daniel@nokee.dev>
2022-01-27 17:08:19 -0500

    Add pre-compiled shared library for FreeBSD systems
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/docs/src/docs/samples/jni-library-composing-from-pre-built-binaries/kotlin-dsl/build.gradle.kts
+++ subprojects/docs/src/docs/samples/jni-library-composing-from-pre-built-binaries/kotlin-dsl/build.gradle.kts
@@ -13,12 +13,14 @@ fun getLibraryFilePathFor(osFamily: OperatingSystemFamily): String {
 		return "pre-built-libraries/linux/libjni-greeter.so"
 	} else if (osFamily.isMacOs) {
 		return "pre-built-libraries/macos/libjni-greeter.dylib"
+	} else if (osFamily.isFreeBSD) {
+		return "pre-built-libraries/linux/libjni-greeter.so"
 	}
 	throw GradleException("Unknown operating system family '${osFamily}'.")
 }
 
 library {
-	targetMachines.set(listOf(machines.windows.x86_64, machines.linux.x86_64, machines.macOS.x86_64))
+	targetMachines.set(listOf(machines.windows.x86_64, machines.linux.x86_64, machines.macOS.x86_64, machines.freeBSD.x86_64))
 	variants.configureEach {
 		val prebuiltLibraryFile = file(getLibraryFilePathFor(targetMachine.operatingSystemFamily))
 		nativeRuntimeFiles.setFrom(prebuiltLibraryFile)
