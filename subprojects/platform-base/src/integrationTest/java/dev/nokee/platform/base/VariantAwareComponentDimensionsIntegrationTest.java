d0753ea03bc229cd746bec2362e0d3f8d438769a
Daniel Lacasse <daniel@nokee.dev>
2022-02-14 21:51:42 -0500

    Provide integration test for VariantAwareComponent dimensions property
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/platform-base/src/integrationTest/java/dev/nokee/platform/base/VariantAwareComponentDimensionsIntegrationTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.platform.base;
+
+import dev.nokee.internal.testing.util.ProjectTestUtils;
+import dev.nokee.model.internal.DomainObjectIdentifierUtils;
+import dev.nokee.model.internal.ProjectIdentifier;
+import dev.nokee.model.internal.core.ModelProjections;
+import dev.nokee.model.internal.core.ModelRegistration;
+import dev.nokee.model.internal.registry.ModelRegistry;
+import dev.nokee.platform.base.internal.ModelBackedVariantAwareComponentMixIn;
+import dev.nokee.platform.base.internal.plugins.ComponentModelBasePlugin;
+import dev.nokee.platform.base.testers.VariantDimensionsIntegrationTester;
+import org.gradle.api.Project;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static dev.nokee.model.internal.type.ModelType.of;
+import static dev.nokee.platform.base.internal.ComponentIdentifier.ofMain;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class VariantAwareComponentDimensionsIntegrationTest extends VariantDimensionsIntegrationTester {
+	private final Project project = ProjectTestUtils.rootProject();
+	private VariantAwareComponent<Variant> subject;
+
+	@BeforeEach
+	void applyPlugins() {
+		project.getPluginManager().apply(ComponentModelBasePlugin.class);
+		subject = project.getExtensions().getByType(ModelRegistry.class).register(ModelRegistration.builder()
+			.withComponent(ofMain(ProjectIdentifier.of(project)))
+			.withComponent(DomainObjectIdentifierUtils.toPath(ofMain(ProjectIdentifier.of(project))))
+			.withComponent(ModelProjections.managed(of(MyComponent.class))).build()).as(MyComponent.class).get();
+	}
+
+	@Test
+	void hasVariantDimensions() {
+		assertNotNull(subject.getDimensions());
+	}
+
+	@Override
+	public VariantAwareComponent<?> subject() {
+		return subject;
+	}
+
+	public interface MyComponent extends ModelBackedVariantAwareComponentMixIn<Variant> {}
+}
