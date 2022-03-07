2e63dc19bffdb8b0e4a6794ad64afb7b292f663f
Daniel Lacasse <daniel@nokee.dev>
2022-03-07 12:10:05 -0500

    Remove old functional tests for samples and documentation
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- gradle/plugins/docs-gradle-plugin/src/functionalTest/groovy/dev/nokee/docs/AbstractDocumentationFunctionalSpec.groovy
+++
@@ -1,7 +0,0 @@
-package dev.nokee.docs
-
-import dev.gradleplugins.integtests.fixtures.AbstractGradleSpecification
-
-abstract class AbstractDocumentationFunctionalSpec extends AbstractGradleSpecification implements DocumentationTaskNames, DocumentationPluginFixture {
-
-}
