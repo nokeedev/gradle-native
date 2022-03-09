73e6e85865fb14bae0d897a1ef78f2db80dcb30f
Daniel Lacasse <daniel@nokee.dev>
2022-03-08 19:05:12 -0500

    Upgrade to Gradle 6.9 and remove Nokee init script
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- gradlew.bat
+++ gradlew.bat
@@ -71,10 +71,7 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
 @rem Execute Gradle
-set NOKEE_INIT_SCRIPT_FILE=%APP_HOME%\gradle\nokee.init.gradle
-if exist "%NOKEE_INIT_SCRIPT_FILE%" set NOKEE_ARGS=--init-script "%NOKEE_INIT_SCRIPT_FILE%"
-
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %NOKEE_ARGS% %*
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
 @rem End local scope for the variables with windows NT shell
