7e1ce8f76a09fe05a57c1281e48311b8ca411f5b
Daniel Lacasse <daniel@nokee.dev>
2022-03-31 12:47:42 -0400

    Allow tasks registration using Gradle build script model
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ gradle/libraries/gradle-build-script/src/main/java/dev/gradleplugins/buildscript/blocks/TaskBlock.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.gradleplugins.buildscript.blocks;
+
+import dev.gradleplugins.buildscript.Visitor;
+import dev.gradleplugins.buildscript.statements.BlockStatement;
+import dev.gradleplugins.buildscript.statements.EmptyAwareSection;
+import dev.gradleplugins.buildscript.statements.GroupStatement;
+import dev.gradleplugins.buildscript.statements.Statement;
+import dev.gradleplugins.buildscript.syntax.Expression;
+
+import static dev.gradleplugins.buildscript.syntax.Syntax.literal;
+
+public final class TaskBlock extends AbstractBlock {
+
+	private TaskBlock(Statement delegate) {
+		super(delegate);
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static final class Builder {
+		private final GroupStatement.Builder builder = GroupStatement.builder();
+
+		public Builder doLast() {
+			builder.add(BlockStatement.of(literal("doLast"), new EmptyStatement()));
+			return this;
+		}
+
+		public TaskBlock build() {
+			return new TaskBlock(builder.build());
+		}
+	}
+
+	private static final class EmptyStatement implements Statement, EmptyAwareSection {
+		@Override
+		public boolean isEmpty() {
+			return false;
+		}
+
+		@Override
+		public Statement prefixWith(Expression prefix) {
+			return this;
+		}
+
+		@Override
+		public void accept(Visitor visitor) {
+			// do nothing
+		}
+	}
+}
---
+++ gradle/libraries/gradle-build-script/src/main/java/dev/gradleplugins/buildscript/blocks/TasksBlock.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.gradleplugins.buildscript.blocks;
+
+import dev.gradleplugins.buildscript.GradleBuildScriptBlockBuilder;
+import dev.gradleplugins.buildscript.statements.BlockStatement;
+import dev.gradleplugins.buildscript.statements.ExpressionStatement;
+import dev.gradleplugins.buildscript.statements.GroupStatement;
+import dev.gradleplugins.buildscript.statements.SingleLineCommentBlock;
+import dev.gradleplugins.buildscript.statements.Statement;
+
+import java.util.function.Consumer;
+
+import static dev.gradleplugins.buildscript.syntax.Syntax.invoke;
+import static dev.gradleplugins.buildscript.syntax.Syntax.literal;
+import static dev.gradleplugins.buildscript.syntax.Syntax.string;
+
+public final class TasksBlock extends AbstractBlock {
+	private TasksBlock(Statement delegate) {
+		super(delegate);
+	}
+
+	public static Statement tasks(Consumer<? super TasksBlock.Builder> action) {
+		final TasksBlock.Builder builder = new TasksBlock.Builder();
+		action.accept(builder);
+		return BlockStatement.of(literal("tasks"), builder.build()).useGetter();
+	}
+
+	public static final class Builder implements GradleBuildScriptBlockBuilder<TasksBlock.Builder> {
+		private final GroupStatement.Builder builder = GroupStatement.builder();
+
+		public Builder register(String name) {
+			builder.add(ExpressionStatement.of(invoke("register", string(name)).alwaysUseParenthesis()));
+			return this;
+		}
+
+		public Builder register(String name, Consumer<? super TaskBlock.Builder> action) {
+			TaskBlock.Builder taskBuilder = TaskBlock.builder();
+			action.accept(taskBuilder);
+			builder.add(BlockStatement.of(invoke("register", string(name)).alwaysUseParenthesis(), taskBuilder.build()));
+			return this;
+		}
+
+		public TasksBlock build() {
+			return new TasksBlock(builder.build());
+		}
+
+		@Override
+		public Builder comment(String comment, Consumer<? super TasksBlock.Builder> action) {
+			final Builder builder = new Builder();
+			action.accept(builder);
+			this.builder.add(SingleLineCommentBlock.comment(comment, builder.build()));
+			return this;
+		}
+	}
+}
