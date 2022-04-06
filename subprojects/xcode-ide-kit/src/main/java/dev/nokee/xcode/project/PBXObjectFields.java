c81c6bbd5708a556c381c2af3258323069ad443c
Daniel Lacasse <daniel@nokee.dev>
2022-04-06 10:03:38 -0400

    Rewrite the project.pbxproj writer using an intermediate model
    The pbxproj file is not a straight forward PBXProject serialization.
    There is an intermediate transformation which convert the PBXProject
    into a simpler dict/array/primitive/reference bag of objects. This
    transformation allocate Global IDs which are used inside Xcode schemes.
    Given we want the model to be immutable once created, we can't simply
    mutate model to add the Global IDs like we used to do (aka what Buck
    do). Instead, we flush down the model into a more primitive one which
    includes the Global IDs and references to other objects.
    
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/xcode-ide-kit/src/main/java/dev/nokee/xcode/project/PBXObjectFields.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.xcode.project;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Map;
+import java.util.Set;
+
+public final class PBXObjectFields {
+	private final ImmutableMap<String, Object> fields;
+
+	private PBXObjectFields(ImmutableMap<String, Object> fields) {
+		this.fields = fields;
+	}
+
+	public Object get(String name) {
+		return fields.get(name);
+	}
+
+	public int size() {
+		return fields.size();
+	}
+
+	public Set<Map.Entry<String, Object>> entrySet() {
+		return fields.entrySet();
+	}
+
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static final class Builder {
+		private final ImmutableMap.Builder<String, Object> builder = ImmutableMap.builder();
+
+		public Builder putField(String name, Object value) {
+			builder.put(name, value);
+			return this;
+		}
+
+		public PBXObjectFields build() {
+			return new PBXObjectFields(builder.build());
+		}
+	}
+}
