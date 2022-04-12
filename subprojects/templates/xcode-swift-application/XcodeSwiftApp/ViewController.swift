4db61ed090f676fda1313383fba69cbb15409fd3
Daniel Lacasse <daniel@nokee.dev>
2022-04-12 12:26:33 -0400

    Try gold Xcode project to be used as-in in testing
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/templates/xcode-swift-application/XcodeSwiftApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  XcodeSwiftApp
+//
+//  Created by Daniel Lacasse on 2022-04-07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
