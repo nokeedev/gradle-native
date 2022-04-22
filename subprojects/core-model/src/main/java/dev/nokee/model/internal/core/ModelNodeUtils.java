15811bd7594c675572764242d8cddf32c00336d5
Daniel Lacasse <daniel@nokee.dev>
2022-04-22 17:14:38 -0400

    Use component bits to test for projection viewing
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelNodeUtils.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/core/ModelNodeUtils.java
@@ -66,7 +66,7 @@ public final class ModelNodeUtils {
 	 * @return true if the node can be projected into the specified type, or false otherwise.
 	 */
 	public static boolean canBeViewedAs(ModelNode self, ModelType<?> type) {
-		return getProjections(self).anyMatch(it -> it.canBeViewedAs(type));
+		return self.getComponentBits().containsAll(ModelComponentType.projectionOf(type.getConcreteType()).familyBits());
 	}
 
 	public static Stream<ModelProjection> getProjections(ModelNode self) {
