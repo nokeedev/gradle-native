7baeba12e559742b4e3f6039aeefd2e782c251c9
Daniel Lacasse <daniel@nokee.dev>
2022-04-25 14:00:02 -0400

    Use component for parent configuration dependencies
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/dependencies/ExtendsFromParentConfigurationAction.java
+++ subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/dependencies/ExtendsFromParentConfigurationAction.java
@@ -15,42 +15,33 @@
  */
 package dev.nokee.platform.base.internal.dependencies;
 
-import com.google.common.collect.Iterables;
 import dev.nokee.model.internal.actions.ModelAction;
 import dev.nokee.model.internal.core.ModelNode;
 import dev.nokee.model.internal.core.ModelNodeUtils;
-import dev.nokee.model.internal.core.ModelNodes;
-import dev.nokee.model.internal.core.ModelPath;
-import dev.nokee.model.internal.core.ModelPathComponent;
-import dev.nokee.model.internal.core.ModelSpecs;
-import dev.nokee.model.internal.registry.ModelLookup;
+import dev.nokee.model.internal.core.ParentComponent;
+import dev.nokee.model.internal.core.ParentUtils;
+import dev.nokee.model.internal.names.ElementNameComponent;
+import dev.nokee.platform.base.internal.dependencybuckets.LinkedConfiguration;
 import lombok.val;
-import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 
-import java.util.Optional;
-
-import static dev.nokee.model.internal.core.ModelComponentType.componentOf;
-
 public final class ExtendsFromParentConfigurationAction implements ModelAction {
-	private final Project project;
-	private final ModelPath path;
-
-	public ExtendsFromParentConfigurationAction(Project project, ModelPath path) {
-		this.project = project;
-		this.path = path;
-	}
-
 	@Override
 	public void execute(ModelNode entity) {
-		val p = entity.get(ModelPathComponent.class);
+		val parent = entity.get(ParentComponent.class);
+		val name = entity.get(ElementNameComponent.class).get();
 		val configuration = ModelNodeUtils.get(entity, Configuration.class);
-		val parentConfigurationResult = project.getExtensions().getByType(ModelLookup.class).query(ModelSpecs.of(ModelNodes.withPath(path.getParent().get().child(p.get().getName()))));
-		Optional.ofNullable(Iterables.getOnlyElement(parentConfigurationResult.get(), null)).ifPresent(parentConfigurationEntity -> {
-			val parentConfiguration = ModelNodeUtils.get(parentConfigurationEntity, Configuration.class);
-			if (!parentConfiguration.getName().equals(configuration.getName())) {
-				configuration.extendsFrom(parentConfiguration);
-			}
-		});
+		ParentUtils.stream(parent)
+			.flatMap(it -> it.getComponents()
+				.filter(LinkedConfiguration.class::isInstance)
+				.map(component -> ((LinkedConfiguration) component).get())
+				.filter(bucketEntity -> bucketEntity
+					.find(ElementNameComponent.class)
+					.map(nameComponent -> nameComponent.get().equals(name))
+					.orElse(false)))
+			.map(bucketEntity -> ModelNodeUtils.get(bucketEntity, Configuration.class))
+			.filter(parentConfiguration -> !parentConfiguration.getName().equals(configuration.getName()))
+			.findFirst()
+			.ifPresent(configuration::extendsFrom);
 	}
 }
