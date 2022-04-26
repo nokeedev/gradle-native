5e2a3ed0b7352d2c9f7b2bde5c972c03f4d7f7e4
Daniel Lacasse <daniel@nokee.dev>
2022-04-26 12:27:13 -0400

    Move link and runtime libraries configuration integration test
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/platform-native/src/integrationTest/java/dev/nokee/platform/nativebase/NativeLinkLibrariesConfigurationIntegrationTester.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.platform.nativebase;
+
+import dev.nokee.internal.testing.IntegrationTest;
+import dev.nokee.internal.testing.junit.jupiter.GradleProject;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.attributes.Usage;
+import org.gradle.api.internal.artifacts.configurations.ConfigurationInternal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static dev.nokee.internal.testing.ConfigurationMatchers.attributes;
+import static dev.nokee.internal.testing.ConfigurationMatchers.description;
+import static dev.nokee.internal.testing.ConfigurationMatchers.resolvable;
+import static dev.nokee.internal.testing.GradleNamedMatchers.named;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.is;
+
+@IntegrationTest
+abstract class NativeLinkLibrariesConfigurationIntegrationTester<T extends NativeBinary> {
+	@GradleProject protected Project project;
+	Configuration subject;
+
+	public abstract T createSubject(String name);
+
+	@BeforeEach
+	void initializeSubject() {
+		createSubject("fifi");
+		this.subject = realize(project.getConfigurations().getByName("fifiLinkLibraries"));
+	}
+
+	private static Configuration realize(Configuration self) {
+		((ConfigurationInternal) self).preventFromFurtherMutation();
+		return self;
+	}
+
+	@Test
+	void hasDescription() {
+		assertThat(subject, description("Link libraries for binary ':fifi'."));
+	}
+
+	@Test
+	void isResolvable() {
+		assertThat(subject, resolvable());
+	}
+
+	@Test
+	void hasNativeLinkUsage() {
+		assertThat(subject, attributes(hasEntry(is(Usage.USAGE_ATTRIBUTE), named("native-link"))));
+	}
+}
