990bab1113ee38e895cf539fcb5c8ea44ea71033
Daniel Lacasse <daniel@nokee.dev>
2022-04-26 08:45:11 -0400

    Fix typo in NativeBinary javadoc
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/NativeBinary.java
+++ subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/NativeBinary.java
@@ -21,14 +21,14 @@ import dev.nokee.platform.base.TaskView;
 
 public interface NativeBinary extends Binary {
 	/**
-	 * Returns a view of all the compile tasks that participate to compiling all the object files for this binary.
+	 * Returns a view of all compile tasks that participate to compiling all the object files for this binary.
 	 *
 	 * @return a view of {@link SourceCompile} tasks, never null.
 	 */
 	TaskView<SourceCompile> getCompileTasks();
 
 	/**
-	 * Returns whether or not this binary can be built in the current environment.
+	 * Returns whether this binary can be built in the current environment.
 	 *
 	 * @return {@code true} if this binary can be built in the current environment or {@code false} otherwise.
 	 */
