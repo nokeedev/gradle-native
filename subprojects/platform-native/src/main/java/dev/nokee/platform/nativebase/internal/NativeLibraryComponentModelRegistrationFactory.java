3a9fc6491aca1f37a1b8ed391fce120108d9b15d
Daniel Lacasse <daniel@nokee.dev>
2022-05-03 09:08:34 -0400

    Use isAtLeast instead of equals for state change
    Due to an issue with internal state changes, we need to be lenient when
    state changes as some state can be skipped.
    
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/internal/NativeLibraryComponentModelRegistrationFactory.java
+++ subprojects/platform-native/src/main/java/dev/nokee/platform/nativebase/internal/NativeLibraryComponentModelRegistrationFactory.java
@@ -120,7 +120,7 @@ public final class NativeLibraryComponentModelRegistrationFactory {
 				private boolean alreadyExecuted = false;
 				@Override
 				public void execute(ModelNode entity, ModelPathComponent path, ModelState state) {
-					if (entityPath.equals(path.get()) && state.equals(ModelState.Registered) && !alreadyExecuted) {
+					if (entityPath.equals(path.get()) && state.isAtLeast(ModelState.Registered) && !alreadyExecuted) {
 						alreadyExecuted = true;
 						val registry = project.getExtensions().getByType(ModelRegistry.class);
 
