b2545f8cb0384827ea3e66733185a0521a8ed1b3
Daniel Lacasse <daniel@nokee.dev>
2022-05-24 10:34:01 -0400

    Introduce the distributions management plugin
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/distributions-management/src/functionalTest/java/dev/nokee/init/ConfigurationCacheDetectsChangesToDotNokeeRCFileFunctionalTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.init;
+
+import dev.gradleplugins.runnerkit.BuildResult;
+import dev.gradleplugins.runnerkit.GradleRunner;
+import dev.nokee.internal.testing.junit.jupiter.ContextualGradleRunnerParameterResolver;
+import dev.nokee.internal.testing.junit.jupiter.GradleFeatureRequirement;
+import dev.nokee.internal.testing.junit.jupiter.RequiresGradleFeature;
+import net.nokeedev.testing.junit.jupiter.io.TestDirectory;
+import net.nokeedev.testing.junit.jupiter.io.TestDirectoryExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+import static dev.gradleplugins.buildscript.blocks.PluginsBlock.plugins;
+import static dev.nokee.init.fixtures.DotNokeeRCTestUtils.writeRcFileTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+
+@RequiresGradleFeature(GradleFeatureRequirement.CONFIGURATION_CACHE)
+@ExtendWith({TestDirectoryExtension.class, ContextualGradleRunnerParameterResolver.class})
+class ConfigurationCacheDetectsChangesToDotNokeeRCFileFunctionalTest {
+	@TestDirectory Path testDirectory;
+	GradleRunner executer;
+	BuildResult result;
+
+	@BeforeEach
+	void setup(GradleRunner runner) throws IOException {
+		writeRcFileTo(testDirectory, "0.3.0");
+		plugins(it -> it.id("dev.nokee.distributions-management")).writeTo(testDirectory.resolve("settings.gradle"));
+		executer = runner.withArgument("verify").withArgument("--configuration-cache");
+		Files.write(testDirectory.resolve("build.gradle"), Arrays.asList(
+			"plugins {",
+			"  id 'dev.nokee.jni-library'", // we must resolve a Nokee plugin so the "version" is marked as used
+			"}",
+			"tasks.register('verify')"
+		));
+		result = executer.build();
+	}
+
+	@Test
+	void reusesConfigurationCacheWhenRCFileDoesNotChanges() {
+		assertThat(executer.build().getOutput(), containsString("Reusing configuration cache"));
+	}
+
+	@Test
+	void doesNotReuseConfigurationCacheWhenRCFileChange() throws IOException {
+		writeRcFileTo(testDirectory, "0.4.0");
+		assertThat(executer.build().getOutput(), not(containsString("Reusing configuration cache")));
+	}
+}
---
+++ subprojects/distributions-management/src/main/java/dev/nokee/init/NokeeRepositoryAction.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.init;
+
+import org.gradle.api.Action;
+import org.gradle.api.artifacts.dsl.RepositoryHandler;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
+import org.gradle.api.provider.Provider;
+
+import java.net.URI;
+
+final class NokeeRepositoryAction implements Action<RepositoryHandler> {
+	private final Provider<URI> repositoryUrl;
+
+	public NokeeRepositoryAction(Provider<URI> repositoryUrl) {
+		this.repositoryUrl = repositoryUrl;
+	}
+
+	@Override
+	public void execute(RepositoryHandler repositories) {
+		repositories.maven(this::nokeeRepository);
+	}
+
+	private void nokeeRepository(MavenArtifactRepository repository) {
+		repository.setName("Nokee Artifact Repository");
+		repository.setUrl(repositoryUrl);
+		repository.mavenContent(content -> {
+			content.includeGroupByRegex("dev\\.nokee.*");
+		});
+	}
+}
