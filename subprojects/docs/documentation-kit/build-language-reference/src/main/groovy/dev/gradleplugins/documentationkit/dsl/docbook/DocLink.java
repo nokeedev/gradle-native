92c584e576c19e00ae6507088787234dfb614eb8
Daniel Lacasse <daniel@nokee.dev>
2022-06-20 16:38:39 +0200

    Remove build-language-reference project
    The project was moved to gradle/plugins/documentation-kit-plugins
    project.
    
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/docs/documentation-kit/build-language-reference/src/main/groovy/dev/gradleplugins/documentationkit/dsl/docbook/DocLink.java
+++
@@ -1,7 +0,0 @@
-package dev.gradleplugins.documentationkit.dsl.docbook;
-
-import org.w3c.dom.Element;
-
-public interface DocLink extends Renderable {
-	Element getDocbook();
-}
--- subprojects/docs/documentation-kit/build-language-reference/src/main/groovy/dev/gradleplugins/documentationkit/dsl/docbook/model/MixinMetaData.java
+++
@@ -1,9 +0,0 @@
-package dev.gradleplugins.documentationkit.dsl.docbook.model;
-
-import lombok.Value;
-
-@Value
-public class MixinMetaData {
-	String pluginId;
-	String mixinClass;
-}
