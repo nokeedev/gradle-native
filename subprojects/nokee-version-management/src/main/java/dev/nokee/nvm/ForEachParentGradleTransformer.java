4db5a646575a9ec09360907fb69a733f52df94ee
Daniel Lacasse <daniel@nokee.dev>
2022-06-21 12:21:09 +0200

    Avoid circular resolution by using BuildIdentifier to find child build
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/nokee-version-management/src/main/java/dev/nokee/nvm/ForEachParentGradleTransformer.java
+++ subprojects/nokee-version-management/src/main/java/dev/nokee/nvm/ForEachParentGradleTransformer.java
@@ -16,6 +16,7 @@
 package dev.nokee.nvm;
 
 import org.gradle.api.Transformer;
+import org.gradle.api.artifacts.component.BuildIdentifier;
 import org.gradle.api.initialization.IncludedBuild;
 import org.gradle.api.internal.GradleInternal;
 import org.gradle.api.invocation.Gradle;
@@ -77,39 +78,49 @@ class ForEachParentGradleTransformer<OUT> implements Transformer<OUT, Gradle> {
 		return null;
 	}
 
-	private static Iterable<GradleInternal> findParents(GradleInternal parent, Gradle childToFind) {
+	private static Iterable<GradleInternal> findParents(GradleInternal parent, GradleInternal childToFind) {
 		final List<GradleInternal> result = new ArrayList<>();
 		findParents(result, parent, childToFind);
 		return Collections.unmodifiableList(result);
 	}
 
-	private static boolean findParents(List<GradleInternal> result, GradleInternal parent, Gradle childToFind) {
-		if (parent == childToFind) {
-			return true; // found our child
-		}
-
+	private static boolean findParents(List<GradleInternal> result, GradleInternal parent, GradleInternal childToFind) {
 		for (final IncludedBuild includedBuild : parent.getIncludedBuilds()) {
-			final GradleInternal includedBuildModel = getGradle(includedBuild);
-			if (findParents(result, includedBuildModel, childToFind)) {
+			// To verify the child, we don't try to get the model for the included build because this can cause a realize loop
+			if (getBuildIdentifier(includedBuild).equals(childToFind.getOwner().getBuildIdentifier())) {
 				result.add(parent);
-				return true; // exit with our parent chain
+				return true; // found our child
+			} else {
+				final GradleInternal includedBuildModel = getGradle(includedBuild);
+				if (findParents(result, includedBuildModel, childToFind)) {
+					result.add(parent);
+					return true; // exit with our parent chain
+				}
 			}
 		}
 		return false; // nothing found
 	}
 
-	private static GradleInternal getGradle(IncludedBuild includedBuild) {
+	private static BuildIdentifier getBuildIdentifier(IncludedBuild includedBuild) {
+		return asBuildState(includedBuild).getBuildIdentifier();
+	}
+
+	private static BuildState asBuildState(IncludedBuild includedBuild) {
 		if (includedBuild instanceof BuildState) {
-			return getGradle((BuildState) includedBuild);
+			return (BuildState) includedBuild;
 		} else {
 			try {
 				Class<?> IncludedBuildInternal = Class.forName("org.gradle.internal.composite.IncludedBuildInternal");
 				Method method = IncludedBuildInternal.getMethod("getTarget");
-				return getGradle((BuildState) method.invoke(includedBuild));
+				return (BuildState) method.invoke(includedBuild);
 			} catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
 				throw new RuntimeException(e);
 			}
 		}
+	}
+
+	private static GradleInternal getGradle(IncludedBuild includedBuild) {
+		return getGradle(asBuildState(includedBuild));
 	}
 
 	// 7.1 can use gradle.getOwner().getMutableModel()
