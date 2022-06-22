bad0cf9328e8a8e7020b39fed7793e63c41fdb7c
Daniel Lacasse <daniel@nokee.dev>
2022-06-22 15:02:32 +0200

    Move properties capability to plugin
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/core-model/src/integrationTest/java/dev/nokee/model/ModelPropertyFileCollectionIntegrationTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.model;
+
+import dev.nokee.internal.testing.util.ProjectTestUtils;
+import dev.nokee.model.internal.core.GradlePropertyComponent;
+import dev.nokee.model.internal.core.ModelElementProviderSourceComponent;
+import dev.nokee.model.internal.core.ModelNode;
+import dev.nokee.model.internal.core.ModelPropertyTag;
+import dev.nokee.model.internal.core.ModelPropertyTypeComponent;
+import dev.nokee.model.internal.plugins.ModelBasePlugin;
+import dev.nokee.model.internal.registry.ModelRegistry;
+import org.gradle.api.Project;
+import org.gradle.api.file.ConfigurableFileCollection;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static dev.nokee.model.internal.core.ModelRegistration.builder;
+import static dev.nokee.model.internal.tags.ModelTags.tag;
+import static dev.nokee.model.internal.type.ModelType.of;
+import static dev.nokee.model.internal.type.ModelTypes.set;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ModelPropertyFileCollectionIntegrationTest {
+	private final Project project = ProjectTestUtils.rootProject();
+	private ModelNode subject;
+
+	@BeforeEach
+	void setUp() {
+		project.getPluginManager().apply(ModelBasePlugin.class);
+		subject = project.getExtensions().getByType(ModelRegistry.class).instantiate(builder()
+			.withComponent(tag(ModelPropertyTag.class))
+			.withComponent(new ModelPropertyTypeComponent(set(of(File.class))))
+			.build());
+	}
+
+	@Test
+	void hasGradlePropertyComponent() {
+		assertTrue(subject.has(GradlePropertyComponent.class));
+	}
+
+	@Test
+	void hasModelElementProviderSourceComponent() {
+		assertTrue(subject.has(ModelElementProviderSourceComponent.class));
+	}
+
+	@Test
+	void usesConfigurableFileCollectionAsGradleProperty() {
+		assertTrue(subject.get(GradlePropertyComponent.class).get() instanceof ConfigurableFileCollection);
+	}
+}
--- subprojects/core-model/src/main/java/dev/nokee/model/internal/properties/ModelPropertiesCapabilityPlugin.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/properties/ModelPropertiesCapabilityPlugin.java
@@ -17,11 +17,14 @@ package dev.nokee.model.internal.properties;
 
 import dev.nokee.model.internal.core.DisplayName;
 import dev.nokee.model.internal.core.DisplayNameComponent;
+import dev.nokee.model.internal.core.GradlePropertyComponent;
 import dev.nokee.model.internal.core.ModelActionWithInputs;
 import dev.nokee.model.internal.core.ModelComponentReference;
+import dev.nokee.model.internal.core.ModelElementProviderSourceComponent;
 import dev.nokee.model.internal.core.ModelNode;
 import dev.nokee.model.internal.core.ModelPathComponent;
 import dev.nokee.model.internal.core.ModelPropertyTag;
+import dev.nokee.model.internal.core.ModelPropertyTypeComponent;
 import dev.nokee.model.internal.core.ParentComponent;
 import dev.nokee.model.internal.names.ElementName;
 import dev.nokee.model.internal.names.ElementNameComponent;
@@ -30,13 +33,29 @@ import dev.nokee.model.internal.tags.ModelComponentTag;
 import dev.nokee.model.internal.tags.ModelTags;
 import lombok.val;
 import org.gradle.api.Plugin;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.plugins.ExtensionAware;
 import org.gradle.api.plugins.PluginAware;
 
 import javax.annotation.Nullable;
+import javax.inject.Inject;
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+
+import static dev.nokee.model.internal.type.ModelType.of;
+import static dev.nokee.model.internal.type.ModelTypes.set;
 
 public abstract class ModelPropertiesCapabilityPlugin<T extends ExtensionAware & PluginAware> implements Plugin<T> {
+	private final ObjectFactory objects;
+
+	@Inject
+	public ModelPropertiesCapabilityPlugin(ObjectFactory objects) {
+		this.objects = objects;
+	}
+
 	@Override
 	public void apply(T target) {
 		val model = target.getExtensions().getByType(ModelConfigurer.class);
@@ -44,6 +63,7 @@ public abstract class ModelPropertiesCapabilityPlugin<T extends ExtensionAware &
 		model.configure(ModelActionWithInputs.of(ModelTags.referenceOf(ModelPropertyTag.class), ModelComponentReference.of(ElementNameComponent.class), this::execute));
 		model.configure(ModelActionWithInputs.of(ModelTags.referenceOf(ModelPropertyTag.class), ModelComponentReference.of(ParentComponent.class), this::execute));
 		model.configure(ModelActionWithInputs.of(ModelTags.referenceOf(ModelPropertyTag.class), ModelComponentReference.of(ElementNameComponent.class), ModelComponentReference.of(ParentComponent.class),this::execute));
+		model.configure(ModelActionWithInputs.of(ModelTags.referenceOf(ModelPropertyTag.class), ModelComponentReference.of(ModelPropertyTypeComponent.class), this::execute));
 	}
 
 	// ComponentFromEntity<ModelPathComponent> read-only (from ParentComponent)
@@ -80,5 +100,29 @@ public abstract class ModelPropertiesCapabilityPlugin<T extends ExtensionAware &
 
 	private static String quote(String s) {
 		return "'" + s + "'";
+	}
+
+	private void execute(ModelNode entity, ModelComponentTag<ModelPropertyTag> tag, ModelPropertyTypeComponent propertyType) {
+		if (propertyType.get().equals(set(of(File.class)))) {
+			val property = objects.fileCollection();
+			entity.addComponent(new GradlePropertyComponent(property));
+			entity.addComponent(new ModelElementProviderSourceComponent(property.getElements()));
+		} else if (propertyType.get().isSubtypeOf(Map.class)) {
+			val property = objects.mapProperty(propertyType.get().getTypeVariables().get(0).getConcreteType(), propertyType.get().getTypeVariables().get(1).getConcreteType());
+			entity.addComponent(new GradlePropertyComponent(property));
+			entity.addComponent(new ModelElementProviderSourceComponent(property));
+		} else if (propertyType.get().isSubtypeOf(List.class)) {
+			val property = objects.listProperty(propertyType.get().getTypeVariables().get(0).getConcreteType());
+			entity.addComponent(new GradlePropertyComponent(property));
+			entity.addComponent(new ModelElementProviderSourceComponent(property));
+		} else if (propertyType.get().isSubtypeOf(Set.class)) {
+			val property = objects.setProperty(propertyType.get().getTypeVariables().get(0).getConcreteType());
+			entity.addComponent(new GradlePropertyComponent(property));
+			entity.addComponent(new ModelElementProviderSourceComponent(property));
+		} else {
+			val property = objects.property(propertyType.get().getConcreteType());
+			entity.addComponent(new GradlePropertyComponent(property));
+			entity.addComponent(new ModelElementProviderSourceComponent(property));
+		}
 	}
 }
