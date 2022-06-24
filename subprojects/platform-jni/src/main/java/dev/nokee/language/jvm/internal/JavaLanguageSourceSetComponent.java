5d3e03a649d8c46d71110b5ad11960248d51c3cc
Daniel Lacasse <daniel@nokee.dev>
2022-06-24 15:27:16 +0200

    Declare JVM language source set component as linked entity
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/platform-jni/src/main/java/dev/nokee/language/jvm/internal/JavaLanguageSourceSetComponent.java
+++ subprojects/platform-jni/src/main/java/dev/nokee/language/jvm/internal/JavaLanguageSourceSetComponent.java
@@ -15,10 +15,11 @@
  */
 package dev.nokee.language.jvm.internal;
 
+import dev.nokee.model.internal.core.LinkedEntity;
 import dev.nokee.model.internal.core.ModelComponent;
 import dev.nokee.model.internal.core.ModelNode;
 
-public final class JavaLanguageSourceSetComponent implements ModelComponent {
+public final class JavaLanguageSourceSetComponent implements ModelComponent, LinkedEntity {
 	private final ModelNode value;
 
 	public JavaLanguageSourceSetComponent(ModelNode value) {
