3f5a5dcdbed6cb1f24c0ae9378d2329e6c4e1b76
Daniel Lacasse <daniel@nokee.dev>
2022-06-27 15:43:47 +0200

    Introduce early implementation of multi-element components
    Just like ModelTag, the ModelBufferElement is an optimization over
    multi-element components. Instead of writing the heavy lifting yourself,
    we can use this special component. There are some constraint to model
    buffers but, generally, they are immutable dynamic buffers. The
    implementation should be consider opaque and each element must be final
    classes (or later managed) and respect the equal contract. At the
    moment, there is no way to specify the backing collection type (set or
    list). We don't intent to allow specific backing collection but we
    intent to allow user specify the duplication strategy (basically set vs
    list). For reproducibility, the backing collection will always keep the
    ordering.
    
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/buffers/ModelBuffers.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.model.internal.buffers;
+
+import com.google.common.collect.ImmutableList;
+import dev.nokee.model.internal.core.ModelComponentReference;
+import dev.nokee.model.internal.core.ModelComponentType;
+import lombok.val;
+
+import java.util.Collections;
+
+public final class ModelBuffers {
+	private ModelBuffers() {}
+
+	public static <E extends ModelBufferElement> ModelComponentReference<ModelBufferComponent<E>> referenceOf(Class<E> elementType) {
+		return ModelComponentReference.ofInstance(typeOf(elementType));
+	}
+
+	public static <T extends ModelBufferElement> ModelComponentType<ModelBufferComponent<T>> typeOf(Class<T> elementType) {
+		@SuppressWarnings("unchecked")
+		val result = (ModelComponentType<ModelBufferComponent<T>>) ((ModelComponentType<?>) ModelComponentType.componentOf(elementType));
+		return result;
+	}
+
+	public static <E extends ModelBufferElement> ModelBufferComponent<E> empty(Class<E> elementType) {
+		return new ModelBufferComponent<>(elementType, Collections.emptyList());
+	}
+
+	@SafeVarargs
+	public static <E extends ModelBufferElement> ModelBufferComponent<E> of(E firstElement, E... otherElements) {
+		@SuppressWarnings("unchecked")
+		val elementType = (Class<E>) firstElement.getClass();
+		val builder = ImmutableList.<E>builder().add(firstElement);
+		for (E otherElement : otherElements) {
+			builder.add(otherElement);
+		}
+		return new ModelBufferComponent<>(elementType, builder.build());
+	}
+}
