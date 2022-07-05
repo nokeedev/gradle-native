5b6afae9f0cbbe9e3c754ebdf57d7e6447d24a0d
Daniel Lacasse <daniel@nokee.dev>
2022-07-04 20:43:21 -0400

    Introduce a component to hold the raw Configuration object
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/dependencies/ResolvableDependencyBucketRegistrationFactory.java
+++ subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/dependencies/ResolvableDependencyBucketRegistrationFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 the original author or authors.
+ * Copyright 2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,6 +76,7 @@ public final class ResolvableDependencyBucketRegistrationFactory {
 			.withComponent(ofInstance(bucket))
 			.withComponent(ofInstance(incoming))
 			.withComponent(tag(ResolvableDependencyBucketTag.class))
+			.withComponent(new ConfigurationComponent(configurationProvider))
 			.build();
 	}
 
