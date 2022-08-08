904618fa1c20eeed64ec1912c421757f9226f9e9
Daniel Lacasse <daniel@nokee.dev>
2022-08-08 14:39:32 -0400

    Ensure ExtensionAwareMixIn reliably resolve extensions as properties
    We also fix the Gradle issue where Property and co. types can be set
    using Groovy assign operators when they are extensions.
    
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/extensionaware/ExtensionAwareMixIn.java
+++ subprojects/platform-base/src/main/java/dev/nokee/platform/base/internal/extensionaware/ExtensionAwareMixIn.java
@@ -15,17 +15,81 @@
  */
 package dev.nokee.platform.base.internal.extensionaware;
 
+import com.google.common.collect.ImmutableMap;
 import dev.nokee.model.internal.core.ModelNodes;
 import dev.nokee.model.internal.tags.ModelTag;
 import dev.nokee.platform.base.internal.DomainObjectEntities;
+import groovy.lang.GroovyRuntimeException;
+import lombok.val;
+import org.gradle.api.internal.provider.PropertyInternal;
 import org.gradle.api.plugins.ExtensionAware;
 import org.gradle.api.plugins.ExtensionContainer;
+import org.gradle.api.plugins.ExtensionsSchema;
+import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.MapProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.api.provider.SetProperty;
+import org.gradle.internal.metaobject.DynamicInvokeResult;
+import org.gradle.internal.metaobject.PropertyAccess;
+import org.gradle.internal.metaobject.PropertyMixIn;
+
+import java.util.Map;
 
 @DomainObjectEntities.Tag(ExtensionAwareMixIn.Tag.class)
-public interface ExtensionAwareMixIn extends ExtensionAware {
+public interface ExtensionAwareMixIn extends ExtensionAware, PropertyMixIn {
 	@Override
 	default ExtensionContainer getExtensions() {
 		return ModelNodes.of(this).get(ExtensionAwareComponent.class).get();
+	}
+
+	@Override
+	default PropertyAccess getAdditionalProperties() {
+		return new PropertyAccess() {
+			@Override
+			public boolean hasProperty(String name) {
+				return getExtensions().findByName(name) != null;
+			}
+
+			@Override
+			public DynamicInvokeResult tryGetProperty(String name) {
+				val result = getExtensions().findByName(name);
+				if (result == null) {
+					return DynamicInvokeResult.notFound();
+				} else {
+					return DynamicInvokeResult.found(result);
+				}
+			}
+
+			@Override
+			public DynamicInvokeResult trySetProperty(String name, Object newValue) {
+				for (ExtensionsSchema.ExtensionSchema element : getExtensions().getExtensionsSchema().getElements()) {
+					if (element.getName().equals(name)) {
+						if (element.getPublicType().equals(Property.class)) {
+							((PropertyInternal<?>) element).setFromAnyValue(newValue);
+						} else if (element.getPublicType().equals(ListProperty.class)) {
+							((PropertyInternal<?>) element).setFromAnyValue(newValue);
+						} else if (element.getPublicType().equals(SetProperty.class)) {
+							((PropertyInternal<?>) element).setFromAnyValue(newValue);
+						} else if (element.getPublicType().equals(MapProperty.class)) {
+							((PropertyInternal<?>) element).setFromAnyValue(newValue);
+						} else {
+							throw new GroovyRuntimeException(String.format("Cannot set the value of read-only property '%s' for %s of type %s.", name, this, getClass().getName()));
+						}
+						return DynamicInvokeResult.found();
+					}
+				}
+				return DynamicInvokeResult.notFound();
+			}
+
+			@Override
+			public Map<String, ?> getProperties() {
+				val builder = ImmutableMap.<String, Object>builder();
+				for (ExtensionsSchema.ExtensionSchema element : getExtensions().getExtensionsSchema().getElements()) {
+					builder.put(element.getName(), getExtensions().getByName(element.getName()));
+				}
+				return builder.build();
+			}
+		};
 	}
 
 	interface Tag extends ModelTag {}
