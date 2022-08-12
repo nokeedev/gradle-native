7abc35bdced858637a70ed13f1738b5b22f8442f
Daniel Lacasse <daniel@nokee.dev>
2022-08-12 12:25:57 -0400

    Request build settings from Xcodebuild when resolving file reference
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/build-adapter-xcode/src/main/java/dev/nokee/buildadapter/xcode/internal/plugins/XcodeTargetExecTask.java
+++ subprojects/build-adapter-xcode/src/main/java/dev/nokee/buildadapter/xcode/internal/plugins/XcodeTargetExecTask.java
@@ -16,15 +16,20 @@
 package dev.nokee.buildadapter.xcode.internal.plugins;
 
 import com.google.common.collect.ImmutableList;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import dev.nokee.utils.FileSystemLocationUtils;
 import dev.nokee.xcode.XCBuildSettings;
 import dev.nokee.xcode.XCProjectReference;
 import lombok.val;
+import org.apache.commons.io.output.NullOutputStream;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.FileSystemOperations;
+import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
+import org.gradle.api.provider.ProviderFactory;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.Internal;
@@ -35,12 +40,16 @@ import org.gradle.process.ExecOperations;
 import org.gradle.process.ExecResult;
 
 import javax.inject.Inject;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static dev.nokee.buildadapter.xcode.internal.plugins.XCBuildSettingsUtils.codeSigningDisabled;
+import static dev.nokee.utils.ProviderUtils.disallowChanges;
+import static dev.nokee.utils.ProviderUtils.finalizeValueOnRead;
 import static dev.nokee.utils.ProviderUtils.ifPresent;
 
 public abstract class XcodeTargetExecTask extends DefaultTask implements XcodebuildExecTask {
@@ -61,6 +70,49 @@ public abstract class XcodeTargetExecTask extends DefaultTask implements Xcodebu
 
 	@OutputDirectory
 	public abstract DirectoryProperty getOutputDirectory();
+
+	@Internal
+	public abstract MapProperty<String, String> getBuildSettings();
+
+	@Inject
+	public XcodeTargetExecTask(ProviderFactory providers) {
+		finalizeValueOnRead(disallowChanges(getBuildSettings().value(providers.provider(() -> {
+			val outStream = new ByteArrayOutputStream();
+			getExecOperations().exec(spec -> {
+				spec.commandLine("xcodebuild", "-project", getXcodeProject().get().getLocation(), "-target", getTargetName().get());
+				spec.args(getDerivedDataPath().map(FileSystemLocationUtils::asPath).map(derivedDataPath -> {
+					return ImmutableList.of("PODS_BUILD_DIR=" + derivedDataPath.resolve("Build/Products"));
+				}).get());
+				spec.args(getDerivedDataPath().map(FileSystemLocationUtils::asPath)
+					.map(new DerivedDataPathAsBuildSettings()).map(this::asFlags).get());
+				ifPresent(getSdk(), sdk -> spec.args("-sdk", sdk));
+				ifPresent(getConfiguration(), buildType -> spec.args("-configuration", buildType));
+				spec.args(codeSigningDisabled());
+				spec.args("-showBuildSettings", "-json");
+
+				ifPresent(getWorkingDirectory(), spec::workingDir);
+				spec.setStandardOutput(outStream);
+				spec.setErrorOutput(NullOutputStream.NULL_OUTPUT_STREAM);
+			});
+			outStream.toString();
+
+			@SuppressWarnings("unchecked")
+			val parsedOutput = (List<ShowBuildSettingsEntry>) new Gson().fromJson(outStream.toString(), new TypeToken<List<ShowBuildSettingsEntry>>() {}.getType());
+			return parsedOutput.get(0).getBuildSettings();
+		}))));
+	}
+
+	private static final class ShowBuildSettingsEntry {
+		private final Map<String, String> buildSettings;
+
+		private ShowBuildSettingsEntry(Map<String, String> buildSettings) {
+			this.buildSettings = buildSettings;
+		}
+
+		public Map<String, String> getBuildSettings() {
+			return buildSettings;
+		}
+	}
 
 	@Inject
 	protected abstract FileSystemOperations getFileOperations();
