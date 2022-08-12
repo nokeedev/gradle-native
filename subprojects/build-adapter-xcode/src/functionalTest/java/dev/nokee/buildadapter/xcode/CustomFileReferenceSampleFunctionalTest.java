6ea7c26621bf9429711db5babfa7ca435c5f3400
Daniel Lacasse <daniel@nokee.dev>
2022-08-12 13:13:14 -0400

    Add functional test for custom file reference
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/build-adapter-xcode/src/functionalTest/java/dev/nokee/buildadapter/xcode/CustomFileReferenceSampleFunctionalTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.buildadapter.xcode;
+
+import dev.gradleplugins.runnerkit.BuildResult;
+import dev.gradleplugins.runnerkit.GradleRunner;
+import dev.gradleplugins.runnerkit.TaskOutcome;
+import dev.nokee.internal.testing.junit.jupiter.ContextualGradleRunnerParameterResolver;
+import dev.nokee.platform.xcode.XcodeCustomFileRef;
+import net.nokeedev.testing.junit.jupiter.io.TestDirectory;
+import net.nokeedev.testing.junit.jupiter.io.TestDirectoryExtension;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static dev.gradleplugins.buildscript.blocks.PluginsBlock.plugins;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@EnabledOnOs(OS.MAC)
+@ExtendWith({TestDirectoryExtension.class, ContextualGradleRunnerParameterResolver.class})
+class CustomFileReferenceSampleFunctionalTest {
+	static GradleRunner executer;
+	@TestDirectory static Path testDirectory;
+	static BuildResult result;
+
+	@BeforeAll
+	static void setup(GradleRunner runner) throws IOException {
+		new XcodeCustomFileRef().writeToProject(testDirectory.toFile());
+		plugins(it -> it.id("dev.nokee.xcode-build-adapter")).writeTo(testDirectory.resolve("settings.gradle"));
+		executer = runner.withArgument(":CustomFileRef");
+	}
+
+	@Test
+	void doesNotFail() {
+		assertThat(executer.build().task(":CustomFileRef").getOutcome(), equalTo(TaskOutcome.SUCCESS));
+	}
+}
---
+++ subprojects/templates/xcode-custom-file-reference/main.c
@@ -0,0 +1,12 @@
+//
+//  main.c
+//  CustomFileRef
+//
+//  Created by Daniel Lacasse on 2022-08-12.
+//
+
+#include "main.h"
+
+int main() {
+    return 0;
+}
