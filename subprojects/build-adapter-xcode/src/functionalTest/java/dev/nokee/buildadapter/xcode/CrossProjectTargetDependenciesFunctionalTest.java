4e345f47a99e4fcea40d8f7755382435cf8835f6
Daniel Lacasse <daniel@nokee.dev>
2022-08-25 12:47:23 -0400

    Fix cross-project target dependencies
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/build-adapter-xcode/src/functionalTest/java/dev/nokee/buildadapter/xcode/CrossProjectTargetDependenciesFunctionalTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.buildadapter.xcode;
+
+import dev.gradleplugins.runnerkit.BuildResult;
+import dev.gradleplugins.runnerkit.GradleRunner;
+import dev.nokee.internal.testing.junit.jupiter.ContextualGradleRunnerParameterResolver;
+import dev.nokee.samples.xcode.CrossProjectReference;
+import net.nokeedev.testing.junit.jupiter.io.TestDirectory;
+import net.nokeedev.testing.junit.jupiter.io.TestDirectoryExtension;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static dev.gradleplugins.buildscript.blocks.PluginsBlock.plugins;
+import static dev.gradleplugins.fixtures.runnerkit.BuildResultMatchers.tasksExecuted;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@ExtendWith({TestDirectoryExtension.class, ContextualGradleRunnerParameterResolver.class})
+class CrossProjectTargetDependenciesFunctionalTest {
+	@TestDirectory static Path testDirectory;
+	static BuildResult result;
+
+	@BeforeAll
+	static void setup(GradleRunner runner) throws IOException {
+		new CrossProjectReference().writeToProject(testDirectory.toFile());
+		plugins(it -> it.id("dev.nokee.xcode-build-adapter")).writeTo(testDirectory.resolve("settings.gradle"));
+		result = runner.withTasks("GreeterApp").build();
+	}
+
+	@Test
+	void doesNotFailTheBuild() {
+		assertThat(result, tasksExecuted(":GreeterApp"));
+	}
+}
