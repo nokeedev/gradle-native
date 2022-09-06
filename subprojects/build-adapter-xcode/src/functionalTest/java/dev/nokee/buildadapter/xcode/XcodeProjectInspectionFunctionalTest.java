ac7e1107b6fe1fe1af218228db4d7ba5334feb1a
Daniel Lacasse <daniel@nokee.dev>
2022-09-06 19:32:21 -0400

    Support for inspecting Xcode projects with cross-reference
    Project containing cross-references are pretty broken given their
    inputs/outputs are not properly considered.
    
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/build-adapter-xcode/src/functionalTest/java/dev/nokee/buildadapter/xcode/XcodeProjectInspectionFunctionalTest.java
+++ subprojects/build-adapter-xcode/src/functionalTest/java/dev/nokee/buildadapter/xcode/XcodeProjectInspectionFunctionalTest.java
@@ -15,10 +15,11 @@
  */
 package dev.nokee.buildadapter.xcode;
 
+import com.google.gson.Gson;
 import dev.gradleplugins.runnerkit.GradleRunner;
-import dev.gradleplugins.runnerkit.TaskOutcome;
 import dev.nokee.internal.testing.junit.jupiter.ContextualGradleRunnerParameterResolver;
 import dev.nokee.platform.xcode.XcodeSwiftApp;
+import dev.nokee.samples.xcode.GreeterAppWithRemoteLib;
 import lombok.val;
 import net.nokeedev.testing.junit.jupiter.io.TestDirectory;
 import net.nokeedev.testing.junit.jupiter.io.TestDirectoryExtension;
@@ -28,31 +29,119 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 import static dev.gradleplugins.buildscript.blocks.PluginsBlock.plugins;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.emptyIterable;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @ExtendWith({TestDirectoryExtension.class, ContextualGradleRunnerParameterResolver.class})
 class XcodeProjectInspectionFunctionalTest {
 	@TestDirectory Path testDirectory;
+	GradleRunner executer;
 
 	@BeforeEach
-	void appliesPluginUnderTest() throws IOException {
+	void appliesPluginUnderTest(GradleRunner runner) throws IOException {
 		plugins(it -> it.id("dev.nokee.xcode-build-adapter")).writeTo(testDirectory.resolve("settings.gradle"));
+		this.executer = runner.withTasks(":inspect", "--format=json");
 	}
 
 	@Test
-	void canInspectXcodeProject(GradleRunner runner) {
+	void canInspectXcodeProject() {
 		new XcodeSwiftApp().writeToProject(testDirectory);
-		val result = runner.withTasks(":inspect", "--format=json").build();
-		assertThat(result.task(":inspect").getOutcome(), equalTo(TaskOutcome.SUCCESS));
+		val report = XCProjectInspectionReport.from(executer.build().task(":inspect").getOutput());
+		assertThat(report.getName(), equalTo("XcodeSwiftApp"));
+		assertThat(report.getTargets(), contains("XcodeSwiftApp", "XcodeSwiftAppTests", "XcodeSwiftAppUITests"));
 	}
 
 	@Test
-	void canInspectXcodeTarget(GradleRunner runner) {
+	void canInspectXcodeTarget() {
 		new XcodeSwiftApp().writeToProject(testDirectory);
-		val result = runner.withTasks(":inspect", "--target=XcodeSwiftApp").build();
-		assertThat(result.task(":inspect").getOutcome(), equalTo(TaskOutcome.SUCCESS));
+		assertAll(() -> {
+				val report = XCTargetInspectionReport.from(executer.withTasks("--target=XcodeSwiftApp").build().task(":inspect").getOutput());
+				assertThat(report.getName(), equalTo("XcodeSwiftApp"));
+				assertThat(report.getDependencies(), emptyIterable());
+				assertThat(report.getInputFiles(), contains("$(SOURCE_ROOT)/XcodeSwiftApp/ViewController.swift", "$(SOURCE_ROOT)/XcodeSwiftApp/AppDelegate.swift", "$(SOURCE_ROOT)/XcodeSwiftApp/Assets.xcassets"));
+				assertThat(report.getProduct().getLocation(), equalTo("$(BUILT_PRODUCT_DIR)/XcodeSwiftApp.app"));
+			},
+			() -> {
+				val report = XCTargetInspectionReport.from(executer.withTasks("--target=XcodeSwiftAppTests").build().task(":inspect").getOutput());
+				assertThat(report.getName(), equalTo("XcodeSwiftAppTests"));
+				assertThat(report.getDependencies(), contains("XcodeSwiftApp:XcodeSwiftApp (local)"));
+				assertThat(report.getInputFiles(), contains("$(SOURCE_ROOT)/XcodeSwiftAppTests/XcodeSwiftAppTests.swift"));
+				assertThat(report.getProduct().getLocation(), equalTo("$(BUILT_PRODUCT_DIR)/XcodeSwiftAppTests.xctest"));
+			},
+			() -> {
+				val report = XCTargetInspectionReport.from(executer.withTasks("--target=XcodeSwiftAppUITests").build().task(":inspect").getOutput());
+				assertThat(report.getName(), equalTo("XcodeSwiftAppUITests"));
+				assertThat(report.getDependencies(), contains("XcodeSwiftApp:XcodeSwiftApp (local)"));
+				assertThat(report.getInputFiles(), contains("$(SOURCE_ROOT)/XcodeSwiftAppUITests/XcodeSwiftAppUITestsLaunchTests.swift", "$(SOURCE_ROOT)/XcodeSwiftAppUITests/XcodeSwiftAppUITests.swift"));
+				assertThat(report.getProduct().getLocation(), equalTo("$(BUILT_PRODUCT_DIR)/XcodeSwiftAppUITests.xctest"));
+			});
+	}
+
+	@Test
+	void canInspectXcodeTargetWithCrossProjectReference() {
+		new GreeterAppWithRemoteLib().writeToProject(testDirectory);
+		val report = XCTargetInspectionReport.from(executer.withTasks("--target=GreeterApp").build().task(":inspect").getOutput());
+		assertThat(report.getName(), equalTo("GreeterApp"));
+		assertThat(report.getDependencies(), contains("GreeterLib:GreeterLib (remote)"));
+		assertThat(report.getInputFiles(), contains("$(SOURCE_ROOT)/GreeterApp/main.c"));
+		assertThat(report.getProduct().getLocation(), equalTo("$(BUILT_PRODUCT_DIR)/GreeterApp"));
+	}
+
+	public static final class XCProjectInspectionReport {
+		private String name;
+		private List<String> targets;
+
+		public String getName() {
+			return name;
+		}
+
+		public List<String> getTargets() {
+			return targets;
+		}
+
+		public static XCProjectInspectionReport from(String output) {
+			return new Gson().fromJson(output, XCProjectInspectionReport.class);
+		}
+	}
+
+	public static final class XCTargetInspectionReport {
+		private String name;
+		private List<String> dependencies;
+		private List<String> inputFiles;
+		private ProductInspectionReport product;
+
+		public String getName() {
+			return name;
+		}
+
+		public List<String> getDependencies() {
+			return dependencies;
+		}
+
+		public List<String> getInputFiles() {
+			return inputFiles;
+		}
+
+		public ProductInspectionReport getProduct() {
+			return product;
+		}
+
+		public static final class ProductInspectionReport {
+			private String location;
+
+			public String getLocation() {
+				return location;
+			}
+		}
+
+		public static XCTargetInspectionReport from(String output) {
+			return new Gson().fromJson(output, XCTargetInspectionReport.class);
+		}
 	}
 }
