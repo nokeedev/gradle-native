b201854a69a1e23b2f0e3655a068295376fad480
Daniel Lacasse <daniel@nokee.dev>
2022-09-28 10:54:59 -0400

    Improve testing of workspace project reference
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/build-adapter-xcode/src/test/java/dev/nokee/xcode/WorkspaceProjectReferenceLoaderTests.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.nokee.xcode;
+
+import dev.nokee.xcode.workspace.XCWorkspaceData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.nio.file.Paths;
+
+import static dev.nokee.buildadapter.xcode.TestProjectReference.project;
+import static dev.nokee.buildadapter.xcode.TestWorkspaceReference.workspace;
+import static dev.nokee.xcode.workspace.XCFileReference.group;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.emptyIterable;
+
+@ExtendWith(MockitoExtension.class)
+class WorkspaceProjectReferenceLoaderTests {
+	@Mock XCLoader<XCWorkspaceData, XCWorkspaceReference> loader;
+	@Mock XCProjectReferenceFactory factory;
+	XCWorkspaceReference reference = workspace("Foo");
+	@InjectMocks WorkspaceProjectReferencesLoader subject;
+
+	@Nested
+	class WhenEmptyWorkspace {
+		@BeforeEach
+		void givenEmptyWorkspace() {
+			Mockito.when(loader.load(reference)).thenReturn(XCWorkspaceData.builder().build());
+		}
+
+		@Test
+		void returnsNoProjectReferences() {
+			assertThat(subject.load(reference), emptyIterable());
+		}
+	}
+
+	@Nested
+	class WhenWorkspaceContainsProjectReferences {
+		@BeforeEach
+		void givenWorkspaceWithProjectReferences() {
+			Mockito.when(loader.load(reference)).thenReturn(XCWorkspaceData.builder().fileRef(group("Foo.xcodeproj")).fileRef(group("Pods/Bar.xcodeproj")).build());
+			Mockito.when(factory.create(Paths.get("/test/Foo.xcodeproj"))).thenReturn(project("Foo"));
+			Mockito.when(factory.create(Paths.get("/test/Pods/Bar.xcodeproj"))).thenReturn(project("Bar"));
+		}
+
+		@Test
+		void returnsProjectReferences() {
+			assertThat(subject.load(reference), contains(project("Foo"), project("Bar")));
+		}
+	}
+
+	@Nested
+	class WhenWorkspaceContainsNonXcodeProjectFileReference {
+		@BeforeEach
+		void givenWorkspaceWithNonProjectReference() {
+			Mockito.when(loader.load(reference)).thenReturn(XCWorkspaceData.builder().fileRef(group("Foo.xcodeproj")).fileRef(group("README")).build());
+			Mockito.when(factory.create(Paths.get("/test/Foo.xcodeproj"))).thenReturn(project("Foo"));
+		}
+
+		@Test
+		void returnsOnlyProjectReferences() {
+			assertThat(subject.load(reference), contains(project("Foo")));
+		}
+	}
+
+	@Nested
+	class WhenWorkspaceHasFileReferenceWhichContainsXcodeProjectExtension {
+		@BeforeEach
+		void givenWorkspaceWithNonProjectReference() {
+			Mockito.when(loader.load(reference)).thenReturn(XCWorkspaceData.builder().fileRef(group("Foo.xcodeproj.bak")).build());
+		}
+
+		@Test
+		void doesNotReturnNonXcodeProjectReference() {
+			assertThat(subject.load(reference), emptyIterable());
+		}
+	}
+}
