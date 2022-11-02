f9889ec3c877c18abdb7a19b14c20a0b29c6f8f6
Daniel Lacasse <daniel@nokee.dev>
2022-11-02 14:16:52 -0400

    Fix master failure detection
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- .github/actions/check-master.js
+++ .github/actions/check-master.js
@@ -4,13 +4,18 @@ const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
 (async function() {
-	const { data } = await octokit.request("GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?branch=master&per_page=1&event=push", {
+	const { data } = await octokit.request("GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?branch=master&per_page=20&event=push", {
 		owner,
 		repo,
 		workflow_id: 1831037,
 	});
 
-	if (data.workflow_runs[0].conclusion == 'failure') {
-		process.exit(1)
-	}
+	data.workflow_runs.forEach(run => {
+		if (run.conclusion == 'failure') {
+			console.log("Branch 'master' is currently failing! Please fix it...");
+			process.exit(-1)
+		} else if (run.conclusion == 'success') {
+			process.exit(0)
+		}
+	})
 }());
