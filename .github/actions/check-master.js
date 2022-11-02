84cfba841ba99cc333d587c43d1a5087b9d2cd62
Daniel Lacasse <daniel@nokee.dev>
2022-11-02 14:09:54 -0400

    Use anonymous async function
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- .github/actions/check-master.js
+++ .github/actions/check-master.js
@@ -3,12 +3,14 @@ const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-const { data } = await octokit.request("POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?branch=master&per_page=1&event=push", {
-  owner,
-  repo,
-  workflow_id: 1831037,
-});
+(async function() {
+	const { data } = await octokit.request("POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?branch=master&per_page=1&event=push", {
+		owner,
+		repo,
+		workflow_id: 1831037,
+	});
 
-if (data.workflow_runs[0].conclusion == 'failure') {
-	process.exit(1)
-}
+	if (data.workflow_runs[0].conclusion == 'failure') {
+		process.exit(1)
+	}
+}());
