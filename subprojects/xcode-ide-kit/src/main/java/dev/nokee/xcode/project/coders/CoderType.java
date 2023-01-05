3c4134bcdf4c306220e20efd4794cfcfa808377c
Daniel Lacasse <daniel@nokee.dev>
2023-01-05 20:11:10 +0400

    Extract CoderType implementations to improve testing
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/xcode-ide-kit/src/main/java/dev/nokee/xcode/project/coders/CoderType.java
+++ subprojects/xcode-ide-kit/src/main/java/dev/nokee/xcode/project/coders/CoderType.java
@@ -15,142 +15,51 @@
  */
 package dev.nokee.xcode.project.coders;
 
-import lombok.EqualsAndHashCode;
-
 import java.util.List;
 import java.util.Map;
 
 public abstract class CoderType<T> {
 	public static <T> CoderType<? extends T> anyOf(Class<T> baseType) {
-		return new AnyOfType<>(baseType);
-	}
-
-	@EqualsAndHashCode(callSuper = false)
-	private static final class AnyOfType<T> extends CoderType<T> {
-		private final Class<T> baseType;
-
-		public AnyOfType(Class<T> baseType) {
-			this.baseType = baseType;
-		}
-
-		@Override
-		public String toString() {
-			return "? extends " + baseType.getCanonicalName();
-		}
+		return new CoderAnyOfType<>(baseType);
 	}
 
 	public static CoderType<Boolean> trueFalseBoolean() {
-		return new BooleanType(BooleanType.Type.TRUE_FALSE);
+		return new CoderTrueFalseBooleanType();
 	}
 
 	public static CoderType<Boolean> oneZeroBoolean() {
-		return new BooleanType(BooleanType.Type.ONE_ZERO);
+		return new CoderOneZeroBooleanType();
 	}
 
 	public static CoderType<Boolean> yesNoBoolean() {
-		return new BooleanType(BooleanType.Type.YES_NO);
-	}
-
-	@EqualsAndHashCode(callSuper = false)
-	private static final class BooleanType extends CoderType<Boolean> {
-		private final Type type;
-
-		private BooleanType(Type type) {
-			this.type = type;
-		}
-
-		enum Type { TRUE_FALSE, ONE_ZERO, YES_NO }
+		return new CoderYesNoBooleanType();
 	}
 
 	public static <T> CoderType<T> of(Class<T> type) {
-		return new OfType<>(type);
+		return new CoderOfType<>(type);
 	}
 
-	@EqualsAndHashCode(callSuper = false)
-	private static final class OfType<T> extends CoderType<T> {
-		private final Class<T> type;
-
-		public OfType(Class<T> type) {
-			this.type = type;
-		}
-
-		@Override
-		public String toString() {
-			return type.getCanonicalName();
-		}
-	}
-
-	public static <T> CoderType<List<T>> listOf(CoderType<T> elementType) {
-		return new ListOfType<>(elementType);
-	}
-
-	@EqualsAndHashCode(callSuper = false)
-	private static final class ListOfType<E> extends CoderType<List<E>> {
-		private final CoderType<E> elementType;
-
-		public ListOfType(CoderType<E> elementType) {
-			this.elementType = elementType;
-		}
-
-		@Override
-		public String toString() {
-			return "List<" + elementType + ">";
-		}
+	public static <T> CoderType<List<T>> list(CoderType<T> elementType) {
+		return new CoderListType<>(elementType);
 	}
 
 	public static <T> CoderType<T> byRef(CoderType<T> objectType) {
-		return new ByRefType<>(objectType);
-	}
-
-	@EqualsAndHashCode(callSuper = false)
-	private static final class ByRefType<T> extends CoderType<T> {
-		private final CoderType<T> objectType;
-
-		public ByRefType(CoderType<T> objectType) {
-			this.objectType = objectType;
-		}
-
-		@Override
-		public String toString() {
-			return "byRef " + objectType;
-		}
+		return new CoderByRefType<>(objectType);
 	}
 
 	public static <T> CoderType<T> byCopy(CoderType<T> objectType) {
-		return new ByCopyType<>(objectType);
-	}
-
-	@EqualsAndHashCode(callSuper = false)
-	private static final class ByCopyType<T> extends CoderType<T> {
-		private final CoderType<T> objectType;
-
-		public ByCopyType(CoderType<T> objectType) {
-			this.objectType = objectType;
-		}
-
-		@Override
-		public String toString() {
-			return "byCopy " + objectType;
-		}
+		return new CoderByCopyType<>(objectType);
 	}
 
 	public static CoderType<Map<String, ?>> dict() {
-		return new MapType();
-	}
-
-	@EqualsAndHashCode(callSuper = false)
-	private static final class MapType extends CoderType<Map<String, ?>> {
-		@Override
-		public String toString() {
-			return "Map<String, ?>";
-		}
+		return new CoderDictionaryType();
 	}
 
 	public static CoderType<String> string() {
-		return new OfType<>(String.class);
+		return new CoderOfType<>(String.class);
 	}
 
 	public static CoderType<Integer> integer() {
-		return new OfType<>(Integer.class);
+		return new CoderOfType<>(Integer.class);
 	}
 }
