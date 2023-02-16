a8df78ca4842fb0bab519e1fd7abac563f6eedfb
Daniel Lacasse <daniel@nokee.dev>
2023-02-16 19:14:02 +0400

    Introduce more generic forwarding wrapper to test perfect forwarding to other delegate
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/internal-testing/src/main/java/dev/nokee/internal/testing/forwarding/ForwardingWrapperMatchers.java
+++ subprojects/internal-testing/src/main/java/dev/nokee/internal/testing/forwarding/ForwardingWrapperMatchers.java
@@ -16,6 +16,7 @@
 package dev.nokee.internal.testing.forwarding;
 
 import dev.nokee.internal.testing.reflect.MethodInformation;
+import dev.nokee.internal.testing.reflect.ReturnInformation;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -28,51 +29,88 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import static org.hamcrest.Matchers.allOf;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class ForwardingWrapperMatchers {
-	public static <T> Matcher<ForwardingWrapper<T>> forwardsToDelegate(MethodInformation<T, ?> methodInfo) {
+	public static <T> Matcher<ForwardingWrapperEx<T, T>> forwardsToDelegate(MethodInformation<T, ?> methodInfo) {
 		return allOf(forwardsParametersToDelegate(methodInfo), forwardsExceptionsToDelegate(methodInfo));
 	}
 
-	public static <T> Matcher<ForwardingWrapper<T>> forwardsParametersToDelegate(MethodInformation<T, ?> methodInfo) {
-		return new InteractionMatcher<>(methodInfo);
+	public static <ObjectType, ReturnInfoType extends ReturnInformation> ForwardingMatcher<ObjectType, ReturnInfoType> forwards(MethodInformation<ObjectType, ReturnInfoType> fromMethodInfo) {
+		return new ForwardingMatcher<>(fromMethodInfo);
 	}
 
-	public static <T> Matcher<ForwardingWrapper<T>> forwardsExceptionsToDelegate(MethodInformation<T, ?> methodInfo) {
-		return new TypeSafeMatcher<ForwardingWrapper<T>>() {
-			@Override
-			protected boolean matchesSafely(ForwardingWrapper<T> actual) {
-				Method method = methodInfo.resolve(actual.getForwardType());
-				Object[] passedArgs = ForwardingTestUtils.getParameterValues(method);
+	public static final class ForwardingMatcher<ObjectType, ReturnInfoType extends ReturnInformation> {
+		private final MethodInformation<ObjectType, ReturnInfoType> fromMethodInfo;
 
-				final RuntimeException exception = new RuntimeException();
-				T proxy = Mockito.mock(actual.getForwardType(), new Answer<Object>() {
-					@Override
-					public Object answer(InvocationOnMock invocation) throws Throwable {
-						assertEquals(method, invocation.getMethod());
-						throw exception;
-					}
-				});
-				T wrapper = actual.wrap(proxy);
-				try {
-					method.invoke(wrapper, passedArgs);
-					Assertions.fail(method + " failed to throw exception as is.");
-					return false; // appeasing the compiler: this line will never be executed.
-				} catch (InvocationTargetException e) {
-					if (exception != e.getCause()) {
-						throw new RuntimeException(e);
-					}
-				} catch (IllegalAccessException e) {
-					throw new AssertionError(e);
+		public ForwardingMatcher(MethodInformation<ObjectType, ReturnInfoType> fromMethodInfo) {
+			this.fromMethodInfo = fromMethodInfo;
+		}
+
+		public <DelegateType> Matcher<ForwardingWrapperEx<DelegateType, ObjectType>> to(MethodInformation<DelegateType, ReturnInfoType> toMethodInfo) {
+			return allOf(new InteractionMatcher<>(fromMethodInfo, toMethodInfo), new ExceptionMatcher<>(fromMethodInfo, toMethodInfo));
+		}
+
+		public Matcher<ForwardingWrapperEx<ObjectType, ObjectType>> toDelegate() {
+			return forwardsToDelegate(fromMethodInfo);
+		}
+	}
+
+	public static <T> Matcher<ForwardingWrapperEx<T, T>> forwardsParametersToDelegate(MethodInformation<T, ?> methodInfo) {
+		return new InteractionMatcher<>(methodInfo, methodInfo);
+	}
+
+	public static <T> Matcher<ForwardingWrapperEx<T, T>> forwardsExceptionsToDelegate(MethodInformation<T, ?> methodInfo) {
+		return new ExceptionMatcher<>(methodInfo, methodInfo);
+	}
+
+	private static class ExceptionMatcher<ObjectType, DelegateType> extends TypeSafeMatcher<ForwardingWrapperEx<DelegateType, ObjectType>> {
+		private final MethodInformation<ObjectType, ?> fromMethodInfo;
+		private final MethodInformation<DelegateType, ?> toMethodInfo;
+
+		public ExceptionMatcher(MethodInformation<ObjectType, ?> fromMethodInfo, MethodInformation<DelegateType, ?> toMethodInfo) {
+			this.fromMethodInfo = fromMethodInfo;
+			this.toMethodInfo = toMethodInfo;
+		}
+
+		@Override
+		protected boolean matchesSafely(ForwardingWrapperEx<DelegateType, ObjectType> actual) {
+			Method toMethod = toMethodInfo.resolve(actual.getForwardType());
+			Object[] passedArgs = ForwardingTestUtils.getParameterValues(toMethod);
+
+			final RuntimeException exception = new RuntimeException();
+			DelegateType proxy = Mockito.mock(actual.getForwardType(), new Answer<Object>() {
+				@Override
+				public Object answer(InvocationOnMock invocation) throws Throwable {
+					assertEquals(toMethod, invocation.getMethod());
+					throw exception;
 				}
-				return true;
-			}
+			});
+			final ObjectType wrapper = actual.wrap(proxy);
 
-			@Override
-			public void describeTo(Description description) {
+			@SuppressWarnings("unchecked")
+			final Class<ObjectType> objectType = (Class<ObjectType>) wrapper.getClass();
+			Method fromMethod = fromMethodInfo.resolve(objectType);
+			assertArrayEquals(fromMethod.getParameterTypes(), toMethod.getParameterTypes());
 
+			try {
+				fromMethod.invoke(wrapper, passedArgs);
+				Assertions.fail(fromMethod + " failed to throw exception as is.");
+				return false; // appeasing the compiler: this line will never be executed.
+			} catch (InvocationTargetException e) {
+				if (exception != e.getCause()) {
+					throw new RuntimeException(e);
+				}
+			} catch (IllegalAccessException e) {
+				throw new AssertionError(e);
 			}
-		};
+			return true;
+		}
+
+		@Override
+		public void describeTo(Description description) {
+
+		}
 	}
 }
