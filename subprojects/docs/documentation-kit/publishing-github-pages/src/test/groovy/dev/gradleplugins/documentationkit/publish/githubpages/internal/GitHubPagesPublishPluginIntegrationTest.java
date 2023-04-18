eb0803ab4912de6f67176df735773c84430b5840
Daniel Lacasse <daniel@nokee.dev>
2023-04-18 09:04:28 +0400

    Reduce lombok usage in test source sets
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/docs/documentation-kit/publishing-github-pages/src/test/groovy/dev/gradleplugins/documentationkit/publish/githubpages/internal/GitHubPagesPublishPluginIntegrationTest.java
+++ subprojects/docs/documentation-kit/publishing-github-pages/src/test/groovy/dev/gradleplugins/documentationkit/publish/githubpages/internal/GitHubPagesPublishPluginIntegrationTest.java
@@ -2,7 +2,6 @@ package dev.gradleplugins.documentationkit.publish.githubpages.internal;
 
 import com.google.common.collect.ImmutableMap;
 import dev.gradleplugins.documentationkit.publish.githubpages.tasks.PublishToGitHubPages;
-import lombok.val;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.publish.plugins.PublishingPlugin;
@@ -11,7 +10,6 @@ import org.gradle.testfixtures.ProjectBuilder;
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
-import spock.lang.Subject;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -30,7 +28,7 @@ class GitHubPagesPublishPluginIntegrationTest {
 
 	@Test
 	void createPublishTask() {
-		val publishToGitHubPages = applyPluginUnderTest().getTasks().withType(PublishToGitHubPages.class).findByName("publishToGitHubPages");
+		PublishToGitHubPages publishToGitHubPages = applyPluginUnderTest().getTasks().withType(PublishToGitHubPages.class).findByName("publishToGitHubPages");
 		assertThat(publishToGitHubPages.getGroup(), equalTo("publishing"));
 		assertThat(publishToGitHubPages.getDescription(), equalTo("Publishes site produced by this project to GitHub pages."));
 		assertThat(publishToGitHubPages.getCredentials().isPresent(), equalTo(true));
