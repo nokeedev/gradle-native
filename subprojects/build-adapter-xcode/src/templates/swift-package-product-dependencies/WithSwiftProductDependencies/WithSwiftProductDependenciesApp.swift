10cc2dcc47137d22b0732e37b985beac7207e1ac
Daniel Lacasse <daniel@nokee.dev>
2023-05-08 09:01:19 +0400

    Add functional tests for Swift package manifest generation
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/build-adapter-xcode/src/templates/swift-package-product-dependencies/WithSwiftProductDependencies/WithSwiftProductDependenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithSwiftProductDependenciesApp.swift
+//  WithSwiftProductDependencies
+//
+//  Created by Daniel Lacasse on 2023-05-06.
+//
+
+import SwiftUI
+
+@main
+struct WithSwiftProductDependenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ subprojects/build-adapter-xcode/src/templates/swift-package-product-dependencies/WithoutSwiftProductDependencies/WithoutSwiftProductDependenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithoutSwiftProductDependenciesApp.swift
+//  WithoutSwiftProductDependencies
+//
+//  Created by Daniel Lacasse on 2023-05-06.
+//
+
+import SwiftUI
+
+@main
+struct WithoutSwiftProductDependenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
