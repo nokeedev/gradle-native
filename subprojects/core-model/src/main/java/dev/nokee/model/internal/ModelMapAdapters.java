158c3d98cc0cecd1404e1438c1d4a55b2571772f
Daniel Lacasse <daniel@nokee.dev>
2023-12-03 13:53:11 +0400

    Move default KnownElements implementation to factory
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/core-model/src/main/java/dev/nokee/model/internal/ModelMapAdapters.java
+++ subprojects/core-model/src/main/java/dev/nokee/model/internal/ModelMapAdapters.java
@@ -21,18 +21,15 @@ import com.google.common.reflect.TypeToken;
 import dev.nokee.internal.Factory;
 import dev.nokee.internal.reflect.Instantiator;
 import org.gradle.api.Action;
-import org.gradle.api.DomainObjectSet;
 import org.gradle.api.ExtensiblePolymorphicDomainObjectContainer;
 import org.gradle.api.NamedDomainObjectFactory;
 import org.gradle.api.NamedDomainObjectProvider;
-import org.gradle.api.NamedDomainObjectSet;
 import org.gradle.api.Namer;
 import org.gradle.api.PolymorphicDomainObjectContainer;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ConfigurationContainer;
-import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.reflect.HasPublicType;
 import org.gradle.api.reflect.TypeOf;
 
@@ -41,7 +38,6 @@ import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static dev.nokee.model.internal.SupportedTypes.instanceOf;
@@ -65,103 +61,6 @@ public final class ModelMapAdapters {
 			if (element != null) {
 				ModelElementSupport.injectIdentifier(it, element);
 			}
-		}
-	}
-
-	private static final class KnownElement implements ModelElement {
-		private final String name;
-		private final DomainObjectSet<ModelElementIdentity> identifiers;
-
-		public KnownElement(String name, ObjectFactory objects) {
-			this.name = name;
-			this.identifiers = objects.domainObjectSet(ModelElementIdentity.class);
-		}
-
-		@Override
-		public String getName() {
-			return name;
-		}
-
-		@Override
-		public ModelObjectIdentifier getIdentifier() {
-			assert identifiers.size() >= 1; // TODO: Need to figure out
-			return identifiers.iterator().next().identifier;
-		}
-	}
-
-	static final class DefaultKnownElements implements KnownElements {
-		private final ProjectIdentifier projectIdentifier;
-		private final DomainObjectSet<ModelElementIdentity> knownElements;
-		private final NamedDomainObjectSet<KnownElement> mapping;
-
-		public DefaultKnownElements(ProjectIdentifier projectIdentifier, ObjectFactory objects) {
-			this.projectIdentifier = projectIdentifier;
-			this.knownElements = objects.domainObjectSet(ModelElementIdentity.class);
-			this.mapping = objects.namedDomainObjectSet(KnownElement.class);
-
-			knownElements.all(identity -> {
-				KnownElement element = mapping.findByName(identity.getName());
-				if (element == null) {
-					element = new KnownElement(identity.getName(), objects);
-					mapping.add(element);
-				}
-
-				element.identifiers.add(identity);
-			});
-		}
-
-		@Override
-		public <S> ModelObject<S> register(ModelObjectIdentifier identifier, Class<S> type, Function<? super String, ? extends NamedDomainObjectProvider<S>> factory) {
-			ModelElementIdentity identity = new ModelElementIdentity(identifier, type);
-			knownElements.add(identity);
-			identity.attachProvider(factory.apply(identity.getName()));
-			return identity.asModelObject(type);
-		}
-
-		@Override
-		public <S> S create(String name, Class<S> type, Function<? super ModelElement, ? extends S> factory) {
-			KnownElement element = mapping.findByName(name);
-			if (element == null) {
-				knownElements.add(new ModelElementIdentity(projectIdentifier.child(name), type));
-				element = Objects.requireNonNull(mapping.findByName(name));
-			}
-
-			return factory.apply(element);
-		}
-
-		@Override
-		public ModelElementIdentity getById(ModelObjectIdentifier identifier) {
-			KnownElement element = mapping.findByName(ModelObjectIdentifiers.asFullyQualifiedName(identifier).toString());
-			if (element == null) {
-				throw new RuntimeException("element not known");
-			}
-
-			return element.identifiers.stream().filter(it -> it.identifier.equals(identifier)).findFirst().orElseThrow(() -> new RuntimeException("element with same name found but not same id"));
-		}
-
-		@Override
-		public ModelElement findByName(String name) {
-			return mapping.findByName(name);
-		}
-
-		@Override
-		public void forEach(Action<? super ModelElementIdentity> configureAction) {
-			knownElements.all(configureAction);
-		}
-
-		public <S> Action<S> forCreatedElements(Namer<S> namer, Function<? super String, NamedDomainObjectProvider<?>> query) {
-			return it -> {
-				KnownElement element = mapping.findByName(namer.determineName(it));
-				if (element == null) {
-					return; // ignore these elements, they were not explicitly registered by us and are not created by our factories
-				}
-
-				element.identifiers.all(t -> {
-					if (t.elementProvider == null) {
-						t.attachProvider(query.apply(element.getName()));
-					}
-				});
-			};
 		}
 	}
 
@@ -472,10 +371,11 @@ public final class ModelMapAdapters {
 	public static final class ModelElementIdentity implements ModelObject<Object> {
 		private final ModelObjectIdentifier identifier;
 		private final Class<?> implementationType;
-		private NamedDomainObjectProvider<?> elementProvider;
+		NamedDomainObjectProvider<?> elementProvider; // TODO: Reduce visibility
 		private boolean forcedRealize = false;
 
-		private ModelElementIdentity(ModelObjectIdentifier identifier, Class<?> implementationType) {
+		// TODO: Reduce visibility
+		ModelElementIdentity(ModelObjectIdentifier identifier, Class<?> implementationType) {
 			this.identifier = identifier;
 			this.implementationType = implementationType;
 		}
