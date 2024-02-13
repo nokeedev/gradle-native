6fd0d9a59917f827323d3de7a52a488aceb473b6
Daniel Lacasse <daniel@nokee.dev>
2024-02-13 17:37:16 -0500

    Allow discovery of test component upon realizing testable components
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

--- subprojects/testing-base/src/main/java/dev/nokee/testing/base/internal/TestableComponentSpec.java
+++ subprojects/testing-base/src/main/java/dev/nokee/testing/base/internal/TestableComponentSpec.java
@@ -16,11 +16,22 @@
 
 package dev.nokee.testing.base.internal;
 
+import dev.nokee.model.internal.discover.DisRule;
+import dev.nokee.model.internal.discover.Discover;
+import dev.nokee.model.internal.discover.Discovery;
+import dev.nokee.model.internal.discover.SubTypeOfRule;
+import dev.nokee.model.internal.names.ElementName;
+import dev.nokee.model.internal.type.ModelType;
 import dev.nokee.platform.base.Component;
+import dev.nokee.testing.base.TestSuiteComponent;
 import dev.nokee.testing.base.TestSuiteContainer;
 import dev.nokee.testing.base.TestableComponent;
 import org.gradle.api.plugins.ExtensionAware;
 
+import java.util.Collections;
+import java.util.List;
+
+@Discover(TestableComponentSpec.Strategy.class)
 public interface TestableComponentSpec extends TestableComponent, Component, ExtensionAware {
 	String TEST_SUITES_EXTENSION_NAME = "testSuites";
 
@@ -31,5 +42,20 @@ public interface TestableComponentSpec extends TestableComponent, Component, Ext
 			throw new RuntimeException("Please apply 'dev.nokee.testing-base' plugin before using test suites.");
 		}
 		return testSuites;
+	}
+
+	final class Strategy implements Discovery {
+		@Override
+		public <T> List<DisRule> discover(ModelType<T> discoveringType) {
+			return Collections.singletonList(new SubTypeOfRule(discoveringType, new DisRule() {
+				@Override
+				public void execute(Details details) {
+					// Assuming all registration happens during realization
+					if (!details.getCandidate().isRealized()) {
+						details.newCandidate(ElementName.of("*"), ModelType.of(TestSuiteComponent.class));
+					}
+				}
+			}));
+		}
 	}
 }
