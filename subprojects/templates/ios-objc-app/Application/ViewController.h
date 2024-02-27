0707489722c3b758a6d8b44dec78e751597c4c4a
Daniel Lacasse <daniel@nokee.dev>
2024-02-27 17:33:25 -0500

    Convert the last source elements in templates to embrace processor
    Signed-off-by: Daniel Lacasse <daniel@nokee.dev>

---
+++ subprojects/templates/ios-objc-app/Application/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Application
+//
+//  Created by Daniel Lacasse on 2024-02-27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
